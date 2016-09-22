78be24d00b0d48c8407bdb76208005a77120c86b
ChaselWu <minowu@foxmail.com>
2016-09-22 14:43:57 +0800

    Add button component

---
+++ src/components/button/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn" :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'secondary'
+      }
+    },
+    computed: {
+      classes () {
+        let classes = {}
+
+        // success, error, secondary, etc.
+        classes[`btn-${this.type}`] = true
+
+        return classes
+      }
+    }
+  }
+</script>
---
+++ src/components/button/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Btn from './button'
+
+const prefix = 'Mn'
+
+Vue.component(`${prefix}Btn`, Btn)
