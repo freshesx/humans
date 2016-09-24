41382272bbd7041ada43429d127fbeec350e916e
ChaselWu <minowu@foxmail.com>
2016-09-24 15:52:43 +0800

    Remove old card component

--- src/docs/sidebars/sidebar.vue
+++ src/docs/sidebars/sidebar.vue
@@ -1,38 +1,33 @@
 <template>
   <div class="cols is-gapless">
     <div class="col is-sm-2">
-      <mn-card>
-        <mn-card-heading>
-          <mn-card-title>Menus</mn-card-title>
-        </mn-card-heading>
-        <mn-card-block>
-          <h4>Bases</h4>
-          <ul>
-            <li><a v-link="{ path: '/bases/colors' }">colors</a></li>
-            <li><a href="#">typography</a></li>
-            <li><a href="#">more</a></li>
-          </ul>
-          <h4>Grids</h4>
-          <ul>
-            <li><a href="#">section</a></li>
-            <li><a v-link="{ path: '/grids/flex' }">flex</a></li>
-            <li><a href="#">spacing</a></li>
-          </ul>
-          <h4>Components</h4>
-          <ul>
-            <li><a v-link="{ path: '/components/button' }">button</a></li>
-            <li><a href="#">icon</a></li>
-            <li><a v-link="{ path: '/components/card' }">card</a></li>
-            <li><a v-link="{ path: '/components/form' }">form</a></li>
-            <li><a href="#">tab</a></li>
-          </ul>
-          <h4>API</h4>
-          <ul>
-            <li><a href="#">variables</a></li>
-            <li><a href="#">mixins</a></li>
-          </ul>
-        </mn-card-block>
-      </mn-card>
+      <div>
+        <h4>Bases</h4>
+        <ul>
+          <li><a v-link="{ path: '/bases/colors' }">colors</a></li>
+          <li><a href="#">typography</a></li>
+          <li><a href="#">more</a></li>
+        </ul>
+        <h4>Grids</h4>
+        <ul>
+          <li><a href="#">section</a></li>
+          <li><a v-link="{ path: '/grids/flex' }">flex</a></li>
+          <li><a href="#">spacing</a></li>
+        </ul>
+        <h4>Components</h4>
+        <ul>
+          <li><a v-link="{ path: '/components/button' }">button</a></li>
+          <li><a href="#">icon</a></li>
+          <li><a v-link="{ path: '/components/card' }">card</a></li>
+          <li><a v-link="{ path: '/components/form' }">form</a></li>
+          <li><a href="#">tab</a></li>
+        </ul>
+        <h4>API</h4>
+        <ul>
+          <li><a href="#">variables</a></li>
+          <li><a href="#">mixins</a></li>
+        </ul>
+      </div>
     </div>
     <div class="col is-sm-10">
       <slot name="mainarea"></slot>
