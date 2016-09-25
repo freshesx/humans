cccab0a098c304652851dcda92675ef26083bd52
ChaselWu <minowu@foxmail.com>
2016-09-25 20:38:08 +0800

    Revert "Use validator"
    This reverts commit 911bcd2c7c1649aaad4e68ee1507c9d4dba7b3c8.

--- src/docs/components/form.vue
+++ src/docs/components/form.vue
@@ -4,53 +4,41 @@
       <div class="cols">
         <div class="col is-sm-6">
           <div class="card">
-            <validator name="validation">
-              <form novalidate>
-                <div class="card-header">
-                  <h3 class="card-title">Basic form</h3>
+            <div class="card-header">
+              <h3 class="card-title">Basic form</h3>
+            </div>
+            <div class="card-lists">
+              <div class="card-item">
+                <div class="card-item-addon">
+                  <label class="form-label">Username</label>
                 </div>
-                <div class="card-block">
-                  <div class="errors">
-                    <p v-if="$validation.username.required">Required your name.</p>
-                  </div>
+                <div class="card-item-body">
+                  <input class="form-control" type="text" placeholder="Type your username">
                 </div>
-                <div class="card-lists">
-                  <div class="card-item">
-                    <div class="card-item-addon">
-                      <label class="form-label">Username</label>
-                    </div>
-                    <div class="card-item-body">
-                      <input class="form-control" type="text" placeholder="Type your username" v-validate:username="['required']">
-                    </div>
-                  </div>
-                  <div class="card-item">
-                    <div class="card-item-addon">
-                      <label class="form-label">Password</label>
-                    </div>
-                    <div class="card-item-body">
-                      <input class="form-control" type="password" placeholder="Type your password">
-                    </div>
-                    <div class="card-item-help">
-                      <mn-icon name="menu"></mn-icon>
-                    </div>
-                  </div>
-                  <div class="card-item">
-                    <div class="card-item-addon">
-                      <label class="form-label">Phone</label>
-                    </div>
-                    <div class="card-item-body">
-                      <input class="form-control" type="text" placeholder="Type your phone number">
-                    </div>
-                    <div class="card-item-action">
-                      <button class="btn is-primary m-b-0" href="#">Send code</button>
-                    </div>
-                  </div>
-                  <div class="card-block">
-                    {{ $validation | json }}
-                  </div>
+              </div>
+              <div class="card-item">
+                <div class="card-item-addon">
+                  <label class="form-label">Password</label>
                 </div>
-              </form>
-            <validator>
+                <div class="card-item-body">
+                  <input class="form-control" type="password" placeholder="Type your password">
+                </div>
+                <div class="card-item-help">
+                  <mn-icon name="menu"></mn-icon>
+                </div>
+              </div>
+              <div class="card-item">
+                <div class="card-item-addon">
+                  <label class="form-label">Phone</label>
+                </div>
+                <div class="card-item-body">
+                  <input class="form-control" type="text" placeholder="Type your phone number">
+                </div>
+                <div class="card-item-action">
+                  <button class="btn is-primary m-b-0" href="#">Send code</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
