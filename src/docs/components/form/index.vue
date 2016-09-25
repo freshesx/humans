a7ff48923a33ff383fe0ec19c65aefa499ffb14a
ChaselWu <minowu@foxmail.com>
2016-09-25 21:35:51 +0800

    Change docs

--- src/docs/components/form/index.vue
+++ src/docs/components/form/index.vue
@@ -34,12 +34,37 @@
                 <div class="card-item-body">
                   <input class="form-control" type="text" placeholder="Type your phone number">
                 </div>
+                <!-- <div class="card-item-help">
+                  <a href="#">send code</a>
+                </div> -->
                 <div class="card-item-action">
-                  <button class="btn is-primary m-b-0" href="#">Send code</button>
+                  <button class="btn is-secondary m-b-0" href="#">Send code</button>
                 </div>
               </div>
             </div>
           </div>
+
+          <div class="card">
+            <div class="card-lists">
+              <div class="card-item">
+                <div class="card-item-body">始终</div>
+                <div class="card-item-help">
+                  <div class="form-radio-box">
+                    <mn-icon name="check"></mn-icon>
+                  </div>
+                </div>
+              </div>
+              <div class="card-item">
+                <div class="card-item-body">仅当 iPhone 已锁定时</div>
+                <div class="card-item-help"></div>
+              </div>
+            </div>
+          </div>
+          <div class="form-help">
+            <mn-icon name="notification"></mn-icon>
+            Add some help for your user.
+          </div>
+
         </div>
       </div>
     </section>
