5eaa83be845f4090d03964db38aab52cf53db510
ChaselWu <minowu@foxmail.com>
2016-09-26 12:43:57 +0800

    Add select component

---
+++ src/components/form/select.vue
@@ -0,0 +1,18 @@
+<template>
+  <select class="form-select-control">
+    <option :value="option.value" v-for="option in options">{{ option.label }}</option>
+  </select>
+</template>
+
+<script>
+  export default {
+    props: {
+      options: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
+    }
+  }
+</script>
