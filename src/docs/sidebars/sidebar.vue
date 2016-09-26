43255d623cb09db5a84750b18a59171bba5a2464
ChaselWu <minowu@foxmail.com>
2016-09-26 16:43:03 +0800

    Using stage

--- src/docs/sidebars/sidebar.vue
+++ src/docs/sidebars/sidebar.vue
@@ -1,35 +1,42 @@
 <template>
   <div class="cols is-gapless is-full-screen">
     <div class="col is-sm-2 is-full-screen">
-      <mn-scroller>
-        <div>
-          <h4>Bases</h4>
-          <ul>
-            <li><a v-link="{ path: '/bases/colors' }">colors</a></li>
-            <li><a href="#">typography</a></li>
-            <li><a href="#">more</a></li>
-          </ul>
-          <h4>Grids</h4>
-          <ul>
-            <li><a href="#">section</a></li>
-            <li><a v-link="{ path: '/grids/flex' }">flex</a></li>
-            <li><a href="#">spacing</a></li>
-          </ul>
-          <h4>Components</h4>
-          <ul>
-            <li><a v-link="{ path: '/components/button' }">button</a></li>
-            <li><a href="#">icon</a></li>
-            <li><a v-link="{ path: '/components/card' }">card</a></li>
-            <li><a v-link="{ path: '/components/form' }">form</a></li>
-            <li><a href="#">tab</a></li>
-          </ul>
-          <h4>API</h4>
-          <ul>
-            <li><a href="#">variables</a></li>
-            <li><a href="#">mixins</a></li>
-          </ul>
+      <div class="stage">
+        <div class="stage-item is-auto">
+          Vue Human UI
         </div>
-      </mn-scoller>
+        <div class="stage-item">
+          <mn-scroller>
+            <div>
+              <h4>Bases</h4>
+              <ul>
+                <li><a v-link="{ path: '/bases/colors' }">colors</a></li>
+                <li><a href="#">typography</a></li>
+                <li><a href="#">more</a></li>
+              </ul>
+              <h4>Grids</h4>
+              <ul>
+                <li><a href="#">section</a></li>
+                <li><a v-link="{ path: '/grids/flex' }">flex</a></li>
+                <li><a href="#">spacing</a></li>
+              </ul>
+              <h4>Components</h4>
+              <ul>
+                <li><a v-link="{ path: '/components/button' }">button</a></li>
+                <li><a href="#">icon</a></li>
+                <li><a v-link="{ path: '/components/card' }">card</a></li>
+                <li><a v-link="{ path: '/components/form' }">form</a></li>
+                <li><a href="#">tab</a></li>
+              </ul>
+              <h4>API</h4>
+              <ul>
+                <li><a href="#">variables</a></li>
+                <li><a href="#">mixins</a></li>
+              </ul>
+            </div>
+          </mn-scoller>
+        </div>
+      </div>
     </div>
     <div class="col is-sm-10">
       <slot name="mainarea"></slot>
