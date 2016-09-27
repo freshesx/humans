c9b8a1a83132fda43977e8a762171aa24be57f62
ChaselWu <minowu@foxmail.com>
2016-09-27 22:09:29 +0800

    New file to save scroll

---
+++ src/components/scroller/save.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+
+/**
+ * Save scroll from vue route from
+ *
+ * @param from
+ */
+export function saveScroll (from) {
+  if (from.path) {
+    const $scroll = $('.scrollbar-wrapper.is-save')
+    let storage = $scroll.data('scroll') || {}
+    storage[from.path] = $scroll.scrollTop()
+    $scroll.data('scroll', storage)
+  }
+}
+
+/**
+ * set scroll from storage
+ *
+ * @param to
+ */
+export function setScroll (to) {
+  setTimeout(() => {
+    const $scroll = $('.scrollbar-wrapper.is-save')
+    const storage = $scroll.data('scroll')
+    const top = storage ? (storage[to.path] || 0) : 0
+
+    $scroll.scrollTop(top)
+  }, 0)
+}
