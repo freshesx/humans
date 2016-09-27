a8b72ae231432a84c6d656eb0beb56166f9ad3a2
ChaselWu <minowu@foxmail.com>
2016-09-27 14:16:03 +0800

    Add popup component

---
+++ src/components/popup/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Popup from './popup'
+
+const prefix = 'Mn'
+
+Vue.component(`${prefix}Popup`, Popup)
---
+++ src/components/popup/popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="popup-mask" v-if="show">
+    <div class="popup">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      show: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
