07bcd4485b47f92528a71844e83eeca032359537
ChaselWu <minowu@foxmail.com>
2016-09-29 00:53:09 +0800

    Change colors docs

--- src/docs/bases/colors.vue
+++ src/docs/bases/colors.vue
@@ -1,33 +1,41 @@
 <template>
-  <div>
-    <h3>Color</h3>
-
-    <div>
-      最近添加2
-    </div>
-
-    <div v-for="color in colors">
-      <div class="scoped-tile">
-        <div class="scoped-tile-box" :class="getBoxClass(color.className)"></div>
-        <div class="scoped-tile-title" :class="getTextClass(color.className)">{{ color.className }}</div>
+  <div class="section">
+    <div class="letter">
+      <div class="letter-body">
+        <h1 class="letter-title is-black-text">Color</h1>
       </div>
     </div>
 
-    <h3>Background color</h3>
+    <div class="card">
+      <div class="card-header">Color schema</div>
+      <div class="card-block">
+        <div class="cols">
+          <div class="col is-xs-4" v-for="color in colors">
+            <div class="sp-tile" :class="getBoxClass(color.className)"></div>
+          </div>
+        </div>
+      </div>
+      <div class="card-block">
+        <pre>.is-{color}-bg</pre>
+      </div>
+    </div>
 
-    <ul>
-      <li v-for="color in colors">
-        bg-{{ color.className }}
-      </li>
-    </ul>
+    <div class="card">
+      <div class="card-header">Color schema</div>
+      <div class="card-block">
+        <div class="cols">
+          <div class="col is-xs-4" v-for="color in colors">
+            <div class="sp-text" :class="getTextClass(color.className)">
+              {{ color.className }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card-block">
+        <pre>.is-{color}-text</pre>
+      </div>
+    </div>
 
-    <h3>Text color</h3>
-
-    <ul>
-      <li v-for="color in colors">
-        text-{{ color.className }}
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -36,12 +44,12 @@
     methods: {
       getBoxClass (className) {
         return {
-          [`bg-${className}`]: true
+          [`is-${className}-bg`]: true
         }
       },
       getTextClass (className) {
         return {
-          [`text-${className}`]: true
+          [`is-${className}-text`]: true
         }
       }
     },
@@ -63,18 +71,14 @@
 </script>
 
 <style lang="scss" scoped>
-  .scoped-tile {
-    float: left;
-  }
-
-  .scoped-tile-box {
-    width: 60px;
-    height: 60px;
+  .sp-tile {
+    height: 80px;
+    margin: 0 auto 1rem auto;
     border-radius: 12px;
-    margin: 0 20px 12px 20px;
   }
 
-  .scoped-tile-title {
+  .sp-text {
     text-align: center;
+    margin-bottom: 1rem;
   }
 </style>
