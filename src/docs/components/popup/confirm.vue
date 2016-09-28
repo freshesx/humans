0797818965111cdaf48c5bc08fde66d422c140f4
ChaselWu <minowu@foxmail.com>
2016-09-28 22:04:04 +0800

    Add docs

---
+++ src/docs/components/popup/confirm.vue
@@ -0,0 +1,29 @@
+<template>
+  <mn-popup :show.sync="show" center>
+    <div class="card m-b-0">
+      <div class="card-block is-center-text">
+        Are you sure?
+      </div>
+      <div class="card-btns is-column">
+        <button class="is-green-text">OK</button>
+        <button @click="cancel">Cancel</button>
+      </div>
+    </div>
+  </mn-popup>
+</template>
+
+<script>
+  export default {
+    props: {
+      show: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      cancel () {
+        this.show = false
+      }
+    }
+  }
+</script>
--- src/docs/components/popup/index.vue
+++ src/docs/components/popup/index.vue
@@ -8,17 +8,31 @@
         <mn-btn class="is-primary is-block" @click.prevent="openPopup">Open popup</mn-btn>
       </div>
     </div>
+
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Confirm</h3>
+      </div>
+      <div class="card-block">
+        <mn-btn class="is-warning is-block" @click.prevent="openConfirm">Open confirm</mn-btn>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import Popup from './popup'
+  import Confirm from './confirm'
 
   export default {
     methods: {
       openPopup () {
         this.popup = this.$element(Popup)
         this.popup.$set('show', true)
+      },
+      openConfirm () {
+        this.confirm = this.$element(Confirm)
+        this.confirm.$set('show', true)
       }
     }
   }
