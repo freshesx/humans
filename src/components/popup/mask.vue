a406d28e4a9dc69c5fe82c2458914a536872c8e7
ChaselWu <minowu@foxmail.com>
2016-09-28 17:33:26 +0800

    Popup component

---
+++ src/components/popup/mask.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="popup-mask" :style="{ 'z-index': zIndex }" transition="is-fade" @click="close"></div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      close () {
+        this.$dispatch('mask.close')
+      }
+    },
+    data () {
+      return {
+        zIndex: 2000
+      }
+    }
+  }
+</script>
--- src/components/popup/popup.vue
+++ src/components/popup/popup.vue
@@ -1,17 +1,57 @@
 <template>
-  <div class="popup-mask" v-if="show">
-    <div class="popup">
-      <slot></slot>
-    </div>
+  <div class="popup" v-if="show" :style="{ 'z-index': zIndex }" transition="is-slide-up">
+    <slot></slot>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import MaskClass from './mask'
+
   export default {
     props: {
       show: {
         type: Boolean,
         default: false
+      }
+    },
+    methods: {
+      close () {
+        this.show = false
+        this.destroyMask()
+      },
+      appendMask (zIndex) {
+        const Mask = Vue.extend(MaskClass)
+
+        this.mask = new Mask({
+          el: document.createElement('div')
+        })
+
+        this.mask.$set('zIndex', zIndex)
+        this.mask.$appendTo(document.body)
+
+        // Listen mask.close click event, and exec this.close()
+        this.mask.$on('mask.close', () => {
+          this.close()
+        })
+      },
+      destroyMask () {
+        if (this.mask) {
+          this.mask.$destroy(true)
+          this.mask = null
+        }
+      }
+    },
+    watch: {
+      show (newValue) {
+        if (newValue) {
+          this.appendMask(this.zIndex - 1)
+        }
+      }
+    },
+    data () {
+      return {
+        zIndex: 20000
       }
     }
   }
