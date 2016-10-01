3636917409249384d918b7a9135a6925c11853e2
ChaselWu <minowu@foxmail.com>
2016-10-01 11:16:14 +0800

    Loading image components

---
+++ src/components/image/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Image from './image'
+import BlockImage from './block-image'
+
+const prefix = 'Mn'
+
+Vue.component(`${prefix}Image`, Image)
+Vue.component(`${prefix}BlockImage`, BlockImage)
