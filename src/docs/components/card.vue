893019f9571edc0f9ea84d77ed657dd7912e8630
ChaselWu <minowu@foxmail.com>
2016-10-01 11:40:38 +0800

    Add article

--- src/docs/components/card.vue
+++ src/docs/components/card.vue
@@ -15,17 +15,19 @@
             <div class="card-title">Card</div>
           </div>
           <div class="card-block">
-            <h3>What is Lorem Ipsum?</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </p>
-            <p>
-              <mn-image :options="inlineImage" width="120" style="float: left; margin-right: 1rem;"></mn-image>
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            </p>
-            <p>
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            <article>
+              <h3>What is Lorem Ipsum?</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+              <p>
+                <mn-image :options="inlineImage" width="120" style="float: left; margin-right: 1rem;"></mn-image>
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+              </p>
+              <p>
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </article>
           </div>
           <div class="card-lists">
             <div class="card-item">
