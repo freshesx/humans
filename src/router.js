500a283a410f869a9639046463091f43fc37240c
ChaselWu <minowu@foxmail.com>
2016-10-05 01:41:12 +0800

    Add router

--- src/router.js
+++ src/router.js
@@ -8,8 +8,36 @@ export default new VueRouter({
   routes: [
     {
       path: '/',
-      meta: {},
+      meta: { scroll: true },
       component: resolve => { require(['./docs/home'], resolve) }
+    },
+    {
+      path: '/bases/colors',
+      component: resolve => { require(['./docs/bases/colors'], resolve) }
+    },
+    {
+      path: '/bases/typography',
+      component: resolve => { require(['./docs/bases/typography'], resolve) }
+    },
+    {
+      path: '/grids/flex',
+      component: resolve => { require(['./docs/grids/flex'], resolve) }
+    },
+    {
+      path: '/components/button',
+      component: resolve => { require(['./docs/components/button'], resolve) }
+    },
+    {
+      path: '/components/card',
+      component: resolve => { require(['./docs/components/card'], resolve) }
+    },
+    {
+      path: '/components/form',
+      component: resolve => { require(['./docs/components/form'], resolve) }
+    },
+    {
+      path: '/components/popup',
+      component: resolve => { require(['./docs/components/popup'], resolve) }
     }
   ]
 })
