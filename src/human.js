e029c29520522404d3a4b29e0979929fe245b0de
ChaselWu <minowu@foxmail.com>
2016-10-12 01:30:26 +0800

    Set new human file

--- src/human.js
+++ src/human.js
@@ -1,5 +1,28 @@
-import Vue from 'vue'
-import VueElement from './components/util/element'
-import './components'
+import Scroller from './components/scroller/scroller.vue'
+import Icon from './components/icon/icon.vue'
+import Btn from './components/button/button.vue'
+import Switch from './components/form/switch.vue'
+import Select from './components/form/select.vue'
+import Pre from './components/code/pre'
+import Popup from './components/popup/popup'
+import Image from './components/image/image'
+import BlockImage from './components/image/block-image'
+import element from './components/util/element'
+import '../sass/human.scss'
 
-Vue.use(VueElement)
+export default {
+  prefix: 'Mn',
+  install (Vue, options) {
+    Vue.component(`${this.prefix}Scroller`, Scroller)
+    Vue.component(`${this.prefix}Icon`, Icon)
+    Vue.component(`${this.prefix}Btn`, Btn)
+    Vue.component(`${this.prefix}Switch`, Switch)
+    Vue.component(`${this.prefix}Select`, Select)
+    Vue.component(`${this.prefix}Pre`, Pre)
+    Vue.component(`${this.prefix}Popup`, Popup)
+    Vue.component(`${this.prefix}Image`, Image)
+    Vue.component(`${this.prefix}BlockImage`, BlockImage)
+
+    Vue.use(element)
+  }
+}
