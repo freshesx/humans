a33ccb5f222d63fcd9ccd82c413d37eb609fd160
ChaselWu <minowu@foxmail.com>
2016-10-12 14:40:48 +0800

    Add Form & inline

--- components/components.js
+++ components/components.js
@@ -1,11 +1,11 @@
 import Scroller from './scroller/scroller'
 import Icon from './icon/icon'
 import Btn from './button/button'
-import Switch from './form/switch'
-import Select from './form/select'
+import FormSwitch from './form/switch'
+import FormSelect from './form/select'
 import Pre from './code/pre'
 import Popup from './popup/popup'
-import Image from './image/image'
+import InlineImage from './image/image'
 import BlockImage from './image/block-image'
 
 export default {
@@ -16,13 +16,13 @@ export default {
   // Button
   Btn,
   // Form
-  Switch,
-  Select,
+  FormSwitch,
+  FormSelect,
   // Code
   Pre,
   // Popup
   Popup,
   // Image
-  Image,
+  InlineImage,
   BlockImage
 }
