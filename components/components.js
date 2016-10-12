36aa94781ba52efbf243b1d197f5a1fbbfc2d490
ChaselWu <minowu@foxmail.com>
2016-10-12 13:49:36 +0800

    Add commits

--- components/components.js
+++ components/components.js
@@ -1,11 +1,18 @@
 export default {
-  Scroller: './scroller/scroller.vue',
-  Icon: './icon/icon.vue',
-  Btn: './button/button.vue',
-  Switch: './form/switch.vue',
-  Select: './form/select.vue',
+  // Scroller
+  Scroller: './scroller/scroller',
+  // Icon
+  Icon: './icon/icon',
+  // Button
+  Btn: './button/button',
+  // Form
+  Switch: './form/switch',
+  Select: './form/select',
+  // Code
   Pre: './code/pre',
+  // Popup
   Popup: './popup/popup',
+  // Image
   Image: './image/image',
   BlockImage: './image/block-image'
 }
