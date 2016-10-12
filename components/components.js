7b5f396ef0e76b78ee90a50eb052f9f8b1e338e3
ChaselWu <minowu@foxmail.com>
2016-10-12 13:43:48 +0800

    require components

---
+++ components/components.js
@@ -0,0 +1,11 @@
+export default {
+  Scroller: './scroller/scroller.vue',
+  Icon: './icon/icon.vue',
+  Btn: './button/button.vue',
+  Switch: './form/switch.vue',
+  Select: './form/select.vue',
+  Pre: './code/pre',
+  Popup: './popup/popup',
+  Image: './image/image',
+  BlockImage: './image/block-image'
+}
--- components/index.js
+++ components/index.js
@@ -1,26 +1,18 @@
-import Scroller from './scroller/scroller.vue'
-import Icon from './icon/icon.vue'
-import Btn from './button/button.vue'
-import Switch from './form/switch.vue'
-import Select from './form/select.vue'
-import Pre from './code/pre'
-import Popup from './popup/popup'
-import Image from './image/image'
-import BlockImage from './image/block-image'
+import lodash from 'lodash'
 import element from './util/element'
+import components from './components'
 
 export default {
   prefix: 'Mn',
   install (Vue, options) {
-    Vue.component(`${this.prefix}Scroller`, Scroller)
-    Vue.component(`${this.prefix}Icon`, Icon)
-    Vue.component(`${this.prefix}Btn`, Btn)
-    Vue.component(`${this.prefix}Switch`, Switch)
-    Vue.component(`${this.prefix}Select`, Select)
-    Vue.component(`${this.prefix}Pre`, Pre)
-    Vue.component(`${this.prefix}Popup`, Popup)
-    Vue.component(`${this.prefix}Image`, Image)
-    Vue.component(`${this.prefix}BlockImage`, BlockImage)
+    // Custom prefix
+    if (lodash.get(options, 'prefix')) {
+      this.prefix = options.prefix
+    }
+
+    lodash.forEach(components, (value, key) => {
+      Vue.component(`${this.prefix}${key}`, require(value))
+    })
 
     Vue.use(element)
   }
