defd50868f5a8b4fb70746b7b52cf973d5912c29
ChaselWu <minowu@foxmail.com>
2016-10-12 14:41:10 +0800

    Using inline-image

--- examples/docs/components/card.vue
+++ examples/docs/components/card.vue
@@ -21,7 +21,7 @@
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               </p>
               <p>
-                <mn-image :options="inlineImage" width="120" style="float: left; margin-right: 1rem;"></mn-image>
+                <mn-inline-image :options="inlineImage" width="120" style="float: left; margin-right: 1rem;"></mn-inline-image>
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
               </p>
               <p>
