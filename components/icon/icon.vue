050c9db4a8ca7b55c24eacd56a8a974a4750175d
ChaselWu <minowu@foxmail.com>
2016-10-14 16:03:18 +0800

    New icon sets

--- components/icon/icon.vue
+++ components/icon/icon.vue
@@ -1,27 +1,15 @@
 <template>
-  <svg version="1.1"
-    class="icon"
-    :class="classes"
-    :width="width"
-    :height="height"
-    :viewBox="box"
-    :style="style"
-  >
-    <path :d="icon.d" />
+  <svg version="1.1" class="icon" :class="classes" :width="width" :height="height" :viewBox="box">
+    <path :d="path.d" :fill="path.fill" v-for="path in paths"></path>
   </svg>
 </template>
 
 <script>
-  import icons from './icons.json'
-
   export default {
     props: {
       name: {
         type: String,
-        required: true,
-        validator: val => {
-          return val in icons
-        }
+        required: true
       },
       scale: {
         type: Number,
@@ -39,29 +27,24 @@
         }
       },
       icon () {
-        return icons[this.name]
+        return this.icons[this.name]
+      },
+      paths () {
+        return this.icon ? this.icon.paths : ''
       },
       box () {
-        return `0 0 ${this.icon.width} ${this.icon.height}`
+        return this.icon ? this.icon.viewBox : '0 0 0 0'
       },
       width () {
         return this.icon ? 20 * this.scale : 0
       },
       height () {
         return this.icon ? 20 * this.scale : 0
-      },
-      style () {
-        if (this.scale === 1) {
-          return false
-        }
-        return {
-          fontSize: this.scale + 'em'
-        }
       }
     },
-    register (custom) {
-      for (let name in custom) {
-        icons[name] = custom[name]
+    data () {
+      return {
+        icons: require('./icons.json')
       }
     }
   }
