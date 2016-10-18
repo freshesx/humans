7049318c3e5119e9b3bf3b10c23aea2a3f0fcb39
xxsue <suki.luux1@gmail.com>
2016-10-18 14:43:13 +0800

    update the sass

--- examples/docs/components/button/circle.vue
+++ examples/docs/components/button/circle.vue
@@ -6,8 +6,8 @@
     <div class="card-block">
       <mn-btn type="primary" circle>hi</mn-btn>
       <mn-btn type="warning" circle><mn-icon name="navicon-round"></mn-icon></mn-btn>
-      <mn-btn type="error" circle>error</mn-btn>
-      <mn-btn type="error" circle size="sm"><mn-icon name="arrow-shrink"></mn-icon></mn-btn>
+      <mn-btn type="error" circle><mn-icon name="bonfire"></mn-icon></mn-btn>
+      <mn-btn type="error" circle size="sm"><mn-icon name="bonfire"></mn-icon></mn-btn>
     </div>
   </div>
 </template>
