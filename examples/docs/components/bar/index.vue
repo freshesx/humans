bcc87592a659fb3966f6d3258c508dea92d588df
xxsue <suki.luux1@gmail.com>
2016-10-18 14:55:01 +0800

    add files and router

---
+++ examples/docs/components/bar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+
+    <div class="letter">
+      <div class="letter-body">
+        <h1>Bar</h1>
+        <small>btn, input</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
