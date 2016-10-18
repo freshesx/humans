b34db5cbd549b2f57df99e3121db9a5d0e87b7e8
xxsue <suki.luux1@gmail.com>
2016-10-18 11:33:04 +0800

    Update the example

--- examples/docs/components/button/outline.vue
+++ examples/docs/components/button/outline.vue
@@ -6,10 +6,10 @@
     <div class="card-block">
       <div class="cols">
         <div class="col is-sm-6">
-          <mn-btn type="primary" outline>Confirm</mn-btn>
+          <mn-btn type="primary" outline><mn-icon name="navicon-round"></mn-icon> Confirm</mn-btn>
+          <mn-btn type="warning" outline loading>Sure?</mn-btn>
+          <mn-btn type="error" outline loading block>Off</mn-btn>
           <mn-btn type="primary" outline size="sm">Confirm</mn-btn>
-          <mn-btn type="warning" outline size="sm">Sure?</mn-btn>
-          <mn-btn type="error" outline size="sm">Off</mn-btn>
         </div>
         <div class="col is-sm-6">
         </div>
