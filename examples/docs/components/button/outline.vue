3345a30b12a9675b13cdce556ac04532f1059d82
xxsue <suki.luux1@gmail.com>
2016-10-18 13:54:14 +0800

    Update the example

--- examples/docs/components/button/outline.vue
+++ examples/docs/components/button/outline.vue
@@ -4,16 +4,10 @@
       <div class="card-title">Outline Button</div>
     </div>
     <div class="card-block">
-      <div class="cols">
-        <div class="col is-sm-6">
-          <mn-btn type="primary" outline><mn-icon name="navicon-round"></mn-icon> Confirm</mn-btn>
-          <mn-btn type="warning" outline loading>Sure?</mn-btn>
-          <mn-btn type="error" outline loading block>Off</mn-btn>
-          <mn-btn type="primary" outline size="sm">Confirm</mn-btn>
-        </div>
-        <div class="col is-sm-6">
-        </div>
-      </div>
+      <mn-btn type="primary" outline><mn-icon name="navicon-round"></mn-icon> Confirm</mn-btn>
+      <mn-btn type="warning" outline loading>Sure?</mn-btn>
+      <mn-btn type="primary" outline size="sm">Confirm</mn-btn>
+      <mn-btn type="error" outline loading block>Off</mn-btn>
     </div>
   </div>
 </template>
