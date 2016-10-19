ca299a8c5fb881ccd67eefebe0984628f49d229a
xxsue <suki.luux1@gmail.com>
2016-10-19 17:39:48 +0800

    Update

--- components/bar/search.vue
+++ components/bar/search.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bar-search">
     <mn-icon name="ios-search-strong"></mn-icon>
-      <input type="text" @focus="show = true" @blur="show = false" placeholder="Search something">
+      <input type="text" @focus="show = true" @blur="show = false" @key.enter="" placeholder="Search something">
     <transition name="ani-bar-search">
-      <button @focus="show = false" v-if="show">cancel</button>
+      <button @click="show = false" v-if="show">cancel</button>
     </transition>
   </div>
 </template>
@@ -14,6 +14,12 @@
       return {
         show: false
       }
+    },
+    props: {
+
+    },
+    methods: {
+
     }
   }
 </script>
--- examples/docs/components/bar/search.vue
+++ examples/docs/components/bar/search.vue
@@ -3,12 +3,16 @@
     <div class="card-header">
       <div class="card-title">Search Bar</div>
     </div>
-    <mn-seachBar></mn-seachBar>
+    <mn-seachBar :keyEnter="search"></mn-seachBar>
   </div>
 </template>
 
 <script>
   export default {
-
+    methods: {
+      search: function () {
+        console.log(1)
+      }
+    }
   }
 </script>
