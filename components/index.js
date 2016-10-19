bbe6b95ba30bee6ab9ef6757cc942e03fe7b89e3
ChaselWu <minowu@foxmail.com>
2016-10-19 15:31:30 +0800

    Remove lodash, and add comments

--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
-import lodash from 'lodash'
 import components from './components'
 import element from './util/element'
 import popup from './popup'
@@ -6,26 +5,41 @@ import scroller from './scroller'
 
 export default {
   options: {
+    /**
+     * Vue human component prefix
+     * @type {String}
+     */
     prefix: 'Mn',
+    /**
+     * Human css prefix, using it in scss
+     * @type {String}
+     */
     cssPrefix: ''
   },
+  /**
+   * Vue install
+   * @param  {Object} Vue
+   * @param  {Object} options
+   */
   install (Vue, options) {
-    // Assign default & options
+    // Merge and assign options
     this.options = Object.assign({}, this.options, options)
 
-    // Using components
-    lodash.forEach(components, (component, name) => {
-      Vue.component(`${this.options.prefix}${name}`, component)
-    })
+    // Vue load components
+    for (let name in components) {
+      Vue.component(`${this.options.prefix}${name}`, components[name])
+    }
 
-    // Adding scroll save & set
+    // Set up vue human
     Vue.human = {
       prefix: this.options.prefix,
       cssPrefix: this.options.cssPrefix
     }
 
+    // Alias for human
     Vue.prototype.$human = Vue.human
 
+    // Using human plugins
     Vue.use(scroller)
     Vue.use(element)
     Vue.use(popup)
