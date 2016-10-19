8aa1956ef906e579e87568e67af789b4a88e21be
ChaselWu <minowu@foxmail.com>
2016-10-19 16:06:59 +0800

    Using popup instead of createElement

--- examples/docs/components/popup/index.vue
+++ examples/docs/components/popup/index.vue
@@ -10,19 +10,10 @@
 
     <div class="card">
       <div class="card-header">
-        <div class="card-title">Pure popup</div>
-      </div>
-      <div class="card-block">
-        <button class="btn is-primary is-block" @click.prevent="openPopup">Open popup</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <div class="card-header">
         <div class="card-title">Confirm extend popup</div>
       </div>
       <div class="card-block">
-        <button class="btn is-warning is-block" @click.prevent="openConfirm">Delete</button>
+        <mn-btn type="warning" block @click.native.prevent="openConfirm">Confirm</mn-btn>
       </div>
     </div>
 
@@ -31,7 +22,16 @@
         <div class="card-title">Alert extend popup</div>
       </div>
       <div class="card-block">
-        <button class="btn is-secondary is-block" @click.prevent="openAlert">Alert</button>
+        <mn-btn type="error" block @click.native.prevent="openAlert">Alert</mn-btn>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <div class="card-title">Custom popup (Activity sheets)</div>
+      </div>
+      <div class="card-block">
+        <mn-btn type="secondary" block @click.native.prevent="openPopup">Open activity sheets</mn-btn>
       </div>
     </div>
   </div>
@@ -43,7 +43,7 @@
   export default {
     methods: {
       openPopup () {
-        this.popup = this.$human.createElement(Popup)
+        this.popup = this.$human.popup(Popup, { show: true })
       },
       openConfirm () {
         const confirm = this.$human.confirm({ show: true })
