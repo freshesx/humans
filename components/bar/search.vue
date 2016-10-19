a9a5dc57b8c479ae95d86318edc046295cc8e575
xxsue <suki.luux1@gmail.com>
2016-10-19 17:11:48 +0800

    Add blur

--- components/bar/search.vue
+++ components/bar/search.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bar-search">
     <mn-icon name="ios-search-strong"></mn-icon>
-    <input @focus="show = true" type="text" class="bar-search-input" placeholder="Search something">
+      <input type="text" @focus="show = true" @blur="show = false" placeholder="Search something">
     <transition name="ani-bar-search">
-      <button @focus="show = false" v-if="show" class="bar-search-button is-error is-sm">cancel</button>
+      <button @focus="show = false" v-if="show">cancel</button>
     </transition>
   </div>
 </template>
