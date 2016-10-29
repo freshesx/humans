5bdb138062aaf1e279b7bfe8497bac2cb0b51be7
ChaselWu <minowu@foxmail.com>
2016-10-29 14:01:29 +0800

    Add a new template component

---
+++ components/template.vue
@@ -0,0 +1,17 @@
+<!-- This is just a component template, copy it to create new component -->
+
+<template>
+  <div :class="[cssPrefix + '-suffix']">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      cssPrefix () {
+        return `${this.$human.cssPrefix}`
+      }
+    }
+  }
+</script>
