f0547fc6bf1c47b4324c83947cc124d1dddd6849
ChaselWu <minowu@foxmail.com>
2016-10-30 01:16:12 +0800

    Rewrite searchbar

--- examples/docs/components/searchbar/index.vue
+++ examples/docs/components/searchbar/index.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="section">
-    <div class="letter">
-      <div class="letter-body">
-        <h1>Search Bar</h1>
-        <small>btn, input</small>
-      </div>
-    </div>
-
+    <mn-letter>
+      <h1>Search Bar</h1>
+      <small>btn, input</small>
+    </mn-letter>
     <demo-search-bar></demo-search-bar>
-
   </div>
 </template>
 
