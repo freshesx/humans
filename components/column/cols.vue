bfe2bf6c6c6de6291970d0f11dab3e0fe9e6177c
ChaselWu <minowu@foxmail.com>
2016-10-29 14:32:20 +0800

    Add gapless class to cols

--- components/column/cols.vue
+++ components/column/cols.vue
@@ -1,14 +1,23 @@
 <template>
-  <div :class="[cssPrefix + 'cols']">
+  <div :class="[cssPrefix + 'cols', gaplessClass]">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      gapless: {
+        type: Boolean,
+        default: false
+      }
+    },
     computed: {
       cssPrefix () {
         return `${this.$human.cssPrefix}`
+      },
+      gaplessClass () {
+        return { 'is-gapless': this.gapless }
       }
     }
   }
