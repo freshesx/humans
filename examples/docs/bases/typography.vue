a47618fbd987754bdb5a87f845256939335cbd44
ChaselWu <minowu@foxmail.com>
2016-10-29 19:41:50 +0800

    Rewrite headings

--- examples/docs/bases/typography.vue
+++ examples/docs/bases/typography.vue
@@ -2,51 +2,43 @@
   <transition name="has-slide">
     <div class="section">
 
-      <div class="letter">
-        <div class="letter-body">
-          <h1>Typograhpy</h1>
-          <small>Headings, paragraph, list, etc.</small>
-        </div>
-      </div>
+      <mn-letter>
+        <h1>Typograhpy</h1>
+        <small>Headings, paragraph, list, etc.</small>
+      </mn-letter>
 
-      <div class="card">
-        <div class="card-block">
-          <div class="card-header">
-            <h3 class="card-title">Headings</h3>
-          </div>
-          <div class="card-lists">
-            <div class="card-item">
-              <div class="card-item-body"><h1>h1</h1></div>
-              <div class="card-item-help"><small>size: 36px, weight: 200</small></div>
-            </div>
-            <div class="card-item">
-              <div class="card-item-body"><h2>h2</h2></div>
-              <div class="card-item-help"><small>size: 30px, weight: 300</small></div>
-            </div>
-            <div class="card-item">
-              <div class="card-item-body"><h3>h3</h3></div>
-              <div class="card-item-help"><small>size: 24px, weight: 400</small></div>
-            </div>
-            <div class="card-item">
-              <div class="card-item-body"><h4>h4</h4></div>
-              <div class="card-item-help"><small>size: 18px, weight: 400</small></div>
-            </div>
-            <div class="card-item">
-              <div class="card-item-body"><h5>h5</h5></div>
-              <div class="card-item-help"><small>size: 16px, weight: 600</small></div>
-            </div>
-            <div class="card-item">
-              <div class="card-item-body"><h6>h6</h6></div>
-              <div class="card-item-help"><small>size: 14px, weight: 700</small></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <mn-card>
+        <mn-card-item>
+          <h3 class="card-title">Headings</h3>
+        </mn-card-item>
+        <mn-card-item v-for="content in contents">
+          <demo-heading slot="body" :level="content.level">{{ content.title }}</demo-heading>
+          <small slot="help">{{ content.description }}</small>
+        </mn-card-item>
+      </mn-card>
+
     </div>
   </transition>
 </template>
 
 <script>
+  import DemoHeading from './heading'
+
   export default {
+    components: {
+      DemoHeading
+    },
+    data () {
+      return {
+        contents: [
+          { level: 1, title: 'h1', description: 'size: 36px, weight: 200' },
+          { level: 2, title: 'h2', description: 'size: 30px, weight: 300' },
+          { level: 3, title: 'h3', description: 'size: 24px, weight: 400' },
+          { level: 4, title: 'h4', description: 'size: 18px, weight: 400' },
+          { level: 5, title: 'h5', description: 'size: 16px, weight: 600' },
+          { level: 6, title: 'h6', description: 'size: 14px, weight: 700' }
+        ]
+      }
+    }
   }
 </script>
