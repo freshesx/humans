d374eb3989c3351ef8f3a0c0c793a03a2c6b320b
ChaselWu <minowu@foxmail.com>
2016-10-29 14:19:49 +0800

    Add query to col

--- components/column/col.vue
+++ components/column/col.vue
@@ -1,14 +1,30 @@
 <template>
-  <div :class="[cssPrefix + 'col']">
+  <div :class="[cssPrefix + 'col', queryClass]">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      query: {
+        type: String,
+        default: ''
+      }
+    },
     computed: {
       cssPrefix () {
         return `${this.$human.cssPrefix}`
+      },
+      queryClass () {
+        let queries = this.query.split(' ')
+        let classes = []
+
+        queries.forEach(item => {
+          classes.push({ [`is-${item}`]: true })
+        })
+
+        return classes
       }
     }
   }
