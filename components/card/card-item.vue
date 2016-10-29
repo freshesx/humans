6d319d59aca961ec2d48785358e8f03730fcaf58
ChaselWu <minowu@foxmail.com>
2016-10-29 11:28:11 +0800

    Add slot default

--- components/card/card-item.vue
+++ components/card/card-item.vue
@@ -3,8 +3,9 @@
     <div :class="[cssPrefix + '-addon']" v-if="$slots.addon">
       <slot name="addon"></slot>
     </div>
-    <div :class="[cssPrefix + '-body']" v-if="$slots.body">
-      <slot name="body"></slot>
+    <div :class="[cssPrefix + '-body']" v-if="$slots.body || $slots.default">
+      <slot name="body"></slot>   <!-- Slot for name -->
+      <slot></slot>               <!-- Slot for default -->
     </div>
     <div :class="[cssPrefix + '-action']" v-if="$slots.action">
       <slot name="action"></slot>
