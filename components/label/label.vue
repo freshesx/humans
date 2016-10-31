4515f5ed709e33adf9aeba588a2667bd70671398
xxsue <suki.luux1@gmail.com>
2016-10-31 14:40:56 +0800

    Add labels.

--- components/label/label.vue
+++ components/label/label.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="">
-    1
-  </div>
+  <i :class="classes">
+    <slot></slot>
+  </i>
 </template>
 
 <script>
   export default {
-
+    data () {
+      return {
+        defaultClasses: this.$human.cssPrefix
+      }
+    },
+    computed: {
+      classes () {
+        let classes = {}
+        // basic class
+        classes[`${this.$human.cssPrefix}label`] = true
+        // type
+        classes[`is-${this.type}`] = this.type
+        // return default and now classes
+        return Object.assign({}, classes)
+      }
+    },
+    props: {
+      // is-primary, is-secondary, is-error etc.
+      type: {
+        type: String
+      }
+    }
   }
 </script>
--- examples/docs/components/label/label.vue
+++ examples/docs/components/label/label.vue
@@ -1,5 +1,22 @@
 <template>
-  <mn-label></mn-label>
+  <mn-card>
+    <mn-card-item>
+      <h5 slot="body">Labels with text</h5>
+      <mn-label type="primary">标签一</mn-label>
+      <mn-label type="warning">标签二</mn-label>
+      <mn-label type="error">标签三</mn-label>
+      <mn-label type="secondary">标签四</mn-label>
+      <mn-label type="inverse">标签五</mn-label>
+    </mn-card-item>
+    <mn-card-item>
+      <h5 slot="body">Labels with icon</h5>
+      <mn-label type="primary"><mn-icon name="pricetags"></mn-icon></mn-label>
+      <mn-label type="warning"><mn-icon name="pricetags"></mn-icon></mn-label>
+      <mn-label type="error"><mn-icon name="pricetags"></mn-icon></mn-label>
+      <mn-label type="secondary"><mn-icon name="pricetags"></mn-icon></mn-label>
+      <mn-label type="inverse"><mn-icon name="pricetags"></mn-icon></mn-label>
+    </mn-card-item>
+  </mn-card>
 </template>
 
 <script>
@@ -7,3 +24,9 @@
 
   }
 </script>
+
+<style scoped>
+  h5{
+    margin-bottom: 20px;
+  }
+</style>
