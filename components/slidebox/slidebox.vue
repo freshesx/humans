a37cf847433923dc442b1954f45f0613de26fdee
xxsue <suki.luux1@gmail.com>
2016-11-03 16:52:47 +0800

    Add swipe and add some notes

--- components/slidebox/slidebox.vue
+++ components/slidebox/slidebox.vue
@@ -47,21 +47,40 @@
       return {
         defaultClasses: this.$human.cssPrefix,
         startPosition: 0,
+        // Start positon depends on  on-touchstart event.
         CurrentPosition: 0,
+        // Current position depends on on-touchmove event.
         lastPosition: 0,
+        // The last position.
         ifTouched: false,
+        // If touched or not
         direction: 'middle',
+        // The direction of touch events.
         distance: 0,
+        // Counting the distance for transform.
         lastDistance: 0,
+        // The last distance.
         slideBack: false,
+        // Trigger the animation or not.
         maxSlideValue: 0,
-        multiple: 10
+        // The max value of slide.
+        startTime: 0,
+        // Start time depends on on-touchstart event.
+        finishTime: 0,
+        // Finish time depends on on-touchend event.
+        swipeDuration: 300,
+        // Default swipe duration
+        swipeDistance: 150,
+        // Default swipe distance
+        swipeMultiple: 5
+        // Default swipe multiple
       }
     },
     methods: {
       moveEvent: function (event) {
         this.CurrentPosition = event.touches[0].screenX
         this.direction = (this.CurrentPosition < this.lastPosition) ? 'left' : 'right'
+        // Judge the direction.
 
         if (!this.ifTouched) {
           this.distance = this.CurrentPosition - this.startPosition
@@ -69,6 +88,9 @@
         } else {
           this.distance = this.CurrentPosition - this.startPosition + this.lastDistance
         }
+        // Counting the distance.
+        // If it is the first touch, return distance.
+        // If touched, return distance + last distance
 
         if (this.distance > 0) {
           this.distance = this.distance / 10
@@ -82,23 +104,34 @@
       startEvent: function (event) {
         this.startPosition = event.touches[0].screenX
         this.lastPosition = this.startPosition
+
+        this.startTime = new Date().getTime()
       },
       endEvent: function (event) {
+        this.finishTime = new Date().getTime()
+        let duration = this.finishTime - this.startTime
+        let distance = Math.abs(this.CurrentPosition - this.startPosition)
+
+        if (duration < this.swipeDuration && distance > this.swipeDistance) {
+          if (this.direction === 'left') {
+            this.distance += this.distance * this.swipeMultiple
+          } else {
+            this.distance -= this.distance * this.swipeMultiple
+          }
+        }
+
         if (this.direction === 'left') {
-          if (this.distance < this.maxSlideValue || this.distance * this.multiple < this.maxSlideValue) {
+          if (this.distance < this.maxSlideValue) {
             this.distance = this.maxSlideValue
             this.lastDistance = this.maxSlideValue
-          } else {
-            this.distance = this.distance * this.multiple
           }
         } else {
           if (this.distance > 0) {
             this.distance = 0
             this.lastDistance = 0
-          } else {
-            this.distance = this.distance / this.multiple
           }
         }
+        this.distance = this.distance
 
         this.ifTouched = true
         this.lastDistance = this.distance
