0c5351ce1a7cff533cca26c0c90290648cf4884d
ChaselWu <minowu@foxmail.com>
2016-11-07 15:48:45 +0800

    Set example

--- examples/docs/components/form/select.vue
+++ examples/docs/components/form/select.vue
@@ -4,26 +4,29 @@
     <mn-card slot="body">
       <mn-card-item type="arrow">
         <label slot="addon" class="form-label">Language</label>
-        <mn-form-select slot="body" :options="options" v-model="data"></mn-form-select>
+        <mn-form-select slot="body" :options="options" v-model="model.language"></mn-form-select>
       </mn-card-item>
     </mn-card>
   </mn-card-wrapper>
 </template>
 
 <script>
-  const options = [{
-    label: 'select language', value: null
-  }, {
-    label: 'English', value: 'en-US'
-  }, {
-    label: '简体中文', value: 'zh-CN'
-  }]
-
   export default {
+    watch: {
+      'model.language' () {
+        console.log(this.model)
+      }
+    },
     data () {
       return {
-        options,
-        data: null
+        options: [
+          { label: 'select language', value: null },
+          { label: 'English', value: 'en-US' },
+          { label: '简体中文', value: 'zh-CN' }
+        ],
+        model: {
+          language: undefined
+        }
       }
     }
   }
