c1628d79c6df191d0dae38be873c5907a1826491
ChaselWu <minowu@foxmail.com>
2016-11-07 15:18:51 +0800

    Receive for v-model

--- components/form/select.vue
+++ components/form/select.vue
@@ -1,5 +1,5 @@
 <template>
-  <select class="form-select-control">
+  <select class="form-select-control" :value="value" @input="onInput">
     <option :value="option.value" v-for="option in options">{{ option.label }}</option>
   </select>
 </template>
@@ -7,11 +7,17 @@
 <script>
   export default {
     props: {
+      value: {},
       options: {
         type: Array,
         default: () => {
           return []
         }
+      }
+    },
+    methods: {
+      onInput (event) {
+        this.$emit('input', event.target.value)
       }
     }
   }
