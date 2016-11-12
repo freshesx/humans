3bec0df7bded43acec4b034c491b0e88a800dfe1
ChaselWu <minowu@foxmail.com>
2016-11-12 14:33:06 +0800

    Add example

---
+++ examples/docs/components/filter-bar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <mn-filter-bar shadow>
+    <mn-filter-bar-item arrow active>
+      综合排序
+    </mn-filter-bar-item>
+    <mn-filter-bar-item>
+      销量优先
+    </mn-filter-bar-item>
+    <mn-filter-bar-item @btn-click="changeView">
+      <mn-icon name="ios-grid-view"></mn-icon>
+    </mn-filter-bar-item>
+    <mn-filter-bar-item bordered>
+      筛选 <mn-icon name="ios-settings"></mn-icon>
+    </mn-filter-bar-item>
+  </mn-filter-bar>
+</template>
+
+<script>
+  export default {
+    methods: {
+      changeView () {
+        console.log('changeView')
+      }
+    }
+  }
+</script>
