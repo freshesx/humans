e285e9ac3d697e2359dd2e9d786026bbca05c049
ChaselWu <minowu@foxmail.com>
2016-11-15 15:19:02 +0800

    WeChat scroller bug

--- components/scroller/scroller.vue
+++ components/scroller/scroller.vue
@@ -1,166 +1,48 @@
 <template>
-  <div class="scrollbar-wrapper"
-    :class="{ 'is-save': save }"
-    @touchstart="startEvent($event)"
-    @touchmove="moveEvent($event)"
-    @touchend="endEvent($event)"
-    @wheel="wheelEvent($event)">
-    <div class="scrollbar-top-content" ref="topContent" :style="styles">
-      <slot name="topContent"><p>正在刷新</p></slot>
-    </div>
-    <div class="scrollbar-contents" ref="content">
+  <div
+    :class="[ `${cssPrefix}scroller`, { 'is-save': save } ]"
+    @touchstart="touchStart"
+    @touchmove="touchMove">
+    <div class="scroller-contents">
       <slot></slot>
     </div>
-    <button
-      type="button"
-      name="button"
-      v-if="this.bottom"
-      class="scrollbar-bottom-button"
-      @click="buttonClick"
-    >{{ this.buttonText }}</button>
-    <transition name="scroller-slideUp">
-      <button
-        type="button"
-        name="button"
-        class="scrollbar-to-top"
-        @click="scrollTop"
-        v-if="isScrollTop">
-        <mn-icon name="arrow-up-a">
-      </button>
-    </transition>
   </div>
 </template>
 
 <script>
+  import $ from 'jquery'
+
   export default {
     props: {
       save: {
         type: Boolean,
         default: false
-      },
-      bottom: {
-        type: Boolean,
-        default: false
-        // whether to show the bottom button
-      },
-      buttonText: {
-        type: String,
-        default: 'Loading...'
-        // the text on the button
-      },
-      scrollToTop: {
-        type: Boolean,
-        default: false
-        // whether to provide the function of scrolling to top
-      },
-      pause: {
-        type: Boolean,
-        default: false
-        // whether to pause the top content for some asynchronous requests
-      }
-    },
-    data () {
-      return {
-        isEnd: false,
-        // whether the page has reached the bottom
-        isStart: false,
-        // whether the page has reached the top
-        startPosition: 0,
-        // the start position
-        lastPosition: 0,
-        // the last position
-        distance: 0,
-        // the distance for styles
-        gesture: '',
-        // the gesture
-        // including: up & down
-        isScrollTop: false,
-        // whether to show the scroll to top button
-        duration: 200
-        // default duration for scrolling
-      }
-    },
-    methods: {
-      startEvent: function (event) {
-        this.isEnd = false
-        this.isStart = false
-        // Reset the data.
-        this.startPosition = event.touches[0].pageY
-        this.lastPosition = event.touches[0].pageY
-      },
-      moveEvent: function (event) {
-        this.startPosition = event.touches[0].pageY
-        this.gesture = (this.startPosition < this.lastPosition) ? 'up' : 'down'
-        // Set the gesture direction.
-
-        if (this.gesture === 'up' && this.$el.scrollTop > this.$el.clientHeight && this.scrollToTop) this.isScrollTop = true
-        if (this.gesture === 'down' && this.$el.scrollTop <= this.$el.clientHeight && this.scrollToTop) this.isScrollTop = false
-        // Determine whether to show the scrollToTop button
-
-        if (this.$el.scrollTop >= this.$el.scrollHeight - this.$el.clientHeight && !this.isEnd && this.gesture === 'up') {
-          this.$emit('pull-up')
-          this.isEnd = true
-          // Determine whether the page has reached the bottom
-          // If reached, trigger the 'pull-up'
-        }
-
-        if (this.$el.scrollTop === 0 && this.gesture === 'down') {
-          this.distance++
-          if (!this.isStart) this.isStart = true
-          // Determine whether the page has reached the top
-        }
-        this.lastPosition = this.startPosition
-      },
-      endEvent: function (event) {
-        if (this.isStart) this.$emit('drag-down', this.distance)
-        //
-        // Trigger the 'drag-down' when the fingers are released
-        // Provide the 'distance'
-        //
-        this.startPosition = 0
-        this.lastPosition = 0
-        this.distance = 0
-        this.isStart = false
-        // Reset the data.
-      },
-      wheelEvent: function (event) {
-        if (event.deltaY > 0 && this.$el.scrollTop >= this.$el.scrollHeight - this.$el.clientHeight && !this.isEnd) {
-          this.isEnd = true
-          this.$emit('pull-up')
-          // Determine whether the page has reached the bottom
-          // If reached, trigger the 'pull-up'
-        }
-        if (event.deltaY < 0) this.isEnd = false
-        if (event.deltaY > 0) this.isStart = false
-
-        if (event.deltaY > 0 && this.$el.scrollTop > this.$el.clientHeight && this.scrollToTop) this.isScrollTop = true
-        if (event.deltaY < 0 && this.$el.scrollTop <= this.$el.clientHeight && this.scrollToTop) this.isScrollTop = false
-        // Determine whether to show the scrollToTop button
-      },
-      buttonClick: function () {
-        this.$emit('button-click')
-        // Trigger the 'button-click' when you click the button
-      },
-      scrollTop: function () {
-        this.scrolling(this.duration)
-        this.isScrollTop = false
-        // Scroll to top
-      },
-      scrolling: function (duration) {
-        let scrollStep = this.$el.scrollTop / (duration / 15)
-        let scrollInterval = setInterval(() => {
-          if (this.$el.scrollTop !== 0) this.$el.scrollTop -= scrollStep
-          else clearInterval(scrollInterval)
-        }, 15)
-        // linear scrolling
       }
     },
     computed: {
-      styles () {
-        if (this.pause) return 'margin-top: 0px'
-        if (this.distance >= 0) return `margin-top: ${this.distance * 2 - 50}px`
-        return 'margin-top: -50px'
-        // If drag down, trigger some styles.
+      cssPrefix () {
+        return this.$human.cssPrefix
+      }
+    },
+    methods: {
+      touchStart (event) {
+        this.startPageY = event.touches[0].pageY
+      },
+      touchMove (event) {
+        let pageY = event.touches[0].pageY
+        let scrollTop = $(this.$el).scrollTop()
+        let scrollHeight = this.$el.scrollHeight
+        let containerHeight = $(this.$el).height()
+
+        // 上面露底
+        if (pageY > this.startPageY && scrollTop <= 0) {
+          event.preventDefault()
+        }
+
+        // 下面露底
+        if (pageY < this.startPageY && scrollTop >= scrollHeight - containerHeight) {
+          event.preventDefault()
+        }
       }
     }
   }
