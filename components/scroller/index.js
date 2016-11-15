9ec8a9978ffd9cd93ac7cd82b31ddf1c4da911bb
ChaselWu <minowu@foxmail.com>
2016-11-15 15:46:55 +0800

    Fix scroll

--- components/scroller/index.js
+++ components/scroller/index.js
@@ -1,37 +1,62 @@
 import $ from 'jquery'
 
-/**
- * Save scroll from vue route from
- *
- * @param from
- */
-export function saveScroll (from) {
-  if (from.path && from.meta.scroll) {
-    const $scroll = $('.scrollbar-wrapper.is-save')
-    let storage = $scroll.data('scroll') || {}
-    storage[from.path] = $scroll.scrollTop()
-    $scroll.data('scroll', storage)
+class Scroller {
+  constructor () {
+    this.storages = []
+  }
+
+  addStorage (name, value) {
+    let filterStorage = this.storages.filter(item => item.name === name)
+
+    filterStorage.length > 0
+      ? filterStorage[0] = value
+      : this.storages.push({ name, value })
+  }
+
+  getStorage (name) {
+    let filterStorage = this.storages.filter(item => item.name === name)
+
+    return filterStorage.length > 0
+      ? filterStorage[0].value
+      : 0
+  }
+
+  /**
+   * Save scroll from vue route from
+   *
+   * @param from
+   */
+  saveScroll (from) {
+    if (from.path && from.meta.scroll) {
+      const $scroll = $('.scroller.is-save')
+      this.addStorage(from.fullPath, $scroll.scrollTop())
+    }
+  }
+
+  /**
+   * set scroll from storage
+   *
+   * @param to
+   */
+  setScroll (to) {
+    let value = this.getStorage(to.fullPath)
+    setTimeout(() => {
+      const $scroll = $('.scroller.is-save')
+      $scroll.scrollTop(value)
+    }, 0)
   }
 }
 
-/**
- * set scroll from storage
- *
- * @param to
- */
-export function setScroll (to) {
-  setTimeout(() => {
-    const $scroll = $('.scrollbar-wrapper.is-save')
-    const storage = $scroll.data('scroll')
-    const top = storage ? (storage[to.path] || 0) : 0
-
-    $scroll.scrollTop(top)
-  }, 0)
-}
+const scroller = new Scroller()
 
 export default {
   install (Vue) {
-    Vue.human.saveScroll = saveScroll
-    Vue.human.setScroll = setScroll
+    Vue.human.saveScroll = from => {
+      scroller.saveScroll(from)
+    }
+
+    Vue.human.setScroll = to => {
+      scroller.setScroll(to)
+    }
   }
 }
