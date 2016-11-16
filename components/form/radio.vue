5532dc93a28097b32aa905c91fb9e5045b28c30b
ChaselWu <minowu@foxmail.com>
2016-11-16 15:53:28 +0800

    Add radio component

---
+++ components/form/radio.vue
@@ -0,0 +1,32 @@
+<template>
+  <mn-card>
+    <mn-card-item v-for="option in options" @click.native.prevent="onClick($event, option.value)">
+      <label slot="body" class="form-label">{{ option.label }}</label>
+      <template slot="action">
+        <mn-icon :class="[`${cssPrefix}form-radio-checkmark`]" name="ios-checkmark-empty" :scale="2" v-if="option.value === value"></mn-icon>
+      </template>
+    </mn-card-item>
+  </mn-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {},
+      options: {
+        type: Array,
+        required: true
+      }
+    },
+    computed: {
+      cssPrefix () {
+        return this.$human.cssPrefix
+      }
+    },
+    methods: {
+      onClick (event, value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
