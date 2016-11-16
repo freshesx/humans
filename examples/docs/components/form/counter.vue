58d361162c16ef905d9aea34b1b3976bc60a908a
ChaselWu <minowu@foxmail.com>
2016-11-16 18:32:15 +0800

    Change cols

--- examples/docs/components/form/counter.vue
+++ examples/docs/components/form/counter.vue
@@ -3,11 +3,12 @@
     <strong slot="header">Input Number</strong>
     <mn-card slot="body">
       <mn-card-item>
-        <template slot="body">
-          nihao
-        </template>
-        <mn-form-counter :min="3" :max="16" :step="2" @change="change">
-        </mn-form-counter>
+        <mn-cols slot="body">
+          <mn-col query="xs-4">nihao</mn-col>
+          <mn-col query="xs-6">
+            <mn-form-counter :min="3" :max="16" :step="2" @change="change"></mn-form-counter>
+          </mn-col>
+        </mn-cols>
       </mn-card-item>
     </mn-card>
   </mn-card-wrapper>
