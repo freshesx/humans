ec1d9e0337fd8ce0e20f8f30bdaac42fc6c2340f
ChaselWu <minowu@foxmail.com>
2016-11-25 14:16:55 +0800

    Using form-radio-card

--- examples/docs/components/filter-bar/sort-panel.vue
+++ examples/docs/components/filter-bar/sort-panel.vue
@@ -1,20 +1,6 @@
 <template>
   <mn-popup :show="show" animation="slideInUp" @close="cancel" :classes="['filter-bar-popup']" :zIndex="zIndex">
-    <mn-card :class="[ 'm-b-0', 'filter-bar-card' ]">
-      <mn-card-item>
-        <template slot="addon">综合排序</template>
-        <input slot="body" class="form-radio-control" type="radio" value="smart" v-model="sortWay">
-      </mn-card-item>
-      <mn-card-item arrow>
-        信用
-      </mn-card-item>
-      <mn-card-item arrow>
-        价格由高到低
-      </mn-card-item>
-      <mn-card-item arrow>
-        价格由低到高
-      </mn-card-item>
-    </mn-card>
+    <mn-form-radio-card :class="[ 'm-b-0', 'filter-bar-card' ]" :options="filterOptions" v-model="filters"></mn-form-radio-card>
   </mn-popup>
 </template>
 
@@ -29,7 +15,13 @@
       return {
         show: false,
         zIndex: 4000,
-        sortWay: 'smart'
+        filterOptions: [
+          { label: '综合排序', value: 'smart' },
+          { label: '信用', value: 'credit' },
+          { label: '价格由高到低', value: 'desc' },
+          { label: '价格由低到高', value: 'asc' }
+        ],
+        filters: 'smart'
       }
     }
   }
