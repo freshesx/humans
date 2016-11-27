5907e9dfdb64f2ef2cd64110e3c151cc9885731f
ChaselWu <minowu@foxmail.com>
2016-11-28 00:53:58 +0800

    Add tab-bar

--- examples/components/home.vue
+++ examples/components/home.vue
@@ -20,6 +20,18 @@
         </mn-section>
       </mn-scroller>
     </mn-row>
+    <mn-row fixed>
+      <mn-tab-bar>
+        <mn-tab-pane title="Bases" icon="ios-home">
+        </mn-tab-pane>
+        <mn-tab-pane title="Core" icon="ios-albums-outline">
+        </mn-tab-pane>
+        <mn-tab-pane title="More" icon="ios-cloud-outline">
+        </mn-tab-pane>
+        <mn-tab-pane title="Github" icon="ios-people">
+        </mn-tab-pane>
+      </mn-tab-bar>
+    </mn-row>
   </mn-rows>
 </template>
 
