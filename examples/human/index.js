db60f03303cf3c29730b51d3f476bff38c9b39c0
ChaselWu <minowu@foxmail.com>
2016-11-27 22:23:53 +0800

    Set human docs

---
+++ examples/human/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueHuman from '../../components/index.js'
+// import resolveLoadingIcon from './resolve-loading-icon'
+import './styles.scss'
+
+Vue.use(VueHuman, {
+  // Using custom icon
+  // loading: resolveLoadingIcon
+})
