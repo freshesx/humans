0d5d095ff62ef53ba9cafcbe5a23afa425f4ab1c
ChaselWu <minowu@foxmail.com>
2016-11-28 15:49:30 +0800

    Using page layout

--- examples/components/colors.vue
+++ examples/components/colors.vue
@@ -1,53 +1,36 @@
 <template>
-  <transition name="has-slide">
-    <mn-rows>
-      <mn-row fixed>
-        <mn-nav-bar>
-          <div slot="prefix" @click="$router.go(-1)">
-            <mn-icon name="ios-arrow-back"></mn-icon>Back
-          </div>
-          <div slot="body">
-            <mn-icon name="ios-color-filter"></mn-icon>
-            Color
-          </div>
-        </mn-nav-bar>
-      </mn-row>
-      <mn-row>
-        <mn-scroller>
-          <mn-section>
-            <mn-letter>
-              <h1>Color</h1>
-              <small>Text, background, other.</small>
-            </mn-letter>
+  <docs-layout-page>
+    <mn-section>
+      <mn-letter>
+        <h1>Color</h1>
+        <small>Text, background, other.</small>
+      </mn-letter>
 
-            <mn-card>
-              <mn-card-item>Color schema</mn-card-item>
-              <mn-card-item>
-                <mn-cols>
-                  <mn-col query="xs-4" v-for="color in colors">
-                    <div class="sp-tile" :class="getBoxClass(color.className)"></div>
-                  </mn-col>
-                </mn-cols>
-              </mn-card-item>
-            </mn-card>
+      <mn-card>
+        <mn-card-item>Color schema</mn-card-item>
+        <mn-card-item>
+          <mn-cols>
+            <mn-col query="xs-4" v-for="color in colors">
+              <div class="sp-tile" :class="getBoxClass(color.className)"></div>
+            </mn-col>
+          </mn-cols>
+        </mn-card-item>
+      </mn-card>
 
-            <mn-card>
-              <mn-card-item>Color schema</mn-card-item>
-              <mn-card-item>
-                <mn-cols>
-                  <mn-col query="xs-4" v-for="color in colors">
-                    <div class="sp-text" :class="getTextClass(color.className)">
-                      {{ color.className }}
-                    </div>
-                  </mn-col>
-                </mn-cols>
-              </mn-card-item>
-            </mn-card>
-          </mn-section>
-        </mn-scroller>
-      </mn-row>
-    </mn-rows>
-  </transition>
+      <mn-card>
+        <mn-card-item>Color schema</mn-card-item>
+        <mn-card-item>
+          <mn-cols>
+            <mn-col query="xs-4" v-for="color in colors">
+              <div class="sp-text" :class="getTextClass(color.className)">
+                {{ color.className }}
+              </div>
+            </mn-col>
+          </mn-cols>
+        </mn-card-item>
+      </mn-card>
+    </mn-section>
+  </docs-layout-page>
 </template>
 
 <script>
