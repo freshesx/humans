59d21358f9d37712f090076f0a9f1c785d2d244f
ChaselWu <minowu@foxmail.com>
2016-11-28 15:36:11 +0800

    Using article component

--- examples/components/card/card-media.vue
+++ examples/components/card/card-media.vue
@@ -15,12 +15,12 @@
       <mn-btn class="is-primary is-block">Submit</mn-btn>
     </mn-card-item>
     <mn-card-item>
-      <article>
+      <mn-article>
         <h3>What is Lorem Ipsum?</h3>
         <p>
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
         </p>
-      </article>
+      </mn-article>
     </mn-card-item>
   </mn-card>
 
