18aa2e4e86ebcf861c5823609c19c89857edcb94
ChaselWu <minowu@foxmail.com>
2016-11-28 15:49:23 +0800

    Add page layout

---
+++ examples/components/layout/page.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="has-slide">
+    <mn-rows>
+      <mn-row fixed>
+        <mn-nav-bar>
+          <div slot="prefix" @click="$router.go(-1)">
+            <mn-icon name="ios-arrow-back"></mn-icon>Back
+          </div>
+          <div slot="body">
+            <mn-icon :name="icon"></mn-icon>
+            {{ title }}
+          </div>
+        </mn-nav-bar>
+      </mn-row>
+      <mn-row>
+        <mn-scroller>
+          <slot></slot>
+        </mn-scroller>
+      </mn-row>
+    </mn-rows>
+  </transition>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'Human UI'
+      },
+      icon: {
+        type: String,
+        default: 'ios-color-filter'
+      }
+    }
+  }
+</script>
--- examples/human/index.js
+++ examples/human/index.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import VueHuman from '../../components/index.js'
 // import resolveLoadingIcon from './resolve-loading-icon'
 import './styles.scss'
+import DocsLayoutPage from '../components/layout/page'
 
 Vue.use(VueHuman, {
   // Using custom icon
   // loading: resolveLoadingIcon
 })
+
+// Using page layout
+Vue.component('DocsLayoutPage', DocsLayoutPage)
