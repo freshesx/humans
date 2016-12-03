44b6827c7e2e93ca2ab56f675719831abe60b0df
ChaselWu <minowu@foxmail.com>
2016-12-03 16:04:08 +0800

    Example

--- examples/components/button/outline.vue
+++ examples/components/button/outline.vue
@@ -2,10 +2,10 @@
   <mn-card>
     <mn-card-item>Outline Button</mn-card-item>
     <mn-card-item>
-      <mn-btn type="primary" outline><mn-icon name="navicon-round"></mn-icon> Confirm</mn-btn>
-      <mn-btn type="warning" outline loading>Warning</mn-btn>
-      <mn-btn type="primary" outline size="sm">Confirm</mn-btn>
-      <mn-btn type="error" outline block loading>Error</mn-btn>
+      <mn-btn class="m-b-1" type="primary-outline"><mn-icon name="navicon-round"></mn-icon> Confirm</mn-btn>
+      <mn-btn class="m-b-1" type="warning-outline" loading>Warning</mn-btn>
+      <mn-btn class="m-b-1" type="primary-outline" size="sm">Confirm</mn-btn>
+      <mn-btn class="m-b-1" type="error-outline" block loading>Error</mn-btn>
     </mn-card-item>
   </mn-card>
 </template>
