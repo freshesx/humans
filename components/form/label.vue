5b1fb5d59d8193ccb54e527328d4b2d1720fe28c
ChaselWu <minowu@foxmail.com>
2016-12-06 17:23:22 +0800

    Using validation in label

--- components/form/label.vue
+++ components/form/label.vue
@@ -1,14 +1,20 @@
 <template>
-  <label :class="[ `${cssPrefix}form-label` ]  ">
+  <label :class="[ `${cssPrefix}form-label`, { 'is-error': isInvalid } ]">
     <slot></slot>
   </label>
 </template>
 
 <script>
   export default {
+    props: {
+      validate: Array
+    },
     computed: {
       cssPrefix () {
         return this.$human.cssPrefix
+      },
+      isInvalid () {
+        return this.validate && this.validate.length > 0
       }
     }
   }
