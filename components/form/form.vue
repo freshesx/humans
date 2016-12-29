74343ceaf9da2660e00e2fb56ca31e33739ec77b
ChaselWu <minowu@foxmail.com>
2016-12-29 18:08:48 +0800

    Simplify form component

--- components/form/form.vue
+++ components/form/form.vue
@@ -1,75 +1,21 @@
 <template>
-  <form @submit.prevent="submit" novalidate>
+  <form @submit="submit" novalidate>
     <slot></slot>
   </form>
 </template>
 
 <script>
-  import Schema from 'async-validator'
-
   export default {
-    props: {
-      value: Object,
-      models: Object,
-      rules: Object
-    },
     data () {
       return {
-        form: {
-          $loading: false,
-          $valid: false,
-          $checked: false
-        }
-      }
-    },
-    watch: {
-      models: {
-        deep: true,
-        handler () {
-          // After first submit
-          if (this.form.$checked) {
-            this.validate().then().catch()
-          }
-        }
+        loading: false
       }
     },
     methods: {
-      submit () {
-        // First using submit method, set checked true
-        this.form.$checked = true
-
-        // Validate and emit
-        this.validate().then(() => {
-          this.$emit('submit', true)
-          this.$emit('success')
-        }).catch(() => {
-          this.$emit('submit', false)
-          this.$emit('fail')
-        })
-      },
-      validate () {
-        return new Promise((resolve, reject) => {
-          // Before validate
-          this.form.$loading = true
-          // this.form.$checked = true
-          this.$emit('input', this.form)
-
-          // Build validate schema
-          const validator = new Schema(this.rules)
-
-          // Async validate
-          validator.validate(this.models, (errors, form) => {
-            this.form.$valid = !!errors
-            this.form.$loading = false
-            this.$emit('input', { ...this.form, ...form })
-            errors ? reject(form) : resolve(form)
-          })
-        })
+      submit ($event) {
+        if (this.loading) return
+        this.$emit('submit', $event, this)
       }
-    },
-    mounted () {
-      // Init input
-      this.$emit('input', this.form)
     }
   }
 </script>
