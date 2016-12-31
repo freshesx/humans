64bb196fd859da66a38d8aebc8825a0b4ae1122a
ChaselWu <minowu@foxmail.com>
2016-12-31 18:04:12 +0800

    Add is-focus

--- components/card/card-item.vue
+++ components/card/card-item.vue
@@ -1,5 +1,7 @@
 <template>
-  <div :class="[`${cssPrefix}card-item`, typeClass]">
+  <div
+    :class="[`${cssPrefix}card-item`, typeClass, { 'is-focus': focus }]"
+    @click="click">
     <slot></slot>
   </div>
 </template>
@@ -12,12 +14,28 @@
         validator: val => ['link', 'arrow', 'cursor'].includes(val)
       }
     },
+    data () {
+      return {
+        focus: false
+      }
+    },
     computed: {
       cssPrefix () {
         return this.$human.cssPrefix
       },
       typeClass () {
         return { [`is-${this.type}`]: !!this.type }
+      }
+    },
+    methods: {
+      click ($event) {
+        this.$emit('click', $event, this)
+        if (this.type) {
+          this.focus = true
+          setTimeout(() => {
+            this.focus = false
+          }, 150)
+        }
       }
     }
   }
