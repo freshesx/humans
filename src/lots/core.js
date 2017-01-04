17dec995f1d4ee82c97dcc40e7da15b007511ef2
ChaselWu <minowu@foxmail.com>
2017-01-04 16:29:09 +0800

    Add more lots component set

---
+++ src/lots/core.js
@@ -0,0 +1,18 @@
+import Icon from '../components/icon'
+import LoadingIcon from '../components/loading-icon'
+import Scroller from '../components/scroller'
+import Btn from '../components/button'
+import Card from '../components/card'
+import Code from '../components/code'
+import Popup from '../components/popup'
+
+export default [
+  // Core
+  ...Icon,
+  ...LoadingIcon,
+  ...Scroller,
+  ...Btn,
+  ...Card,
+  ...Code,
+  ...Popup
+]
---
+++ src/lots/index.js
@@ -0,0 +1,9 @@
+import Core from './core'
+import Form from './form'
+import Layout from './layout'
+
+export default [
+  ...Core,
+  ...Form,
+  ...Layout
+]
