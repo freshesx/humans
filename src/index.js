68b7080f3c5b8d836d135e9eb46b48bd4168d80d
ChaselWu <minowu@foxmail.com>
2017-01-04 16:12:16 +0800

    Set add and register method

--- src/index.js
+++ src/index.js
@@ -49,12 +49,34 @@ export default {
     Vue.use(popup)
     Vue.use(browser)
   },
-  add (VueComponent, name = VueComponent.name) {
+  /**
+   * Register VueComponent to Vue
+   *
+   * @param {VueComponent}   VueComponent
+   * @param {String}         name
+   */
+  register (VueComponent, name = VueComponent.name) {
+    if (!this.$vue) {
+      console && console.warn('可能没有执行 Vue.use(VueHuman) 来初始化。')
+    }
+
     this.$vue.component(name, VueComponent)
   },
-  use (VueComponents) {
-    for (let name in VueComponents) {
-      this.add(VueComponents[name])
+  /**
+   * Add lots or one of VueComponent to Vue
+   *
+   * @param {Array|VueComponent}   components
+   * @param {String}               name
+   */
+  add (components, name) {
+    if (typeof components !== 'object') {
+      console && console.warn('传递的 component 存在错误。')
+    }
+
+    if (components instanceof Array) {
+      components.forEach(item => this.register(item))
+    } else {
+      this.register(components, name)
     }
   }
 }
