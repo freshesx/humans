bbfe7d0d5daca40802ec2a9308debd42a8323638
ChaselWu <minowu@foxmail.com>
2017-01-09 16:35:01 +0800

    Set hypertext link

--- examples/components/button/link.vue
+++ examples/components/button/link.vue
@@ -1,29 +1,27 @@
 <template>
   <mn-card>
     <mn-card-item><h5>{{ title }}</h5></mn-card-item>
-    <mn-card-item v-for="types in buttons">
+    <mn-card-item>
       <mn-card-body>
         <mn-link
-          :icon="iosAnalytics"
+          type="primary"
           :class="'has-one-margin-right'"
-          :type="button"
-          :disabled="disabled"
-          @click="click"
-          @error="error"
-          v-for="button in types">{{ button }}</mn-link>
+          @click="click">Hyper link</mn-link>
       </mn-card-body>
     </mn-card-item>
   </mn-card>
 </template>
 
 <script>
-  import Normal from './normal'
-
   export default {
-    mixins: [ Normal ],
     data () {
       return {
         title: '<a> link'
+      }
+    },
+    methods: {
+      click () {
+        console.log('Click hyper link')
       }
     }
   }
