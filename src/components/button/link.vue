9d378eb95b9a809ad66d58f78ae0e96d7f6272bf
ChaselWu <minowu@foxmail.com>
2017-01-09 15:46:09 +0800

    Create link component

---
+++ src/components/button/link.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    :class="[`${cssPrefix}btn`, classes]"
+    :title="title"
+    @click="click">
+    <mn-loading-icon :class="'has-one-margin-right'" v-if="loading"></mn-loading-icon>
+    <mn-icon :class="'has-one-margin-right'" :name="icon" v-if="icon && !loading"></mn-icon>
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+  import MnBtn from './button'
+
+  export default {
+    name: 'mn-link',
+    mixins: [ MnBtn ]
+  }
+</script>
