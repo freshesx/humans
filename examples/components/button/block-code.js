7b6330fbf0d8efbef87a6b513fd2ff198e26adb9
ChaselWu <minowu@foxmail.com>
2017-01-11 15:58:26 +0800

    Remove code component and example

--- examples/components/button/block-code.js
+++
@@ -1,5 +0,0 @@
-export default `
-<mn-btn type="primary" block>Confirm</mn-btn>
-<mn-btn type="warning" block>Sure?</mn-btn>
-<mn-btn type="error" block loading>Off</mn-btn>
-`
--- src/components/code/index.js
+++
@@ -1,5 +0,0 @@
-import Pre from './pre'
-
-export default [
-  Pre
-]
