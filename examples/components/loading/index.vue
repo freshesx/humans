2ec30e8ece61aeb3ccfd59638e272ca197973c04
xxsue <suki.luux1@gmail.com>
2017-01-11 16:45:11 +0800

    Add basic function for loading

--- examples/components/loading/index.vue
+++ examples/components/loading/index.vue
@@ -16,6 +16,13 @@
                 </mn-card-body>
               </mn-card-item>
             </mn-card>
+            <mn-card>
+              <mn-card-item>
+                <mn-card-body>
+                  <mn-btn type="primary" block @click.native="isLoading()">加载</mn-btn>
+                </mn-card-body>
+              </mn-card-item>
+            </mn-card>
           </mn-section>
         </mn-scroller>
       </mn-row>
@@ -24,6 +31,12 @@
 </template>
 
 <script>
+  import loading from 'vue-human/util/loading'
   export default {
+    methods: {
+      isLoading: function () {
+        loading({ show: true })
+      }
+    }
   }
 </script>
