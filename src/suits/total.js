28aa83a35e4e432618ea6b895fc66ac6cf4e6bca
ChaselWu <minowu@foxmail.com>
2017-01-24 20:09:38 +0800

    Set suits

---
+++ src/suits/total.js
@@ -0,0 +1,23 @@
+import Basic from './basic'
+// More components
+import Article from '../components/article'
+import Tag from '../components/tag'
+import Rate from '../components/rate'
+import Menu from '../components/menu'
+import NavBar from '../components/nav-bar'
+import TabBar from '../components/tab-bar'
+import SearchBar from '../components/search-bar'
+import SlideBar from '../components/slide-bar'
+
+export default [
+  ...Basic,
+  // More components
+  ...Article,
+  ...Tag,
+  ...Rate,
+  ...Menu,
+  ...NavBar,
+  ...TabBar,
+  ...SearchBar,
+  ...SlideBar
+]
