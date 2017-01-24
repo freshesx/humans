d4bf8ba8f7180408eb1ae08bb3718eddc77d04b4
ChaselWu <minowu@foxmail.com>
2017-01-24 18:06:33 +0800

    Move scss to vue in loading-icon

--- src/components/loading-icon/default-icon.vue
+++ src/components/loading-icon/default-icon.vue
@@ -11,3 +11,31 @@
     }
   }
 </script>
+
+<style lang="scss">
+  @import "../../sass/variables";
+
+  .#{$namespace}loading-icon {
+    @keyframes #{$namespace}loading-icon {
+      0% {
+        transform: scale(1);
+      }
+
+      50% {
+        transform: scale(0.6);
+      }
+
+      100% {
+        transform: scale(1);
+      }
+    }
+
+    width: 16px;
+    height: 16px;
+    display: inline-block;
+    background: currentColor;
+    border-radius: 8px;
+    vertical-align: -2px;
+    animation: #{$namespace}loading-icon 1.5s infinite linear;
+  }
+</style>
