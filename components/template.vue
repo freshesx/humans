0681b534650fcf44489bf4cf321e4e9d8cb9d36f
ChaselWu <minowu@foxmail.com>
2017-02-07 16:00:33 +0800

    Change template.vue

--- components/template.vue
+++ components/template.vue
@@ -1,18 +1,17 @@
 <!-- This is just a component template, copy it to create new component -->
 
 <template>
-  <!-- class and style use Array, not Object -->
-  <div :class="[ `${cssPrefix}className` ]">
+  <div class="mn-component" :class="{}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    computed: {
-      cssPrefix () {
-        return this.$human.cssPrefix
-      }
-    }
+    name: 'mn-component'
   }
 </script>
+
+<style lang="scss">
+  // component scss
+</style>
