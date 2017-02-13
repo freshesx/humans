eee6e3d6f25f7f9d3f569390d54eb79ec2acb4d1
ChaselWu <minowu@foxmail.com>
2017-02-13 14:01:53 +0800

    Remove breakpoints, loading, icons

--- index.js
+++ index.js
@@ -2,33 +2,6 @@ import browser from './util/browser/plugins.js'
 import './scss/bases.scss'
 
 export default {
-  options: {
-    /**
-     * Human css breakpoints
-     * @type {Object}
-     * @var {Number} breakpoints.mobile
-     * @var {Number} breakpoints.tablets
-     * @var {Number} breakpoints.desktop
-     * @var {Number} breakpoints.widescreen
-     */
-    breakpoints: {
-      mobile: 0,
-      tablets: 768,
-      desktop: 992,
-      widescreen: 1200
-    },
-    /**
-     * Human load function
-     * @example function (resolve) { require(['./file/path'], resolve) }
-     * @type {Function}
-     */
-    loading: undefined,
-    /**
-     * Human icon array
-     * @type {Array}
-     */
-    icons: {}
-  },
   /**
    * Vue install
    * @param  {Object} Vue
@@ -36,19 +9,6 @@ export default {
    */
   install (Vue, options) {
     this.$vue = Vue
-
-    // Merge and assign options
-    this.options = { ...this.options, ...options }
-
-    // Set up vue human
-    Vue.human = {
-      breakpoints: this.options.breakpoints,
-      loading: this.options.loading,
-      icons: this.options.icons
-    }
-
-    // Alias for human
-    Vue.prototype.$human = Vue.human
 
     // Using human plugins
     Vue.use(browser)
