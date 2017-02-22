7112dd675768876aa8c0f05a056910609d326c7d
ChaselWu <minowu@foxmail.com>
2017-02-22 16:42:39 +0800

    Using suits class in loader

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import Suits from './util/suits'
 import browser from './util/browser/plugins.js'
 import './scss/bases.scss'
 
@@ -13,6 +14,7 @@ export default {
     // Using human plugins
     Vue.use(browser)
   },
+
   /**
    * Register VueComponent to Vue
    *
@@ -21,26 +23,26 @@ export default {
    */
   register (VueComponent, name = VueComponent.name) {
     if (!this.$vue) {
-      console && console.warn('可能没有执行 Vue.use(VueHuman) 来初始化。')
+      return console && console.warn('可能没有执行 Vue.use(VueHuman) 来初始化。')
     }
 
     this.$vue.component(name, VueComponent)
   },
-  /**
-   * Add lots or one of VueComponent to Vue
-   *
-   * @param {Array|VueComponent}   components
-   * @param {String}               name
-   */
-  add (components, name) {
-    if (typeof components !== 'object') {
-      console && console.warn('传递的 component 存在错误。')
-    }
 
-    if (components instanceof Array) {
-      components.forEach(item => this.register(item))
-    } else {
-      this.register(components, name)
-    }
+  /**
+   * @param {Suits} suits
+   */
+  addSuits (suits) {
+    if (!suits instanceof Suits) return console.warn('必须是 Suits 的实例')
+    suits.getComponents().forEach(item => this.register(item))
+  },
+
+  /**
+   * @param {Object} component
+   * @param {String} name
+   */
+  addComponent (component, name = undefined) {
+    if (typeof component !== 'object') return console.warn('必须是对象')
+    this.register(component, name)
   }
 }
