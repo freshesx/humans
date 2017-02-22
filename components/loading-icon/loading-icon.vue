9996920fa5dc4743fc27b747a4b2275fa6c1c235
ChaselWu <minowu@foxmail.com>
2017-02-22 17:15:02 +0800

    Add install for loading-icon and add defaultIcon

--- components/loading-icon/loading-icon.vue
+++ components/loading-icon/loading-icon.vue
@@ -6,15 +6,21 @@
   import Vue from 'vue'
   import defaultIcon from './default-icon'
 
+  let loadingIcon = defaultIcon
+
   export default {
     name: 'mn-loading-icon',
+    install (InstallVue, options = {}) {
+      if (options.icon && typeof options.icon === 'object') loadingIcon = options.icon
+      InstallVue.component(this.name, this)
+    },
     data () {
       return {
         icon: undefined
       }
     },
     mounted () {
-      const Component = Vue.extend(defaultIcon)
+      const Component = Vue.extend(loadingIcon)
       this.icon = new Component({ el: document.createElement('span') })
 
       this.$nextTick(() => {
