0b21e1f59e80f61aef4857d62817f51fd3e0adab
ChaselWu <minowu@foxmail.com>
2017-02-23 14:46:50 +0800

    Remove comments

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ import './scss/bases.scss'
 import browser from './util/browser/plugins.js'
 
 export default {
-  /**
-   * Vue install
-   * @param  {Object} Vue
-   * @param  {Object} options
-   */
   install (Vue, options) {
-    // Set global vue for Human
     this.$vue = Vue
-
-    // Using human plugins
     Vue.use(browser)
   }
 }
