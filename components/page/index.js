d57961d5006eb92bdf3ed2c59dade39e636f103c
ChaselWu <minowu@foxmail.com>
2017-02-24 12:52:54 +0800

    New Page component

---
+++ components/page/index.js
@@ -0,0 +1,6 @@
+import Suits from '../../util/suits'
+import Page from './page'
+
+export default new Suits([
+  Page
+])
--- suits/layout.js
+++ suits/layout.js
@@ -3,10 +3,12 @@ import SectionSuits from '../components/section'
 import ColumnSuits from '../components/column'
 import RowSuits from '../components/row'
 import LetterSuits from '../components/letter'
+import PageSuits from '../components/page'
 
 export default new Suits([
   SectionSuits,
   ColumnSuits,
   RowSuits,
-  LetterSuits
+  LetterSuits,
+  PageSuits
 ])
