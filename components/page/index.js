de30675608dae8bebf269dd6e4d4d7c017749595
ChaselWu <minowu@foxmail.com>
2017-02-28 01:54:12 +0800

    Add page-group

--- components/page/index.js
+++ components/page/index.js
@@ -1,6 +1,8 @@
 import Suits from '../../util/suits'
 import Page from './page'
+/* import PageGroup from './page-group' */
 
 export default new Suits([
-  Page
+  Page/* ,
+  PageGroup */
 ])
---
+++ components/page/page-group.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition-group name="has-slide">
+    <slot></slot>
+  </transition-group>
+</template>
+
+<script>
+  import Element from '../../util/element'
+  import Page from './page'
+
+  export default new Element({
+    name: 'mn-page-group',
+    mixins: [ Page ]
+  })
+</script>
