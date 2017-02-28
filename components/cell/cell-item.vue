06141b3981809955195bfb2da609a3a9bf2d49b5
ChaselWu <minowu@foxmail.com>
2017-02-28 17:31:29 +0800

    Create cell component

---
+++ components/cell/cell-item.vue
@@ -0,0 +1,23 @@
+<template>
+  <mn-card-item :type="type">
+    <mn-card-prefix>
+      单选
+    </mn-card-prefix>
+    <slot></slot>
+  </mn-card-item>
+</template>
+
+<script>
+  import Element from '../../util/element'
+
+  export default new Element({
+    name: 'mn-cell-item',
+    props: {
+      type: String
+    }
+  })
+</script>
+
+<style lang="scss">
+  // component scss
+</style>
---
+++ components/cell/cell.vue
@@ -0,0 +1,37 @@
+<template>
+  <mn-card>
+    <mn-card-item>
+      <mn-card-prefix>
+        全选 (0)
+      </mn-card-prefix>
+    </mn-card-item>
+    <mn-loading-icon v-if="loading"></mn-loading-icon>
+    <slot v-if="!loading">没有找到合适的记录</slot>
+  </mn-card>
+</template>
+
+<script>
+  import Element from '../../util/element'
+
+  export default new Element({
+    name: 'mn-cell',
+    data () {
+      return {
+        loading: false
+      }
+    },
+    methods: {
+      update (callback) {
+        this.loading = true
+        callback.apply(this, [this.next])
+      },
+      next () {
+        this.loading = false
+      }
+    }
+  })
+</script>
+
+<style lang="scss">
+  // component scss
+</style>
