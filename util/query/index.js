5dc585aade747f533450542ac40c72d930911930
ChaselWu <minowu@foxmail.com>
2017-03-02 15:40:24 +0800

    Simplify

--- util/query/index.js
+++ util/query/index.js
@@ -1,3 +1,5 @@
+import { isObjectLike } from 'lodash'
+
 /**
  * 存储 Query 请求时的参数，并按一定的格式字符串化
  * @class Query
@@ -9,20 +11,15 @@ export default class Query {
    */
   conditions = {}
 
-  /**
-   * 存储排序条件
-   * @var {Object}
-   */
-  orderBys = {}
-
   constructor (datasets) {
+    // Exec beforeCreate
     this.beforeCreate.apply(this)
 
     for (var name in datasets) {
       if (!datasets.hasOwnProperty(name)) return
       // 储存器特殊储存
-      if (['conditions', 'orderBys'].includes(name)) {
-        this.set(name, datasets[name])
+      if (['conditions'].includes(name)) {
+        this.set(datasets[name])
       } else {
         this[name] = datasets[name]
       }
@@ -35,89 +32,95 @@ export default class Query {
   changed () {
   }
 
-  addCondition (name, value) {
-    return this.add('conditions', name, value)
-  }
-
-  setConditions (conditions) {
-    return this.set('conditions', conditions)
-  }
-
-  addOrderBy (name, value) {
-    return this.add('orderBys', name, value)
-  }
-
-  setOrderBys (orderBys) {
-    return this.set('orderBys', orderBys)
-  }
-
   /**
    * 返回合并后的参数
    */
   params () {
-    return { ...this.conditions, orderBys: this.orderBys }
+    // return this.conditions
+    this.conditions
+    let obj = {}
+
+    for (var name in this.conditions) {
+      if (this.conditions.hasOwnProperty(name)) {
+        let condition = this.conditions[name]
+
+        //  = this.conditions[name]
+        //
+        // // 如果
+        // obj[name] = typeof condition === 'object'
+        //   ? condition
+        //   : condition
+        //
+        // typeof condition === 'object' && condition.constructor
+
+        if (isObjectLike(condition)) {
+          condition = convertObject(condition)
+        }
+
+        obj[name] = condition
+      }
+    }
+    console.log(obj)
+    return obj
   }
 
-  toSerialize () {
-  }
-
-  toString () {
+  convertObject (condition) {
+    return JSON.stringify(condition)
   }
 
   /**
    * 添加单个结果至储存器
    *
-   * @param {String} storageName
    * @param {String} name
    * @param {null|mixin} value null 为删除
+   * @param {Boolean} notifyChange
    *
    * @return this
    */
-  add (storageName, name, value, notifyChange = true) {
-    if (value === null) return this.remove(storageName, name)
-    this[storageName][name] = value
-
-    if (notifyChange) this.changed.apply(this, this[storageName]) // 通知更新
+  add (name, value, notifyChange = true) {
+    if (value === null) return this.remove(name)
+    this.conditions[name] = value
+    // 通知更新
+    if (notifyChange) this.changed.apply(this, this.conditions)
     return this
   }
 
   /**
    * 移除单个结果至储存器
    *
-   * @param {String} storageName
    * @param {String} name
+   * @param {Boolean} notifyChange
    *
    * @return this
    */
-  remove (storageName, name, notifyChange = true) {
-    if (name in this[storageName]) delete this[storageName][name]
-
-    if (notifyChange) this.changed.apply(this, this[storageName]) // 通知更新
+  remove (name, notifyChange = true) {
+    if (name in this.conditions) delete this.conditions[name]
+    // 通知更新
+    if (notifyChange) this.changed.apply(this, this.conditions)
     return this
   }
 
   /**
    * 替换新的储存内容至储存器
    *
-   * @param {String} storageName
    * @param {Object} storageValue
+   * @param {Boolean} notifyChange
    *
    * @return this
    */
-  set (storageName, storageValue, notifyChange = true) {
+  set (storageValue, notifyChange = true) {
     if (typeof storageValue !== 'object' || storageValue.constructor !== Object) {
-      // this[storageName] = storageValue
       console && console.warn('传递的存储内容不是一个对象。')
       return this
     }
 
     for (var name in storageValue) {
       if (storageValue.hasOwnProperty(name)) {
-        this.add(storageName, name, storageValue[name], false)
+        this.add(name, storageValue[name], false)
       }
     }
 
-    if (notifyChange) this.changed.apply(this, this[storageName]) // 通知更新
+    if (notifyChange) this.changed.apply(this, this.conditions) // 通知更新
 
     return this
   }
