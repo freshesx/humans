dc9beeffb6fe9b2d943bc817ed53cf8421ec6713
ChaselWu <minowu@foxmail.com>
2017-03-26 02:11:52 +0800

    Add locale file and support vue-i18n in vue-human entry

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 import './scss/bases.scss'
 import Vuelidate from 'vuelidate'
+import VueI18n from 'vue-i18n'
+import merge from 'deepmerge'
+import componentLocales from './locale'
 
 export default {
-  install (Vue, options) {
+  install (Vue, options = {}) {
     this.$vue = Vue
+
+    // Vuelidate
     Vue.use(Vuelidate)
+
+    // VueI18n
+    Vue.use(VueI18n)
+
+    // Set Vue config language
+    Vue.config.lang = options.lang || 'zh-CN'
+
+    // Use Promise all
+    this.mergeLocals([ componentLocales, options.locales ])
+      .then(translations => {
+        Vue.locale(this.$vue.config.lang, merge.all(translations))
+      })
+  },
+
+  mergeLocals (locales) {
+    return Promise.all(locales.map(locale => this.addLocales(locale)))
+  },
+
+  addLocales (locales = []) {
+    return new Promise(resolve => {
+      let locale = locales.find(item => item.name === this.$vue.config.lang)
+
+      if (!locale) {
+        resolve({})
+      } else if (typeof locale.translation === 'function') {
+        locale.translation(file => {
+          resolve(file.default)
+        })
+      } else {
+        resolve(locale.translation)
+      }
+    })
   }
 }
---
+++ locale/langs/zh-CN.js
@@ -0,0 +1,9 @@
+export default {
+  mn: {
+    popup: {
+      confirmTitle: '确认操作',
+      confirmText: '确认',
+      cancelText: '取消'
+    }
+  }
+}
