a0e0378d22fa4fafc454689b7886c735ef70ae96
ChaselWu <minowu@foxmail.com>
2017-04-03 21:45:43 +0800

    Add AxiosHelper

--- utils/AxiosHelper.js
+++ utils/AxiosHelper.js
@@ -3,6 +3,10 @@ import isFunction from 'lodash/isFunction'
 
 export default class AxiosHelper {
   message (error) {
+    if (!error.response) {
+      return this.errorException(error)
+    }
+
     const funcName = `error${error.response.status}`
 
     if (isFunction(this[funcName])) {
@@ -17,25 +21,31 @@ export default class AxiosHelper {
     })
   }
 
-  error401 (response) {
+  errorException (error) {
+    this.openErrorMessage({
+      description: '网络异常错误，请刷新。'
+    })
+  }
+
+  $error401 (error) {
     this.openErrorMessage({
       description: '帐户已注销，请重新登录。'
     })
   }
 
-  error403 (response) {
+  $error403 (error) {
     this.openErrorMessage({
       description: '您没有权限操作该功能。'
     })
   }
 
-  // error404 (response) {
-  //   this.openErrorMessage({
-  //     description: '资源不存在或已删除。'
-  //   })
-  // }
+  $error404 (error) {
+    this.openErrorMessage({
+      description: '资源不存在或已删除。'
+    })
+  }
 
-  error500 (response) {
+  $error500 (error) {
     this.openErrorMessage({
       description: '服务器出了一点故障，请稍后重试。'
     })
