cc2668e6652650ee1a0233d118b30005cdbd850f
ChaselWu <minowu@foxmail.com>
2017-04-06 11:35:48 +0800

    Add comments

--- util/Element.js
+++ util/Element.js
@@ -1,9 +1,19 @@
+/** Class 封装 vue component export */
 export default class Element {
-
-  constructor (options) {
-    Object.assign(this, options)
+  /**
+   * 构造函数，合并传入的 vue 配置和 this
+   * @param {Object} vueComponent vue 组件的配置对象
+   */
+  constructor (vueComponent) {
+    Object.assign(this, vueComponent)
   }
 
+  /**
+   * vue.use() 需要的 install 方法
+   * @param {Vue} Vue
+   * @param {Object} [options={}]
+   * @return {void}
+   */
   install (Vue, options = {}) {
     if (this.beforeInstalled) {
       this.beforeInstalled(Vue, options)
--- util/Suits.js
+++ util/Suits.js
@@ -1,6 +1,4 @@
-/**
- * @class Suits
- */
+/** Class 封装多个组件的 Suits 类 */
 export default class Suits {
   /**
    * @var {Array}
@@ -8,7 +6,7 @@ export default class Suits {
   components = []
 
   /**
-   * Build components for suits
+   * 构造函数，判断传入的对象是 Suits 对象还是数组，并合并
    * @param {Array} components
    */
   constructor (components) {
@@ -22,17 +20,22 @@ export default class Suits {
   }
 
   /**
-   * Return components storage
-   *
+   * 返回 Suits 内的所有组件
    * @return {Array}
    */
   getComponents () {
     return this.components
   }
 
+  /**
+   * vue.use() 的安装方法
+   * @param {Vue} Vue
+   */
   install (Vue) {
     this.components.forEach(component => {
-      if (!component.install) return console.warn(component.name + '没有 install 方法', component)
+      if (!component.install) {
+        return console.warn(`${component.name} 没有 install 方法`, component)
+      }
       Vue.use(component)
     })
   }
