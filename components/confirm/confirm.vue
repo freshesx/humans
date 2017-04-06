2f0716598192bf7e9eae2239d88f8e5e625a872c
ChaselWu <minowu@foxmail.com>
2017-04-06 22:37:22 +0800

    Create confirm

--- components/confirm/confirm.vue
+++ components/confirm/confirm.vue
@@ -1,5 +1,5 @@
 <template>
-  <mn-popup :show="show" :class="[ 'mn-popup-confirm' ]">
+  <mn-popup :show="isShow" :class="[ 'mn-popup-confirm' ]">
     <mn-popup-card class="has-none-margin-bottom">
       <mn-card-item>
         <mn-card-body class="has-center-text">
@@ -17,33 +17,37 @@
 
 <script>
   import Element from '../../util/Element'
-  import Popup from './popup'
-  import PopupCard from '../popup/popup-card'
-  import CardItem from '../card/card-item'
-  import CardBody from '../card/card-body'
-  import CardBtns from '../card/card-btns'
+  import popup from '../popup/popup'
+  import popupCard from '../popup/popup-card'
+  import cardItem from '../card/card-item'
+  import cardBody from '../card/card-body'
+  import cardBtns from '../card/card-btns'
 
   export default new Element({
     components: {
-      [Popup.name]: Popup,
-      [PopupCard.name]: PopupCard,
-      [CardItem.name]: CardItem,
-      [CardBody.name]: CardBody,
-      [CardBtns.name]: CardBtns
+      [popup.name]: popup,
+      [popupCard.name]: popupCard,
+      [cardItem.name]: cardItem,
+      [cardBody.name]: cardBody,
+      [cardBtns.name]: cardBtns
     },
     methods: {
       cancel () {
-        this.show = false
+        this.isShow = false
         this.$emit('cancel')
       },
       confirm () {
-        this.show = false
+        this.isShow = false
         this.$emit('confirm')
+      },
+      show () {
+        this.isShow = true
+        return this
       }
     },
     data () {
       return {
-        show: false,
+        isShow: false,
         title: this.$t('mn.popup.confirmTitle'),
         description: undefined,
         cancelText: this.$t('mn.popup.cancelText'),
