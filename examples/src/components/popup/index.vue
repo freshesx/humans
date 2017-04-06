071465422d5fd85cf0589c732b71ed4e950c1b29
ChaselWu <minowu@foxmail.com>
2017-04-06 17:28:59 +0800

    Add popup example

---
+++ examples/src/components/popup/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button @click="onOpenActionSheet">Action Sheets</button>
+
+    <mn-action-card v-model="showActionSheet">
+      <mn-card-item>
+        <mn-card-body class="has-center-text">
+          <small>是否将这个照片从您的设备中删除</small>
+        </mn-card-body>
+      </mn-card-item>
+      <mn-card-btns>
+        <button class="has-red-text">删除照片</button>
+      </mn-card-btns>
+    </mn-action-card>
+  </div>
+</template>
+
+<script>
+  import Element from 'vue-human/util/Element'
+  import actionCard from 'vue-human/components/action-card/action-card'
+  import cardItem from 'vue-human/components/card/card-item'
+  import cardBody from 'vue-human/components/card/card-body'
+  import cardBtns from 'vue-human/components/card/card-btns'
+
+  export default new Element({
+    name: 'p-popup',
+    components: {
+      [actionCard.name]: actionCard,
+      [cardItem.name]: cardItem,
+      [cardBody.name]: cardBody,
+      [cardBtns.name]: cardBtns
+    },
+    data () {
+      return {
+        showActionSheet: false
+      }
+    },
+    methods: {
+      onOpenActionSheet () {
+        this.showActionSheet = true
+      }
+    }
+  })
+</script>
