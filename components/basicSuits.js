e9fa712d740a045414653ff0d9bbbf5684018319
ChaselWu <minowu@foxmail.com>
2017-04-06 10:26:38 +0800

    Use new Suits name

---
+++ components/basicSuits.js
@@ -0,0 +1,10 @@
+import Suits from '../util/suits'
+import coreSuits from './coreSuits'
+import formSuits from './formSuits'
+import layoutSuits from './layoutSuits'
+
+export default new Suits ([
+  coreSuits,
+  formSuits,
+  layoutSuits
+])
---
+++ components/layoutSuits.js
@@ -0,0 +1,16 @@
+import Suits from '../util/suits'
+import containerSuits from '../components/container'
+import sectionSuits from '../components/section'
+import columnSuits from '../components/column'
+import rowSuits from '../components/row'
+import letterSuits from '../components/letter'
+import pageSuits from '../components/page'
+
+export default new Suits([
+  containerSuits,
+  sectionSuits,
+  columnSuits,
+  rowSuits,
+  letterSuits,
+  pageSuits
+])
