64038aa7144e8b985b97c26e3ae75fb6447e0378
ChaselWu <minowu@foxmail.com>
2017-04-08 16:28:46 +0800

    Update webpack.base.conf.js

--- examples/build/webpack.base.conf.js
+++ examples/build/webpack.base.conf.js
@@ -21,8 +21,7 @@ function includeFolders () {
 
 module.exports = {
   entry: {
-    polyfill: 'babel-polyfill',
-    app: resolve('src/main.js')
+    app: ['babel-polyfill', resolve('src/main.js')]
   },
   output: {
     path: config.build.assetsRoot,
@@ -67,7 +66,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
@@ -75,7 +74,7 @@ module.exports = {
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
