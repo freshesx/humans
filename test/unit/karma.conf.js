9e6a2ea4a7cbcc01c20e937e56f9a059bdc215fe
ChaselWu <minowu@foxmail.com>
2017-04-09 02:46:10 +0800

    Change text-summary to text

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
       dir: './coverage',
       reporters: [
         { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
+        { type: 'text' }
       ]
     }
   })
