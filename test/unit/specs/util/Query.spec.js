b309e141be85bc8437818b81c5b735de4b133a00
ChaselWu <minowu@foxmail.com>
2017-04-09 02:45:43 +0800

    Finish Query spec

--- test/unit/specs/util/Query.spec.js
+++ test/unit/specs/util/Query.spec.js
@@ -2,14 +2,29 @@ import Query from 'vue-human/util/Query'
 
 describe('util/Query', () => {
   it('.parse()', () => {
-    const query = new Query()
     const input = {
-      stringNumber: '10'
+      n1: '10',
+      n2: '0.6',
+      n3: 0.6,
+      n4: 'Hello World!',
+      n5: 'undefined',
+      n6: 'Undefined',
+      n7: 'null',
+      n8: 'Null',
+      n9: ''
     }
     const hope = {
-      stringNumber: 10
+      n1: 10,
+      n2: 0.6,
+      n3: 0.6,
+      n4: 'Hello World!',
+      n5: undefined,
+      n6: undefined,
+      n7: null,
+      n8: null,
+      n9: undefined
     }
-    const output = query.parse(input)
+    const output = Query.parse(input)
 
     expect(output).to.deep.equal(hope)
   })
