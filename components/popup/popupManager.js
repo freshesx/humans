ca37ffebb337e419cb02307a41adbfdd78479587
ChaselWu <minowu@foxmail.com>
2017-04-28 15:54:11 +0800

    Add comment in popupManager

--- components/popup/popupManager.js
+++ components/popup/popupManager.js
@@ -1,15 +1,29 @@
 export default {
   data () {
     return {
+      /**
+       * Control the component is show or hide
+       */
       isShow: false
     }
   },
   methods: {
+    /**
+     * Set isShow to false, and emit close event.
+     *
+     * @return {this}
+     */
     close () {
       this.isShow = false
       this.$emit('close')
       return this
     },
+
+    /**
+     * Set isShow to true, and emit show event.
+     *
+     * @return {this}
+     */
     show () {
       this.isShow = true
       this.$emit('show')
