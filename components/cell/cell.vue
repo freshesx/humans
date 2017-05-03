c79f70d13c934d437d7bf40c4596e28f7de1ae5d
ChaselWu <minowu@foxmail.com>
2017-05-03 12:44:10 +0800

    Change tag

--- components/cell/cell.vue
+++ components/cell/cell.vue
@@ -1,57 +1,57 @@
 <template>
-  <card>
+  <mn-card>
     <!-- 如果隐藏多选功能，并且 action 的 slot 不存在的话，则不显示 -->
-    <card-item v-if="!hideSelections || $slots.action || $slots.title" type="cursor">
+    <mn-card-item v-if="!hideSelections || $slots.action || $slots.title" type="cursor">
       <!-- 如果隐藏多选功能，则不显示，并且当加载的时候，选中数量和图标变为未选中 -->
-      <card-prefix v-if="!hideSelections">
-        <cell-icon :checked="!loading && isAllChecked" @click.native.stop="onAddSelections"></cell-icon>
-      </card-prefix>
-      <card-prefix v-if="!hideSelections">
+      <mn-card-prefix v-if="!hideSelections">
+        <mn-cell-icon :checked="!loading && isAllChecked" @click.native.stop="onAddSelections"></mn-cell-icon>
+      </mn-card-prefix>
+      <mn-card-prefix v-if="!hideSelections">
         {{ $t('mn.cell.allSelectedText') }}
         ({{ loading ? 0 : selections.length }})
-      </card-prefix>
+      </mn-card-prefix>
       <!-- 顶部的中间区域 -->
       <slot name="title">
-        <card-body></card-body>
+        <mn-card-body></mn-card-body>
       </slot>
       <!-- 顶部的后部区域 -->
       <slot name="action"></slot>
-    </card-item>
+    </mn-card-item>
     <!-- 当加载的时候，显示加载条，关闭其他的渲染 -->
-    <card-item v-if="loading">
-      <card-body :class="'has-center-text'">
-        <loading-icon></loading-icon>
-      </card-body>
-    </card-item>
+    <mn-card-item v-if="loading">
+      <mn-card-body :class="'has-center-text'">
+        <mn-loading-icon></mn-loading-icon>
+      </mn-card-body>
+    </mn-card-item>
     <!-- 当没有一个记录时，则显示提示 -->
-    <card-item v-if="contents.length === 0 && !loading">
-      <card-body :class="'has-center-text'">
+    <mn-card-item v-if="contents.length === 0 && !loading">
+      <mn-card-body :class="'has-center-text'">
         <p>{{ $t('mn.cell.noDataText') }}</p>
-      </card-body>
-    </card-item>
+      </mn-card-body>
+    </mn-card-item>
     <!-- default 的 slot 区域，外部调用时，请使用 "template slot"!! 插槽 -->
     <slot :item="item" v-for="item in contents" v-if="!loading"></slot>
-  </card>
+  </mn-card>
 </template>
 
 <script>
   import Element from '../../util/Element'
-  import Card from '../card/card'
-  import CardItem from '../card/card-item'
-  import CardPrefix from '../card/card-prefix'
-  import CardBody from '../card/card-body'
-  import LoadingIcon from '../loading-icon/loading-icon'
-  import CellIcon from './cell-icon'
+  import card from '../card/card'
+  import cardItem from '../card/card-item'
+  import cardPrefix from '../card/card-prefix'
+  import cardBody from '../card/card-body'
+  import loadingIcon from '../loading-icon/loading-icon'
+  import cellIcon from './cell-icon'
 
   export default new Element({
     name: 'mn-cell',
     components: {
-      CellIcon,
-      Card,
-      CardItem,
-      CardPrefix,
-      CardBody,
-      LoadingIcon
+      ...card.inject(),
+      ...cardItem.inject(),
+      ...cardPrefix.inject(),
+      ...cardBody.inject(),
+      ...loadingIcon.inject(),
+      ...cellIcon.inject()
     },
     props: {
       contents: {
