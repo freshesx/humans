85179d23d03f883264d1713284177be82e302ec0
ChaselWu <minowu@foxmail.com>
2017-05-03 12:57:55 +0800

    Change loading

--- components/cell/cell.vue
+++ components/cell/cell.vue
@@ -4,11 +4,11 @@
     <mn-card-item v-if="!hideSelections || $slots.action || $slots.title" type="cursor">
       <!-- 如果隐藏多选功能，则不显示，并且当加载的时候，选中数量和图标变为未选中 -->
       <mn-card-prefix v-if="!hideSelections">
-        <mn-cell-icon :checked="!loading && isAllChecked" @click.native.stop="onAddSelections"></mn-cell-icon>
+        <mn-cell-icon :checked="contents && isAllChecked" @click.native.stop="onAddSelections"></mn-cell-icon>
       </mn-card-prefix>
       <mn-card-prefix v-if="!hideSelections">
         {{ $t('mn.cell.allSelectedText') }}
-        ({{ loading ? 0 : selections.length }})
+        ({{ !contents ? 0 : selections.length }})
       </mn-card-prefix>
       <!-- 顶部的中间区域 -->
       <slot name="title">
@@ -18,19 +18,19 @@
       <slot name="action"></slot>
     </mn-card-item>
     <!-- 当加载的时候，显示加载条，关闭其他的渲染 -->
-    <mn-card-item v-if="loading">
+    <mn-card-item v-if="!contents">
       <mn-card-body :class="'has-center-text'">
         <mn-loading-icon></mn-loading-icon>
       </mn-card-body>
     </mn-card-item>
     <!-- 当没有一个记录时，则显示提示 -->
-    <mn-card-item v-if="contents.length === 0 && !loading">
+    <mn-card-item v-if="contents && contents.length === 0">
       <mn-card-body :class="'has-center-text'">
         <p>{{ $t('mn.cell.noDataText') }}</p>
       </mn-card-body>
     </mn-card-item>
     <!-- default 的 slot 区域，外部调用时，请使用 "template slot"!! 插槽 -->
-    <slot :item="item" v-for="item in contents" v-if="!loading"></slot>
+    <slot :item="item" v-for="item in contents" v-if="contents && contents.length > 0"></slot>
   </mn-card>
 </template>
 
@@ -54,18 +54,31 @@
       ...cellIcon.inject()
     },
     props: {
+      /**
+       * When contents.length > 0, show contents of list.
+       * When contents.length === 0, show tip: nothing found.
+       * When content is undefined, show loading icon.
+       */
       contents: {
-        type: Array,
-        default: () => []
+        type: Array
       },
+      /**
+       * When contents is changed, selections is cleared.
+       */
       keepSelections: {
         type: Boolean,
         default: false
       },
+      /**
+       * Hide selections
+       */
       hideSelections: {
         type: Boolean,
         default: false
       },
+      /**
+       * Selections storage
+       */
       selections: {
         type: Array,
         default: () => {
@@ -73,30 +86,7 @@
         }
       }
     },
-    data () {
-      return {
-        loading: false
-      }
-    },
     methods: {
-      /**
-       * 便于动画的加载和使用的方法
-       * this.$refs.cell.update(next => {
-       *   // 处理你的逻辑
-       *   next()  // 通知 cell 加载结束
-       * })
-       * @param {Function} callback - 回调函数
-       */
-      update (callback) {
-        this.loading = true
-        callback.apply(this, [this.next])
-      },
-      /**
-       * 关闭加载状态
-       */
-      next () {
-        this.loading = false
-      },
       /**
        * 对比 selections，切换加入还是删除
        * @param {*} item
