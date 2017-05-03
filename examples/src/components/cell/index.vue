825bb0bfe460b25a9863c47786d70f6f952956f8
ChaselWu <minowu@foxmail.com>
2017-05-03 12:29:57 +0800

    Add cell example file

--- examples/src/components/cell/index.vue
+++ examples/src/components/cell/index.vue
@@ -1,3 +1,57 @@
 <template>
-  <div>cell</div>
+  <mn-scroller>
+    <mn-container>
+      <mn-section>
+        <mn-cell :contents="contents" :selections="selections" @selections="selections = arguments[0]">
+          <mn-card-suffix slot="action" action>
+            <mn-btn theme="secondary" size="sm">删除</mn-btn>
+          </mn-card-suffix>
+          <template scope="scope">
+            <mn-cell-item type="link" :item="scope.item">
+              <mn-card-body>
+                <h2>{{ scope.item.name }} <small>{{ scope.item.zip }}</small></h2>
+              </mn-card-body>
+            </mn-cell-item>
+          </template>
+        </mn-cell>
+      </mn-section>
+    </mn-container>
+  </mn-scroller>
 </template>
+
+<script>
+  import scrollerSuit from 'vue-human/components/scroller'
+  import containerSuit from 'vue-human/components/container'
+  import sectionSuit from 'vue-human/components/section'
+  import cellSuit from 'vue-human/components/cell'
+  import cardSuit from 'vue-human/components/card'
+  import buttonSuit from 'vue-human/components/button'
+  import { getDistrict } from './api'
+
+  export default {
+    components: {
+      ...scrollerSuit.map(),
+      ...containerSuit.map(),
+      ...sectionSuit.map(),
+      ...cellSuit.map(),
+      ...cardSuit.map(),
+      ...buttonSuit.map()
+    },
+    data () {
+      return {
+        contents: undefined,
+        selections: []
+      }
+    },
+    methods: {
+      update () {
+        getDistrict().then(response => {
+          this.contents = response
+        })
+      }
+    },
+    mounted () {
+      this.update()
+    }
+  }
+</script>
