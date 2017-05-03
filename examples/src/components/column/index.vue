a8379b0f7c469e333e4acaabe720f33141eb5bd8
ChaselWu <minowu@foxmail.com>
2017-05-03 15:51:32 +0800

    Change example

--- examples/src/components/column/index.vue
+++ examples/src/components/column/index.vue
@@ -9,8 +9,8 @@
     </mn-section>
 
     <mn-section>
-      <mn-columns>
-        <mn-column :tablet="{ flex: '0 1 33.3%' }" :desktop="{ flex: '0 1 20%' }" v-for="item in 10" :key="item">
+      <mn-flexes>
+        <mn-flex :tablet="{ flex: '0 1 33.3%' }" :desktop="{ flex: '0 1 20%' }" v-for="item in 10" :key="item">
           <mn-card>
             <mn-card-item>
               <mn-card-body>
@@ -18,18 +18,18 @@
               </mn-card-body>
             </mn-card-item>
           </mn-card>
-        </mn-column>
-      </mn-columns>
+        </mn-flex>
+      </mn-flexes>
     </mn-section>
 
     <mn-section>
-      <mn-columns :desktop="{ margin: 0, 'margin-left': '1px' }">
-        <mn-column :mobile="{ flex: '1 1 50%' }" :tablet="{ flex: '1 1 25%' }" :desktop="{ flex: '1 1 20%' }" :style="{ 'border': 'solid 1px #e5e5e5', 'margin-left': '-1px', 'margin-top': '-1px', padding: 0 }" v-for="item in 10" :key="item">
+      <mn-flexes :desktop="{ margin: 0, 'margin-left': '1px' }">
+        <mn-flex :mobile="{ flex: '1 1 50%' }" :tablet="{ flex: '1 1 25%' }" :desktop="{ flex: '1 1 20%' }" :style="{ 'border': 'solid 1px #e5e5e5', 'margin-left': '-1px', 'margin-top': '-1px', padding: 0 }" v-for="item in 10" :key="item">
           <div :style="{ height: '160px', display: 'flex', 'justify-content': 'center', 'align-items': 'center', background: '#fff' }">
             <h5>奈好</h5>
           </div>
-        </mn-column>
-      </mn-columns>
+        </mn-flex>
+      </mn-flexes>
     </mn-section>
   </mn-container>
 </template>
@@ -45,8 +45,8 @@
   import CardItem from 'vue-human/components/card/card-item'
   import CardBody from 'vue-human/components/card/card-body'
 
-  import Columns from 'vue-human/components/column/columns'
-  import Column from 'vue-human/components/column/column'
+  import flexes from 'vue-human/components/flex/flexes'
+  import flex from 'vue-human/components/flex/flex'
 
   export default {
     components: {
@@ -59,8 +59,8 @@
       [Card.name]: Card,
       [CardItem.name]: CardItem,
       [CardBody.name]: CardBody,
-      [Columns.name]: Columns,
-      [Column.name]: Column
+      ...flexes.inject(),
+      ...flex.inject()
     }
   }
 </script>
