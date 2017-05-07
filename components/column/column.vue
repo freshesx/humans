f19298f0e15616b57f311e1a549b799432272879
ChaselWu <minowu@foxmail.com>
2017-05-07 15:32:08 +0800

    optimize column

--- components/column/column.vue
+++ components/column/column.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mn-column" :style="[ computedStyle, sideStyle ]">
+  <div class="mn-column" :style="[ widthStyle, offsetStyle, orderStyle, paddingStyle]">
     <slot></slot>
   </div>
 </template>
@@ -7,7 +7,6 @@
 <script>
   import Element from '../../util/Element'
   import media from './media'
-  import isUndefined from 'lodash/isUndefined'
   import isString from 'lodash/isString'
 
   export default new Element({
@@ -15,42 +14,104 @@
     mixins: [ media ],
     data () {
       return {
-        leftNoneSpan: false,
-        rightNoneSpan: false
+        /**
+         * Clear padding-left
+         *
+         * @type {Boolean}
+         */
+        clearLeftPadding: false,
+
+        /**
+         * Clear padding-right
+         *
+         * @type {Boolean}
+         */
+        clearRightPadding: false
       }
     },
     computed: {
-      current () {
-        // Return default span
-        if (isUndefined(this.adaptedMedia)) {
-          return { span: 12 }
+      /**
+       * Adapted column config
+       * @todo Convert object media query
+       *
+       * @return {Object}
+       */
+      columnConfig () {
+        let columnConfig = { span: 12, offset: 0, order: 0 }
+
+        if (isString(this.adaptedMedia)) {
+          Object.assign(columnConfig, this.convertQuery(this.adaptedMedia))
         }
 
-        const adaptedMedia = this.adaptedMedia
+        return columnConfig
+      },
 
-        if (isString(adaptedMedia)) {
-          return {
-            span: parseInt(adaptedMedia)
-          }
-        }
-      },
-      currentSpan () {
-        return this.current.span
-      },
-      computedStyle () {
-        const current = this.current
+      /**
+       * Flex width
+       *
+       * @return {Object}
+       */
+      widthStyle () {
         return {
-          flex: 'none',
-          width: `${(100 / 12) * current.span}%`
+          width: `${(100 / 12) * this.columnConfig.span}%`
         }
       },
-      sideStyle () {
-        const style = {}
 
-        if (this.leftNoneSpan) style['padding-left'] = 0
-        if (this.rightNoneSpan) style['padding-right'] = 0
+      /**
+       * Flex marginLeft
+       *
+       * @return {Object}
+       */
+      offsetStyle () {
+        return {
+          'margin-left': this.columnConfig.offset > 0
+            ? `${(100 / 12) * this.columnConfig.offset}%`
+            : undefined
+        }
+      },
 
-        return style
+      /**
+       * Flex order
+       *
+       * @return {Object}
+       */
+      orderStyle () {
+        return {
+          order: this.columnConfig.order > 0
+            ? this.columnConfig.order
+            : undefined
+        }
+      },
+
+      /**
+       * Flex padding
+       *
+       * @return {Object}
+       */
+      paddingStyle () {
+        return {
+          'padding-left': this.clearLeftPadding ? 0 : undefined,
+          'padding-right': this.clearRightPadding ? 0 : undefined
+        }
+      }
+    },
+    methods: {
+      /**
+       * Convert string media value to number object
+       *
+       * @method convertQuery
+       * @param  {String}     mediaValue
+       * @return {Object}
+       */
+      convertQuery (mediaValue) {
+        const splitReg = /,\s*/
+        const [span, offset, order] = mediaValue.split(splitReg)
+
+        return {
+          span: span ? parseInt(span) : undefined,
+          offset: offset ? parseInt(offset) : undefined,
+          order: order ? parseInt(order) : undefined
+        }
       }
     }
   })
