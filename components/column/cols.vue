8db21baf5b65c3ef50bd0c14707b16a004eb0444
ChaselWu <minowu@foxmail.com>
2017-05-07 22:16:40 +0800

    Change col and cols, and add warn

--- components/column/cols.vue
+++ components/column/cols.vue
@@ -1,38 +1,9 @@
-<template>
-  <div class="mn-cols" :class="{ 'is-gapless': gapless }">
-    <slot></slot>
-  </div>
-</template>
-
 <script>
   import Element from '../../util/Element'
+  import columns from './columns'
 
   export default new Element({
     name: 'mn-cols',
-    props: {
-      gapless: {
-        type: Boolean,
-        default: false
-      }
-    }
+    mixins: [ columns ]
   })
 </script>
-
-<style lang="scss">
-  @import "../../scss/vars";
-
-  .mn-cols {
-    display: flex;
-    margin: 0 $grid-gutters * -0.5;
-    flex-wrap: wrap;
-
-    &.is-gapless {
-      margin: 0;
-
-      // @affect(./col.vue)
-      > .mn-col {
-        padding: 0;
-      }
-    }
-  }
-</style>
