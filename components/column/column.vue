c6a927b031be9aa560cd6dfdd88aec2f7a6acf14
ChaselWu <minowu@foxmail.com>
2017-05-07 09:30:52 +0800

    New column component

---
+++ components/column/column.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="mn-column" :style="[ computedStyle, sideStyle ]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Element from '../../util/Element'
+  import media from './media'
+  import isUndefined from 'lodash/isUndefined'
+  import isString from 'lodash/isString'
+
+  export default new Element({
+    name: 'mn-column',
+    mixins: [ media ],
+    data () {
+      return {
+        leftNoneSpan: false,
+        rightNoneSpan: false
+      }
+    },
+    computed: {
+      current () {
+        if (isUndefined(this.mediaStyle)) {
+          return { span: 12 }
+        }
+
+        const mediaStyle = this.mediaStyle
+
+        if (isString(mediaStyle)) {
+          return {
+            span: parseInt(mediaStyle)
+          }
+        }
+      },
+      currentSpan () {
+        return this.current.span
+      },
+      computedStyle () {
+        const current = this.current
+        return {
+          flex: 'none',
+          width: `${(100 / 12) * current.span}%`
+        }
+      },
+      sideStyle () {
+        const style = {}
+
+        if (this.leftNoneSpan) style['padding-left'] = 0
+        if (this.rightNoneSpan) style['padding-right'] = 0
+
+        return style
+      }
+    }
+  })
+</script>
+
+<style lang="scss">
+  @import "../../scss/vars";
+
+  .mn-column {
+    width: 100%;
+    padding: 0 $grid-gutters * 0.5;
+  }
+</style>
