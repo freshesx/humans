89b49ed6f6a69fc98583b8a7ee5909e5ad3fb85b
ChaselWu <minowu@foxmail.com>
2017-05-08 00:16:35 +0800

    Upgrade image example

--- examples/src/components/image/index.vue
+++ examples/src/components/image/index.vue
@@ -1,48 +1,34 @@
 <template>
   <mn-scroller>
-    <mn-section>
-      <mn-cols>
-        <mn-col tablet="ten, one" desktop="eight, two">
-          <mn-letter>
-            <mn-letter-body>
-              <h1>Hi</h1>
-              <small>I am Vue Human!</small>
-            </mn-letter-body>
-          </mn-letter>
+    <mn-container>
+      <mn-section>
+        <mn-columns>
+          <mn-column tablet="10, 1" desktop="8, 2">
+            <mn-letter>
+              <mn-letter-body>
+                <h1>Hi</h1>
+                <small>I am Vue Human!</small>
+              </mn-letter-body>
+            </mn-letter>
 
-          <mn-card>
-            <mn-card-media>
-              <mn-image :srcset="imageSets"></mn-image>
-            </mn-card-media>
-          </mn-card>
-        </mn-col>
-      </mn-cols>
-    </mn-section>
+            <mn-card>
+              <mn-card-media>
+                <mn-image :srcset="imageSets"></mn-image>
+              </mn-card-media>
+            </mn-card>
+          </mn-column>
+        </mn-columns>
+      </mn-section>
+    </mn-container>
   </mn-scroller>
 </template>
 
 <script>
-  import scroller from 'vue-human/components/scroller/scroller'
-  import section from 'vue-human/components/section/section'
-  import cols from 'vue-human/components/column/cols'
-  import col from 'vue-human/components/column/col'
-  import letter from 'vue-human/components/letter/letter'
-  import letterBody from 'vue-human/components/letter/letter-body'
-  import card from 'vue-human/components/card/card'
-  import cardMedia from 'vue-human/components/card/card-media'
-  import image from 'vue-human/components/image/image'
+  import coreSuits from 'vue-human/components/coreSuits'
 
   export default {
     components: {
-      ...scroller.inject(),
-      ...section.inject(),
-      ...cols.inject(),
-      ...col.inject(),
-      ...letter.inject(),
-      ...letterBody.inject(),
-      ...card.inject(),
-      ...cardMedia.inject(),
-      ...image.inject()
+      ...coreSuits.map()
     },
     data () {
       return {
