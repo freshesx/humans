c33f6c987821cdca26370aa1cc446fd562c17709
ChaselWu <minowu@foxmail.com>
2017-05-08 15:42:07 +0800

    Change media to order

--- components/column/media.js
+++ components/column/media.js
@@ -1,51 +1,20 @@
-import Human from '../../index'
+import get from 'lodash/get'
 
 export default {
-  props: {
-    mobile: {
-      type: [String, Object]
-    },
-    tablet: {
-      type: [String, Object]
-    },
-    desktop: {
-      type: [String, Object]
-    },
-    widescreen: {
-      type: [String, Object]
-    }
-  },
   data () {
     return {
       offsetWidth: 0
     }
   },
   computed: {
-    /**
-     * Adapted media query config
-     *
-     * @return {Object}
-     */
-    adaptedMediaConfig () {
-      let screens = Human.$screens.filter(item => {
-        return this.offsetWidth > item.min && typeof this[item.name] !== 'undefined'
-      }).reverse()
-
-      // 取结果集中的最后一个匹配值
-      return screens.length > 0 ? this[screens[0].name] : {}
+    adaptedConfig () {
+      return this.mediaQueries.filter(item => {
+        return this.offsetWidth > item.min
+      }).reverse()[0]
     },
 
-    /**
-     * Adapted media query's name
-     *
-     * @return {String}
-     */
-    adaptedMediaName () {
-      let screens = Human.$screens.filter(item => {
-        return this.offsetWidth > item.min
-      }).reverse()
-
-      return screens.length > 0 ? screens[0].name : 'mobile'
+    orderStyle () {
+      return { order: get(this, 'adaptedConfig.order') }
     }
   },
   methods: {
