8631a79e0d3f2a4d05229551c65534ba5aba33d8
ChaselWu <minowu@foxmail.com>
2017-05-08 12:30:23 +0800

    Add carousel example

--- examples/src/components/carousel/index.vue
+++ examples/src/components/carousel/index.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>carousel</div>
+  <mn-page>
+    <mn-scroller>
+      <mn-container>
+        <mn-columns>
+          <mn-column tablet="8, 2" desktop="6, 3">
+            <mn-card>
+              <mn-card-media>
+                <mn-carousel>
+                  <mn-carousel-item v-for="item in pictures" :key="item">
+                    <img :src="item.src" style="width: 100%; display: block;">
+                  </mn-carousel-item>
+                </mn-carousel>
+              </mn-card-media>
+            </mn-card>
+
+            <mn-card>
+              <mn-card-item>
+                <mn-card-body>
+                  @todo How to use image component
+                </mn-card-body>
+              </mn-card-item>
+            </mn-card>
+          </mn-column>
+        </mn-columns>
+      </mn-container>
+    </mn-scroller>
+  </mn-page>
 </template>
+
+<script>
+  import coreSuits from 'vue-human/components/coreSuits'
+  import carouselSuits from 'vue-human/components/carousel'
+
+  export default {
+    components: {
+      ...coreSuits.map(),
+      ...carouselSuits.map()
+    },
+    data () {
+      return {
+        pictures: [
+          { src: 'https://ois1yok9v.qnssl.com/home-banner-1.jpg' },
+          { src: 'https://ois1yok9v.qnssl.com/home-banner-2.jpg' },
+          { src: 'https://ois1yok9v.qnssl.com/home-banner-3.jpg' }
+        ]
+      }
+    }
+  }
+</script>
