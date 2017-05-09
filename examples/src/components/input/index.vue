966a1b0d1b2a37a318b1c984229cb449647b5f7a
ChaselWu <minowu@foxmail.com>
2017-05-09 13:34:44 +0800

    Enhance input example.

--- examples/src/components/input/index.vue
+++ examples/src/components/input/index.vue
@@ -1,23 +1,41 @@
 <template>
-  <mn-card>
-    <mn-card-item>
-      <mn-card-prefix>
-        Username
-      </mn-card-prefix>
-      <mn-card-body>
-        <mn-input v-model="models.username" placeholder="Username"></mn-input>
-      </mn-card-body>
-    </mn-card-item>
-  </mn-card>
+  <mn-page>
+    <mn-scroller>
+      <mn-container>
+        <mn-letter>
+          <mn-letter-body><h1>Input</h1></mn-letter-body>
+        </mn-letter>
+
+        <mn-form>
+          <mn-section>
+            <mn-card>
+              <mn-card-item>
+                <mn-card-prefix>
+                  Username
+                </mn-card-prefix>
+                <mn-card-body>
+                  <mn-input v-model="models.username" placeholder="Username"></mn-input>
+                </mn-card-body>
+              </mn-card-item>
+            </mn-card>
+          </mn-section>
+
+          <mn-btn theme="primary">Sumbit</mn-btn>
+        </mn-form>
+      </mn-container>
+    </mn-scroller>
+  </mn-page>
 </template>
 
 <script>
-  import cardSuit from 'vue-human/components/card'
+  import coreSuit from 'vue-human/components/coreSuits'
+  import formSuit from 'vue-human/components/formSuits'
   import inputSuit from 'vue-human/components/input'
 
   export default {
     components: {
-      ...cardSuit.map(),
+      ...coreSuit.map(),
+      ...formSuit.map(),
       ...inputSuit.map()
     },
     data () {
