ea2f3545729bb2b5a888380ff7e64c1186317115
ChaselWu <minowu@foxmail.com>
2017-05-12 23:24:29 +0800

    Sperate home menu in example

---
+++ examples/src/components/base/menu.js
@@ -0,0 +1,134 @@
+export default {
+  data () {
+    return {
+      menu: [
+        {
+          name: this.$t('examples.menus.core'),
+          children: [
+            {
+              name: 'Button',
+              small: this.$t('examples.submenus.core.button'),
+              push: { name: 'button' }
+            },
+            {
+              name: 'Icon',
+              small: this.$t('examples.submenus.core.icon'),
+              push: { name: 'icon' }
+            },
+            {
+              name: 'Scroller',
+              small: this.$t('examples.submenus.core.scroller'),
+              push: { name: 'scroller' }
+            },
+            {
+              name: 'Container',
+              small: this.$t('examples.submenus.core.container'),
+              push: { name: 'container' }
+            },
+            {
+              name: 'Column',
+              small: this.$t('examples.submenus.core.column'),
+              push: { name: 'column' }
+            }
+          ]
+        },
+        {
+          name: this.$t('examples.menus.popup'),
+          children: [
+            // @todo update to custom popup docs
+            // {
+            //   name: 'Popup',
+            //   small: this.$t('examples.submenus.popup.popup'),
+            //   push: { name: 'popup' }
+            // },
+            {
+              name: 'Message',
+              small: this.$t('examples.submenus.popup.message'),
+              push: { name: 'message' }
+            },
+            {
+              name: 'Confirm',
+              small: this.$t('examples.submenus.popup.confirm'),
+              push: { name: 'confirm' }
+            },
+            {
+              name: 'Alert',
+              small: this.$t('examples.submenus.popup.alert'),
+              push: { name: 'alert' }
+            },
+            {
+              name: 'LoadingMask',
+              small: this.$t('examples.submenus.popup.loadingMask'),
+              push: { name: 'loadingMask' }
+            },
+            {
+              name: 'Modal',
+              small: this.$t('examples.submenus.popup.modal'),
+              push: { name: 'modal' }
+            },
+            {
+              name: 'ActionSheet',
+              small: this.$t('examples.submenus.popup.actionSheet'),
+              push: { name: 'actionSheet' }
+            }
+          ]
+        },
+        {
+          name: this.$t('examples.menus.form'),
+          children: [
+            {
+              name: 'Form',
+              small: this.$t('examples.submenus.form.form'),
+              push: { name: 'form' }
+            },
+            {
+              name: 'Input',
+              small: this.$t('examples.submenus.form.input'),
+              push: { name: 'input' }
+            },
+            {
+              name: 'Check',
+              small: this.$t('examples.submenus.form.check'),
+              push: { name: 'check' }
+            },
+            {
+              name: 'Counter',
+              small: this.$t('examples.submenus.form.counter'),
+              push: { name: 'counter' }
+            }
+          ]
+        },
+        {
+          name: this.$t('examples.menus.components'),
+          children: [
+            {
+              name: 'Image',
+              small: this.$t('examples.submenus.core.image'),
+              push: { name: 'image' }
+            },
+            {
+              name: 'Paginate',
+              small: this.$t('examples.submenus.components.paginate'),
+              push: { name: 'paginate' }
+            },
+            {
+              name: 'Cell',
+              small: this.$t('examples.submenus.components.cell'),
+              push: { name: 'cell' }
+            },
+            {
+              name: 'Carousel',
+              small: this.$t('examples.submenus.components.carousel'),
+              push: { name: 'carousel' }
+            },
+            {
+              name: 'Dashboard',
+              small: this.$t('examples.submenus.components.dashboard'),
+              push: { name: 'dashboard' }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
--- examples/src/components/home/index.vue
+++ examples/src/components/home/index.vue
@@ -10,14 +10,14 @@
 
     <mn-columns>
       <mn-column tablet="4" desktop="3"
-        v-for="(menu, menuIndex) in menus" :key="menuIndex">
+        v-for="(item, itemIndex) in menu" :key="itemIndex">
         <mn-section>
           <mn-section-note>
-            {{ menu.name }}
+            {{ item.name }}
           </mn-section-note>
           <mn-card>
-            <mn-card-item type="link" @click="$router.push(child.link)"
-              v-for="(child, childIndex) in menu.children" :key="childIndex">
+            <mn-card-item type="link" @click="$router.push(child.push)"
+              v-for="(child, childIndex) in item.children" :key="childIndex">
               <mn-card-body>
                 {{ child.name }}
               </mn-card-body>
@@ -38,146 +38,16 @@
   import letterSuit from 'vue-human/suites/letter'
   import cardSuit from 'vue-human/suites/card'
   import columnSuit from 'vue-human/suites/column'
+  import menu from '../base/menu'
 
   export default {
+    mixins: [ menu ],
     components: {
       ...containerSuit.map(),
       ...sectionSuit.map(),
       ...letterSuit.map(),
       ...cardSuit.map(),
       ...columnSuit.map()
-    },
-    data () {
-      return {
-        menus: [
-          {
-            name: this.$t('examples.menus.core'),
-            children: [
-              {
-                name: 'Button',
-                small: this.$t('examples.submenus.core.button'),
-                link: { name: 'button' }
-              },
-              {
-                name: 'Icon',
-                small: this.$t('examples.submenus.core.icon'),
-                link: { name: 'icon' }
-              },
-              {
-                name: 'Scroller',
-                small: this.$t('examples.submenus.core.scroller'),
-                link: { name: 'scroller' }
-              },
-              {
-                name: 'Container',
-                small: this.$t('examples.submenus.core.container'),
-                link: { name: 'container' }
-              },
-              {
-                name: 'Column',
-                small: this.$t('examples.submenus.core.column'),
-                link: { name: 'column' }
-              }
-            ]
-          },
-          {
-            name: this.$t('examples.menus.popup'),
-            children: [
-              // @todo update to custom popup docs
-              // {
-              //   name: 'Popup',
-              //   small: this.$t('examples.submenus.popup.popup'),
-              //   link: { name: 'popup' }
-              // },
-              {
-                name: 'Message',
-                small: this.$t('examples.submenus.popup.message'),
-                link: { name: 'message' }
-              },
-              {
-                name: 'Confirm',
-                small: this.$t('examples.submenus.popup.confirm'),
-                link: { name: 'confirm' }
-              },
-              {
-                name: 'Alert',
-                small: this.$t('examples.submenus.popup.alert'),
-                link: { name: 'alert' }
-              },
-              {
-                name: 'LoadingMask',
-                small: this.$t('examples.submenus.popup.loadingMask'),
-                link: { name: 'loadingMask' }
-              },
-              {
-                name: 'Modal',
-                small: this.$t('examples.submenus.popup.modal'),
-                link: { name: 'modal' }
-              },
-              {
-                name: 'ActionSheet',
-                small: this.$t('examples.submenus.popup.actionSheet'),
-                link: { name: 'actionSheet' }
-              }
-            ]
-          },
-          {
-            name: this.$t('examples.menus.form'),
-            children: [
-              {
-                name: 'Form',
-                small: this.$t('examples.submenus.form.form'),
-                link: { name: 'form' }
-              },
-              {
-                name: 'Input',
-                small: this.$t('examples.submenus.form.input'),
-                link: { name: 'input' }
-              },
-              {
-                name: 'Check',
-                small: this.$t('examples.submenus.form.check'),
-                link: { name: 'check' }
-              },
-              {
-                name: 'Counter',
-                small: this.$t('examples.submenus.form.counter'),
-                link: { name: 'counter' }
-              }
-            ]
-          },
-          {
-            name: this.$t('examples.menus.components'),
-            children: [
-              {
-                name: 'Image',
-                small: this.$t('examples.submenus.core.image'),
-                link: { name: 'image' }
-              },
-              {
-                name: 'Paginate',
-                small: this.$t('examples.submenus.components.paginate'),
-                link: { name: 'paginate' }
-              },
-              {
-                name: 'Cell',
-                small: this.$t('examples.submenus.components.cell'),
-                link: { name: 'cell' }
-              },
-              {
-                name: 'Carousel',
-                small: this.$t('examples.submenus.components.carousel'),
-                link: { name: 'carousel' }
-              },
-              {
-                name: 'Dashboard',
-                small: this.$t('examples.submenus.components.dashboard'),
-                link: { name: 'dashboard' }
-              }
-            ]
-          }
-        ]
-      }
     }
   }
 </script>
