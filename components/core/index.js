1376d7b3f198b134c816397dd234529c9b1bab01
ChaselWu <minowu@foxmail.com>
2017-05-12 13:06:41 +0800

    Combine coreSuits and formSuits, and use components/core

---
+++ components/core/index.js
@@ -0,0 +1,32 @@
+import Suits from '../../util/Suits'
+import iconSuits from '../icon'
+import loadingIconSuits from '../loading-icon'
+import scrollerSuits from '../scroller'
+import buttonSuits from '../button'
+import cardSuits from '../card'
+import containerSuits from '../container'
+import sectionSuits from '../section'
+import columnSuits from '../column'
+import rowSuits from '../row'
+import letterSuits from '../letter'
+import pageSuits from '../page'
+import formSuits from '../form'
+import labelSuits from '../label'
+import helperSuits from '../helper'
+
+export default new Suits([
+  iconSuits,
+  loadingIconSuits,
+  scrollerSuits,
+  buttonSuits,
+  cardSuits,
+  containerSuits,
+  sectionSuits,
+  columnSuits,
+  rowSuits,
+  letterSuits,
+  pageSuits,
+  formSuits,
+  labelSuits,
+  helperSuits
+])
