3ef2a1096b21946ee155a4f359d3dc29b8c7b42e
ChaselWu <minowu@foxmail.com>
2017-05-12 14:23:40 +0800

    Change Card to card in card/index.js

--- suites/card/index.js
+++ suites/card/index.js
@@ -1,5 +1,5 @@
 import Suites from '../../utils/Suites'
-import Card from './card'
+import card from './card'
 import CardItem from './card-item'
 import CardBody from './card-body'
 import CardPrefix from './card-prefix'
@@ -8,7 +8,7 @@ import CardMedia from './card-media'
 import CardBtns from './card-btns'
 
 export default new Suites([
-  Card,
+  card,
   CardItem,
   CardBody,
   CardPrefix,
