c699d090ebd5c6e6bcb394687ed5c298a738ee97
ChaselWu <minowu@foxmail.com>
2017-05-12 12:27:19 +0800

    Add new Suits test file

---
+++ test/unit/specs/util/Suits.spec.js
@@ -0,0 +1,54 @@
+import Suits from 'vue-human/util/Suits'
+import iconSuits from 'vue-human/components/icon'
+import iconElement from 'vue-human/components/icon/icon'
+
+describe('util/Suits', () => {
+  it('#constructor using suits', () => {
+    const suits = new Suits([iconSuits])
+    const components = suits.getComponents()
+
+    expect(components).to.have.lengthOf(1)
+    expect(components[0]).to.be.equal(iconElement)
+  })
+
+  it('#constructor using element', () => {
+    const suits = new Suits([iconElement])
+    const components = suits.getComponents()
+
+    expect(components).to.have.lengthOf(1)
+    expect(components[0]).to.be.equal(iconElement)
+  })
+
+  it('#map and mapComponents', () => {
+    const suits = new Suits([iconElement])
+    const map = suits.map()
+    const icon = map['mn-icon']
+
+    expect(icon).to.be.equal(iconElement)
+  })
+
+  it('#install with component install', () => {
+    let installedComponent = undefined
+    const suits = new Suits([iconElement])
+    const Vue = {
+      use (component) {
+        installedComponent = component
+      }
+    }
+    suits.install(Vue)
+    expect(installedComponent).to.be.equal(iconElement)
+  })
+
+  it('#install without component install', () => {
+    let installedComponent = undefined
+    const element = { name: 'test-component' }
+    const suits = new Suits([element])
+    const Vue = {
+      use (component) {
+        installedComponent = component
+      }
+    }
+    suits.install(Vue)
+    expect(installedComponent).to.be.equal(undefined)
+  })
+})
