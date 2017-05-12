d42f90beaf320e22f62a0a9824465a59f62dc223
ChaselWu <minowu@foxmail.com>
2017-05-12 14:18:53 +0800

    Update counter example

--- examples/src/components/counter/index.vue
+++ examples/src/components/counter/index.vue
@@ -25,28 +25,13 @@
 </template>
 
 <script>
-  import Container from 'vue-human/suites/container/container'
-  import Section from 'vue-human/suites/section/section'
-  import SectionNote from 'vue-human/suites/section/section-note'
-  import Letter from 'vue-human/suites/letter/letter'
-  import LetterBody from 'vue-human/suites/letter/letter-body'
-  import Card from 'vue-human/suites/card/card'
-  import CardItem from 'vue-human/suites/card/card-item'
-  import CardBody from 'vue-human/suites/card/card-body'
-
-  import Counter from 'vue-human/suites/counter/counter'
+  import core from 'vue-human/suites/core'
+  import counter from 'vue-human/suites/counter'
 
   export default {
     components: {
-      [Container.name]: Container,
-      [Section.name]: Section,
-      [SectionNote.name]: SectionNote,
-      [Letter.name]: Letter,
-      [LetterBody.name]: LetterBody,
-      [Card.name]: Card,
-      [CardItem.name]: CardItem,
-      [CardBody.name]: CardBody,
-      [Counter.name]: Counter
+      ...core.map(),
+      ...counter.map()
     },
     data () {
       return {
