7c92a33e423085b4535851f45579eb66c55ce26a
ChaselWu <minowu@foxmail.com>
2017-05-12 13:22:08 +0800

    Change Suits to Suites

--- test/unit/specs/util/Suits.spec.js
+++ test/unit/specs/util/Suits.spec.js
@@ -1,27 +1,27 @@
-import Suits from 'vue-human/util/Suits'
-import iconSuits from 'vue-human/components/icon'
+import Suites from 'vue-human/util/Suites'
+import iconSuites from 'vue-human/components/icon'
 import iconElement from 'vue-human/components/icon/icon'
 
-describe('util/Suits', () => {
-  it('#constructor using suits', () => {
-    const suits = new Suits([iconSuits])
-    const components = suits.getComponents()
+describe('util/Suites', () => {
+  it('#constructor using suites', () => {
+    const suites = new Suites([iconSuites])
+    const components = suites.getComponents()
 
     expect(components).to.have.lengthOf(1)
     expect(components[0]).to.be.equal(iconElement)
   })
 
   it('#constructor using element', () => {
-    const suits = new Suits([iconElement])
-    const components = suits.getComponents()
+    const suites = new Suites([iconElement])
+    const components = suites.getComponents()
 
     expect(components).to.have.lengthOf(1)
     expect(components[0]).to.be.equal(iconElement)
   })
 
   it('#map and mapComponents', () => {
-    const suits = new Suits([iconElement])
-    const map = suits.map()
+    const suites = new Suites([iconElement])
+    const map = suites.map()
     const icon = map['mn-icon']
 
     expect(icon).to.be.equal(iconElement)
@@ -29,26 +29,26 @@ describe('util/Suits', () => {
 
   it('#install with component install', () => {
     let installedComponent
-    const suits = new Suits([iconElement])
+    const suites = new Suites([iconElement])
     const Vue = {
       use (component) {
         installedComponent = component
       }
     }
-    suits.install(Vue)
+    suites.install(Vue)
     expect(installedComponent).to.be.equal(iconElement)
   })
 
   it('#install without component install', () => {
     let installedComponent
     const element = { name: 'test-component' }
-    const suits = new Suits([element])
+    const suites = new Suites([element])
     const Vue = {
       use (component) {
         installedComponent = component
       }
     }
-    suits.install(Vue)
+    suites.install(Vue)
     expect(installedComponent).to.be.equal(undefined)
   })
 })
