3f77d09be21b3977a1db8d9d5d37932a9b841a73
ChaselWu <minowu@foxmail.com>
2017-05-14 01:26:41 +0800

    Optimize dashboard

--- examples/src/components/dashboard/index.vue
+++ examples/src/components/dashboard/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <mn-dashboard>
+  <mn-dashboard :status="statusMenu">
     <div class="brand" slot="brand">
       <mn-icon :name="icons.logo" :width="44"></mn-icon>
     </div>
-    <div slot="side">
+
+    <mn-dashboard-sidebar slot="sidebar" :show.sync="showSidebar">
       <mn-scroller name="dashboard-side">
         <mn-section class="has-one-padding-all">
           <mn-search-bar>
@@ -28,7 +29,14 @@
           </mn-card>
         </mn-section>
       </mn-scroller>
-    </div>
+    </mn-dashboard-sidebar>
+
+    <mn-dashboard-status slot="suffix"
+      :menu="statusMenu" :show.sync="showSidebar"></mn-dashboard-status>
+
+    <mn-scroller>
+      <div>Mainarea</div>
+    </mn-scroller>
   </mn-dashboard>
 </template>
 
@@ -52,7 +60,30 @@
         icons: {
           logo: require('vue-human-icons/js/ios/analytics')
         },
-        search: undefined
+        showSidebar: false,
+        search: undefined,
+        statusMenu: [
+          {
+            name: '首页',
+            icon: require('vue-human-icons/js/ios/world'),
+            push: { name: 'dashboard' }
+          },
+          {
+            name: '资料库',
+            icon: require('vue-human-icons/js/ios/box'),
+            push: { name: 'homepage' }
+          },
+          {
+            name: '购物袋',
+            icon: require('vue-human-icons/js/ios/cart'),
+            push: { name: 'homepage' }
+          },
+          {
+            name: '我的',
+            icon: require('vue-human-icons/js/ios/contact'),
+            push: { name: 'homepage' }
+          }
+        ]
       }
     }
   }
