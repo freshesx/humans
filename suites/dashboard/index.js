37f371b6edae6953116dfd6664857e9a74c2c57e
ChaselWu <minowu@foxmail.com>
2017-05-14 01:26:24 +0800

    Add dashboardStatus and dashboardSidebar

--- suites/dashboard/index.js
+++ suites/dashboard/index.js
@@ -1,8 +1,12 @@
 import Suites from '../../utils/Suites'
 import dashboard from './dashboard'
 import dashboardMenu from './dashboardMenu'
+import dashboardStatus from './dashboardStatus'
+import dashboardSidebar from './dashboardSidebar'
 
 export default new Suites([
   dashboard,
-  dashboardMenu
+  dashboardMenu,
+  dashboardStatus,
+  dashboardSidebar
 ])
