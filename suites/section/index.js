1e3c885b4a93d4a08f4dc2f5dbd1659475d08d00
ChaselWu <minowu@foxmail.com>
2017-05-14 17:46:37 +0800

    Add new section-btn component

--- suites/section/index.js
+++ suites/section/index.js
@@ -1,8 +1,10 @@
 import Suites from '../../utils/Suites'
 import section from './section'
 import sectionNote from './sectionNote'
+import sectionBtn from './sectionBtn'
 
 export default new Suites([
   section,
-  sectionNote
+  sectionNote,
+  sectionBtn
 ])
---
+++ suites/section/sectionBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mn-section-btn">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Element from '../../utils/Element'
+
+  export default new Element({
+    name: 'mn-section-btn'
+  })
+</script>
+
+<style lang="scss">
+  @import "../../scss/vars";
+  @import "../../scss/mixins/media";
+
+  .mn-section-btn {
+    padding: 0 $grid-gutters;
+    margin-bottom: $grid-gutters / 2;
+
+    @include min-screen(tablet) {
+      padding: 0;
+    }
+  }
+</style>
