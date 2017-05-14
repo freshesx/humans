430ef7aa5c59feb605e93622bf78d5ff7bd4eca6
ChaselWu <minowu@foxmail.com>
2017-05-14 11:23:01 +0800

    Change homepage

--- examples/src/components/home/index.vue
+++ examples/src/components/home/index.vue
@@ -1,35 +1,39 @@
 <template>
-  <mn-container>
-    <mn-section>
-      <mn-letter>
-        <mn-letter-body>
-          <h1>Vue Human</h1>
-        </mn-letter-body>
-      </mn-letter>
-    </mn-section>
-
-    <mn-columns>
-      <mn-column tablet="4" desktop="3"
-        v-for="(item, itemIndex) in menu" :key="itemIndex">
+  <mn-page>
+    <mn-scroller>
+      <mn-container fluid>
         <mn-section>
-          <mn-section-note>
-            {{ item.name }}
-          </mn-section-note>
-          <mn-card>
-            <mn-card-item type="link" @click="$router.push(child.push)"
-              v-for="(child, childIndex) in item.children" :key="childIndex">
-              <mn-card-body>
-                {{ child.name }}
-              </mn-card-body>
-              <mn-card-suffix>
-                <small>{{ child.small }}</small>
-              </mn-card-suffix>
-            </mn-card-item>
-          </mn-card>
+          <mn-letter>
+            <mn-letter-body>
+              <h1>Vue Human</h1>
+            </mn-letter-body>
+          </mn-letter>
         </mn-section>
-      </mn-column>
-    </mn-columns>
-  </mn-container>
+
+        <mn-columns>
+          <mn-column tablet="4" desktop="3"
+            v-for="(item, itemIndex) in menu" :key="itemIndex">
+            <mn-section>
+              <mn-section-note>
+                {{ item.name }}
+              </mn-section-note>
+              <mn-card>
+                <mn-card-item type="link" @click="$router.push(child.push)"
+                  v-for="(child, childIndex) in item.children" :key="childIndex">
+                  <mn-card-body>
+                    {{ child.name }}
+                  </mn-card-body>
+                  <mn-card-suffix>
+                    <small>{{ child.small }}</small>
+                  </mn-card-suffix>
+                </mn-card-item>
+              </mn-card>
+            </mn-section>
+          </mn-column>
+        </mn-columns>
+      </mn-container>
+    </mn-scroller>
+  </mn-page>
 </template>
 
 <script>
