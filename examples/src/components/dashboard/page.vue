8faa0e7a6220f8087544662001c1429c17a7d8ea
ChaselWu <minowu@foxmail.com>
2017-05-14 11:34:24 +0800

    New example page

---
+++ examples/src/components/dashboard/page.vue
@@ -0,0 +1,19 @@
+<template>
+  <mn-page>
+    <mn-scroller>
+      <mn-container>
+        <slot></slot>
+      </mn-container>
+    </mn-scroller>
+  </mn-page>
+</template>
+
+<script>
+  import core from 'vue-human/suites/core'
+
+  export default {
+    components: {
+      ...core.map()
+    }
+  }
+</script>
