03645d2e25f96102d12daa92a8061e3e0252bfe4
ChaselWu <minowu@foxmail.com>
2017-05-14 11:34:33 +0800

    Using page in example

--- examples/src/components/home/index.vue
+++ examples/src/components/home/index.vue
@@ -1,49 +1,47 @@
 <template>
-  <mn-page>
-    <mn-scroller>
-      <mn-container fluid>
-        <mn-section>
-          <mn-letter>
-            <mn-letter-body>
-              <h1>Vue Human</h1>
-            </mn-letter-body>
-          </mn-letter>
-        </mn-section>
+  <page>
+    <mn-section>
+      <mn-letter>
+        <mn-letter-body>
+          <h1>Vue Human</h1>
+        </mn-letter-body>
+      </mn-letter>
+    </mn-section>
 
-        <mn-columns>
-          <mn-column tablet="4" desktop="3"
-            v-for="(item, itemIndex) in menu" :key="itemIndex">
-            <mn-section>
-              <mn-section-note>
-                {{ item.name }}
-              </mn-section-note>
-              <mn-card>
-                <mn-card-item type="link" @click="$router.push(child.push)"
-                  v-for="(child, childIndex) in item.children" :key="childIndex">
-                  <mn-card-body>
-                    {{ child.name }}
-                  </mn-card-body>
-                  <mn-card-suffix>
-                    <small>{{ child.small }}</small>
-                  </mn-card-suffix>
-                </mn-card-item>
-              </mn-card>
-            </mn-section>
-          </mn-column>
-        </mn-columns>
-      </mn-container>
-    </mn-scroller>
-  </mn-page>
+    <mn-columns>
+      <mn-column tablet="4" desktop="3"
+        v-for="(item, itemIndex) in menu" :key="itemIndex">
+        <mn-section>
+          <mn-section-note>
+            {{ item.name }}
+          </mn-section-note>
+          <mn-card>
+            <mn-card-item type="link" @click="$router.push(child.push)"
+              v-for="(child, childIndex) in item.children" :key="childIndex">
+              <mn-card-body>
+                {{ child.name }}
+              </mn-card-body>
+              <mn-card-suffix>
+                <small>{{ child.small }}</small>
+              </mn-card-suffix>
+            </mn-card-item>
+          </mn-card>
+        </mn-section>
+      </mn-column>
+    </mn-columns>
+  </page>
 </template>
 
 <script>
   import core from 'vue-human/suites/core'
+  import page from '../dashboard/page'
   import menu from '../base/menu'
 
   export default {
     mixins: [ menu ],
     components: {
-      ...core.map()
+      ...core.map(),
+      page
     }
   }
 </script>
