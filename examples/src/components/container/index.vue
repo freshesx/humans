44dabb962c1968909caf8059fdcb9c2a4651eee9
ChaselWu <minowu@foxmail.com>
2017-05-14 17:02:32 +0800

    Optimize example style

--- examples/src/components/container/index.vue
+++ examples/src/components/container/index.vue
@@ -1,44 +1,42 @@
 <template>
-  <mn-page>
-    <mn-scroller>
-      <mn-container>
-        <mn-letter>
-          <mn-letter-body>
-            <h1>Container</h1>
-          </mn-letter-body>
-        </mn-letter>
+  <page>
+    <mn-letter>
+      <mn-letter-body>
+        <h1>Container</h1>
+      </mn-letter-body>
+    </mn-letter>
 
-        <mn-section>
-          <div class="example-tile">
-            <h1>Container</h1>
-            <small v-for="(name, key) in nameOfScreens">
-              {{ name }}
-              <span v-if="key !== nameOfScreens.length - 1"> / </span>
-            </small>
-          </div>
-        </mn-section>
+    <mn-section>
+      <div class="example-tile">
+        <h1>Container</h1>
+        <small v-for="(name, key) in nameOfScreens">
+          {{ name }}
+          <span v-if="key !== nameOfScreens.length - 1"> / </span>
+        </small>
+      </div>
+    </mn-section>
 
-        <mn-section>
-          <mn-card>
-            <mn-card-item>
-              <mn-card-body>
-                View mobile and desktop
-              </mn-card-body>
-            </mn-card-item>
-          </mn-card>
-        </mn-section>
-      </mn-container>
-    </mn-scroller>
-  </mn-page>
+    <mn-section>
+      <mn-card>
+        <mn-card-item>
+          <mn-card-body>
+            View mobile and desktop
+          </mn-card-body>
+        </mn-card-item>
+      </mn-card>
+    </mn-section>
+  </page>
 </template>
 
 <script>
   import core from 'vue-human/suites/core'
+  import page from '../base/page'
   import Human from 'vue-human'
 
   export default {
     components: {
-      ...core.map()
+      ...core.map(),
+      page
     },
     data () {
       return {
--- examples/src/components/scroller/index.vue
+++ examples/src/components/scroller/index.vue
@@ -1,48 +1,48 @@
 <template>
-  <mn-scroller>
-    <mn-container>
-      <mn-section>
-        <mn-letter>
-          <mn-letter-body>
-            <h1>Scroller <small>滚动条</small></h1>
-          </mn-letter-body>
-        </mn-letter>
-      </mn-section>
+  <page>
+    <mn-section>
+      <mn-letter>
+        <mn-letter-body>
+          <h1>Scroller <small>滚动条</small></h1>
+        </mn-letter-body>
+      </mn-letter>
+    </mn-section>
 
-      <mn-section>
-        <mn-card>
-          <mn-card-item>
-            <mn-card-body>
-              建议在手机版测试一下，在 mn-scroller 底下时启动 popup mask，
-              然后判断 touch 模式下，滚动条是否滚动。
-            </mn-card-body>
-          </mn-card-item>
-          <mn-card-item type="link" @click="onOpenConfirm">
-            <mn-card-body>
-              点击打开 popup 以测试
-            </mn-card-body>
-          </mn-card-item>
-        </mn-card>
-      </mn-section>
+    <mn-section>
+      <mn-card>
+        <mn-card-item>
+          <mn-card-body>
+            建议在手机版测试一下，在 mn-scroller 底下时启动 popup mask，
+            然后判断 touch 模式下，滚动条是否滚动。
+          </mn-card-body>
+        </mn-card-item>
+        <mn-card-item type="link" @click="onOpenConfirm">
+          <mn-card-body>
+            点击打开 popup 以测试
+          </mn-card-body>
+        </mn-card-item>
+      </mn-card>
+    </mn-section>
 
-      <mn-section>
-        <mn-card>
-          <mn-card-item v-for="(item, key) in 20" :key="key" type="arrow">
-            <mn-card-body>Row {{ item }}</mn-card-body>
-          </mn-card-item>
-        </mn-card>
-      </mn-section>
-    </mn-container>
-  </mn-scroller>
+    <mn-section>
+      <mn-card>
+        <mn-card-item v-for="(item, key) in 20" :key="key" type="arrow">
+          <mn-card-body>Row {{ item }}</mn-card-body>
+        </mn-card-item>
+      </mn-card>
+    </mn-section>
+  </page>
 </template>
 
 <script>
   import core from 'vue-human/suites/core'
+  import page from '../base/page'
   import Confirm from 'vue-human/utils/Confirm'
 
   export default {
     components: {
-      ...core.map()
+      ...core.map(),
+      page
     },
     methods: {
       onOpenConfirm () {
