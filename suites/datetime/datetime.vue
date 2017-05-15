d6a70eef9f941e0b2f6160c1393a9b12acf0b083
ChaselWu <minowu@foxmail.com>
2017-05-15 21:23:21 +0800

    Update datetime

--- suites/datetime/datetime.vue
+++ suites/datetime/datetime.vue
@@ -14,12 +14,12 @@
           <p class="mn-datetime-date">2017年12月17日 周一</p>
         </mn-card-suffix>
       </mn-card-item>
-      <mn-card-item style="height: 91px; padding-left: 0; padding-right: 0;">
+      <mn-card-item style="height: 91px;">
         <mn-card-body>
           <div class="mn-datetime-input">
             <div class="mn-datetime-item">
-              <select>
-                <option :value="option.value" v-for="option in yearOptions">{{ option.label }}</option>
+              <select v-model="models.fullYear">
+                <option :value="option.value" v-for="option in fullYearOptions">{{ option.label }}</option>
               </select>
             </div>
             <div class="mn-datetime-item">
@@ -28,18 +28,23 @@
               </select>
             </div>
             <div class="mn-datetime-item">
-              <select>
+              <select v-model="models.date">
                 <option :value="option.value" v-for="option in dateOptions">{{ option.label }}</option>
               </select>
             </div>
             <div class="mn-datetime-item">
-              <select>
-                <option :value="option.value" v-for="option in hourOptions">{{ option.label }}</option>
+              <select v-model="models.hours">
+                <option :value="option.value" v-for="option in hoursOptions">{{ option.label }}</option>
               </select>
             </div>
             <div class="mn-datetime-item">
-              <select>
-                <option :value="option.value" v-for="option in minOptions">{{ option.label }}</option>
+              <select v-model="models.minutes">
+                <option :value="option.value" v-for="option in minutesOptions">{{ option.label }}</option>
+              </select>
+            </div>
+            <div class="mn-datetime-item">
+              <select v-model="models.seconds">
+                <option :value="option.value" v-for="option in secondsOptions">{{ option.label }}</option>
               </select>
             </div>
           </div>
@@ -64,6 +69,7 @@
   import cardSuffix from '../card/cardSuffix'
   import cardBtns from '../card/cardBtns'
   import datetimePicker from './datetimePicker'
+  import { isLunarMonth, isLeapYear, isFebruary } from './dateChecker'
 
   export default new Element({
     name: 'mn-datetime',
@@ -83,34 +89,68 @@
        */
       popupManager
     ],
-    props: {
-      // value: {
-      //   type: null,
-      //   default: undefined,
-      //   required: true
-      // },
-      // readonly: {
-      //   type: Boolean,
-      //   default: false
-      // },
-      // disabled: {
-      //   type: Boolean,
-      //   default: false
-      // }
-    },
     data () {
       return {
+        datetime: '2017-05-15 19:19:00',
         models: {
-          month: 12
+          fullYear: 2017,
+          month: 0,    // 0 - 11
+          date: 1,
+          hours: 0,    // 0 - 23
+          minutes: 0,  // 0 - 59
+          seconds: 0    // 0 - 59
         }
       }
     },
+    methods: {
+      updateModels (datetime) {
+        const at = new Date(datetime)
+        this.models.fullYear = at.getFullYear()
+        console.log(this.models.fullYear)
+        this.models.month = at.getMonth()
+        this.models.date = at.getDate()
+        this.models.hours = at.getHours()
+        this.models.minutes = at.getMinutes()
+        this.models.seconds = at.getSeconds()
+      },
+      parseDoubleNumber (number) {
+        return number < 10 ? '0' + number : number
+      },
+      checkSomeDate () {
+        if (isLunarMonth(this.models.month) && this.models.date > 30) {
+          this.models.date = 30
+        }
+
+        // 平年 2 月份
+        if (!isLeapYear(this.models.fullYear) &&
+          isFebruary(this.models.month) && this.models.date > 28) {
+          this.models.date = 28
+        }
+
+        // 闰年 2 月份
+        if (isLeapYear(this.models.fullYear) &&
+          isFebruary(this.models.month) && this.models.date > 29) {
+          this.models.date = 29
+        }
+      }
+    },
+    watch: {
+      datetime (newValue) {
+        this.updateModels(newValue)
+      },
+      'models.fullYear' () {
+        this.checkSomeDate()
+      },
+      'models.month' () {
+        this.checkSomeDate()
+      }
+    },
     computed: {
-      yearOptions () {
+      fullYearOptions () {
         const options = []
 
         for (let i = 1970; i <= 2050; i++) {
-          options.push({ label: i, value: i })
+          options.push({ label: i + '年', value: i })
         }
 
         return options
@@ -118,39 +158,68 @@
       monthOptions () {
         const options = []
 
-        for (let i = 1; i <= 12; i++) {
-          options.push({ label: i + '月', value: i })
+        for (let i = 0; i <= 11; i++) {
+          options.push({ label: (i + 1) + '月', value: i })
         }
 
         return options
       },
       dateOptions () {
         const options = []
+        // 默认大月份
+        let max = 31
 
-        for (let i = 1; i <= 31; i++) {
+        // 小月份
+        if (isLunarMonth(this.models.month)) {
+          max = 30
+        }
+
+        // 平年 2 月
+        if (!isLeapYear(this.models.fullYear) && isFebruary(this.models.month)) {
+          max = 28
+        }
+
+        // 闰年 2 月
+        if (isLeapYear(this.models.fullYear) && isFebruary(this.models.month)) {
+          max = 29
+        }
+
+        for (let i = 1; i <= max; i++) {
           options.push({ label: i + ' 日', value: i })
         }
 
         return options
       },
-      hourOptions () {
+      hoursOptions () {
         const options = []
 
         for (let i = 0; i <= 23; i++) {
-          options.push({ label: '0' + i, value: i })
+          options.push({ label: this.parseDoubleNumber(i), value: i })
         }
 
         return options
       },
-      minOptions () {
+      minutesOptions () {
         const options = []
 
         for (let i = 0; i <= 59; i++) {
-          options.push({ label: '0' + i, value: i })
+          options.push({ label: this.parseDoubleNumber(i), value: i })
+        }
+
+        return options
+      },
+      secondsOptions () {
+        const options = []
+
+        for (let i = 0; i <= 59; i++) {
+          options.push({ label: this.parseDoubleNumber(i), value: i })
         }
 
         return options
       }
+    },
+    mounted () {
+      this.updateModels(this.datetime)
     }
   })
 </script>
@@ -196,18 +265,18 @@
   }
 
   .mn-datetime-item {
-    flex: 1;
+    flex: 1 1;
     text-align: center;
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-left: 0.2rem;
 
     > select {
       border: none;
       background: transparent;
       font-size: 1.5rem;
       font-weight: 300;
+      appearance: none;
     }
   }
 </style>
