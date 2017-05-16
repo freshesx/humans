f753f1d6203f52969d4a7ce61276a7f8c458e52a
ChaselWu <minowu@foxmail.com>
2017-05-16 13:52:21 +0800

    Rich datetime example

--- examples/src/components/datetime/index.vue
+++ examples/src/components/datetime/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    <button @click="onOpenSingle">Datetime single</button>
-    <button @click="onOpenRange">Datetime range</button>
-  </div>
+  <page>
+    <mn-letter>
+      <mn-letter-body><h1>Datetime</h1></mn-letter-body>
+    </mn-letter>
+
+    <mn-card>
+      <mn-card-item>
+        <mn-card-body>
+          <mn-btn theme="primary" @click="onOpenSingle">Datetime single</mn-btn>
+          <mn-btn theme="primary" @click="onOpenRange">Datetime range</mn-btn>
+        </mn-card-body>
+      </mn-card-item>
+    </mn-card>
+  </page>
 </template>
 
 <script>
+  import core from 'vue-human/suites/core'
+  import page from '../base/page'
   import Datetime from 'vue-human/utils/Datetime'
   import DatetimeRange from 'vue-human/utils/DatetimeRange'
 
   export default {
+    components: {
+      ...core.map(),
+      page
+    },
     methods: {
       onOpenSingle () {
         const popup = Datetime.create({ showDate: false }).show()
