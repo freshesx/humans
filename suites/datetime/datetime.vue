fac2a1e1a6a14b3a5910cf320809a9ed3a8c2acb
ChaselWu <minowu@foxmail.com>
2017-05-16 11:37:01 +0800

    Merge datetime and vars

--- suites/datetime/datetime.vue
+++ suites/datetime/datetime.vue
@@ -67,7 +67,6 @@
   import cardSuffix from '../card/cardSuffix'
   import cardBtns from '../card/cardBtns'
   import { isLunarMonth, isLeapYear, isFebruary } from './dateChecker'
-  import vars from './vars'
   import options from './options'
   import filters from './filters'
 
@@ -87,17 +86,63 @@
        * Add isShow, close(), show() mixins
        */
       popupManager,
-      vars,
       options,
       filters
     ],
+    data () {
+      return {
+        fromAt: new Date(), // 初始开始时间
+        minAt: new Date('1970-01-01 00:00:00'),  // 最小时间
+        maxAt: new Date('2049-12-31 23:59:59'),  // 最大时间
+        title: '选择时间日期',
+        cancelText: '取消',
+        confirmText: '确认',
+        showFullYear: true,
+        showHours: true,
+        showMintues: true,
+        showSeconds: true,
+
+        models: {
+          fullYear: 2017,
+          month: 0,
+          date: 1,
+          hours: 0,
+          minutes: 0,
+          seconds: 0
+        }
+      }
+    },
     methods: {
       onCancel () {
         this.$emit('cancel', this.fromAt)
       },
+
       onConfirm () {
         this.$emit('confirm', this.fromAt)
       },
+
+      updateModels (at) {
+        this.models = {
+          fullYear: at.getFullYear(),
+          month: at.getMonth(),
+          date: at.getDate(),
+          hours: at.getHours(),
+          minutes: at.getMinutes(),
+          seconds: at.getSeconds()
+        }
+      },
+
+      updateAt (models) {
+        const at = new Date()
+        at.setFullYear(models.fullYear)
+        at.setMonth(models.month)
+        at.setDate(models.date)
+        at.setHours(models.hours)
+        at.setMinutes(models.minutes)
+        at.setSeconds(models.seconds)
+        return at
+      },
+
       checkSpecialDate () {
         if (isLunarMonth(this.models.month) && this.models.date > 30) {
           this.models.date = 30
@@ -115,6 +160,26 @@
           this.models.date = 29
         }
       }
+    },
+    watch: {
+      models: {
+        deep: true,
+        handler (newValue) {
+          this.checkSpecialDate()
+          this.fromAt = this.updateAt(newValue)
+        }
+      }
+    },
+    created () {
+      if (!this.showMintues) {
+        this.fromAt.setMinutes(0)
+      }
+
+      if (!this.showSeconds) {
+        this.fromAt.setSeconds(0)
+      }
+
+      this.updateModels(this.fromAt)
     }
   })
 </script>
