2d4dafbd2e502df64002b9be54bc377f1bd20c21
ChaselWu <minowu@foxmail.com>
2017-05-16 13:45:02 +0800

    Datetime example

--- examples/src/components/datetime/index.vue
+++ examples/src/components/datetime/index.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <button @click="onClick">Datetime</button>
+    <button @click="onOpenSingle">Datetime single</button>
+    <button @click="onOpenRange">Datetime range</button>
   </div>
 </template>
 
 <script>
   import Datetime from 'vue-human/utils/Datetime'
+  import DatetimeRange from 'vue-human/utils/DatetimeRange'
 
   export default {
     methods: {
-      onClick () {
-        Datetime.create().show()
+      onOpenSingle () {
+        const popup = Datetime.create({ showDate: false }).show()
+        popup.$on('confirm', display => {
+          console.log(display)
+        })
+      },
+      onOpenRange () {
+        DatetimeRange.fromAt({ showSeconds: false })
+          .toAt({ showSeconds: false })
+          .show()
+          .change(formats => {
+            console.log(formats)
+          })
       }
     }
   }
