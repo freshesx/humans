52c1987c6561ee653d8dfc33b11d09dcdb5405c1
ChaselWu <minowu@foxmail.com>
2017-05-18 16:43:50 +0800

    Update dashboard breadcrumb example

--- examples/src/components/dashboard/breadcrumb.vue
+++ examples/src/components/dashboard/breadcrumb.vue
@@ -1,15 +1,21 @@
 <template>
-  <mn-dashboard-breadcrumb></mn-dashboard-breadcrumb>
+  <mn-dashboard-breadcrumb :show.sync="storage.showSidebar"></mn-dashboard-breadcrumb>
 </template>
 
 <script>
   import core from 'vue-human/suites/core'
   import dashboard from 'vue-human/suites/dashboard'
+  import storage from './storage'
 
   export default {
     components: {
       ...core.map(),
       ...dashboard.map()
+    },
+    data () {
+      return {
+        storage
+      }
     }
   }
 </script>
