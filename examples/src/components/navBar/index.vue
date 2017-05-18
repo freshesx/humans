267eba41f04c3150e1f42bcdf2b7c395c65bbfa6
ChaselWu <minowu@foxmail.com>
2017-05-18 11:19:46 +0800

    Add navBar example

--- examples/src/components/navBar/index.vue
+++ examples/src/components/navBar/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>navBar</div>
+  <page>
+    <mn-letter>
+      <mn-letter-body>
+        <h1>Nav Bar</h1>
+      </mn-letter-body>
+    </mn-letter>
+
+    <mn-nav-bar>
+      <mn-nav-bar-back slot="prefix">返回</mn-nav-bar-back>
+      <mn-nav-bar-title slot="body">HumanUI</mn-nav-bar-title>
+    </mn-nav-bar>
+  </page>
 </template>
+
+<script>
+  import core from 'vue-human/suites/core'
+  import navBar from 'vue-human/suites/navBar'
+  import page from '../base/page'
+
+  export default {
+    components: {
+      ...core.map(),
+      ...navBar.map(),
+      page
+    }
+  }
+</script>
