31048624eedad2543c0ab4c41f3a51cc26a099b7
ChaselWu <minowu@foxmail.com>
2017-05-19 12:27:50 +0800

    Remove dashboard-container, using container fluid

--- examples/src/components/base/page.vue
+++ examples/src/components/base/page.vue
@@ -1,21 +1,19 @@
 <template>
   <mn-page>
     <mn-scroller>
-      <mn-dashboard-container>
+      <mn-container fluid>
         <slot></slot>
-      </mn-dashboard-container>
+      </mn-container>
     </mn-scroller>
   </mn-page>
 </template>
 
 <script>
   import core from 'vue-human/suites/core'
-  import dashboard from 'vue-human/suites/dashboard'
 
   export default {
     components: {
-      ...core.map(),
-      ...dashboard.map()
+      ...core.map()
     }
   }
 </script>
