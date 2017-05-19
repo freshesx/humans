3e666f84b6160234235693d29f0f60c9258c5f2f
ChaselWu <minowu@foxmail.com>
2017-05-19 12:29:51 +0800

    Remove example navbar

--- examples/src/components/dashboard/layout.vue
+++ examples/src/components/dashboard/layout.vue
@@ -21,7 +21,6 @@
   import core from 'vue-human/suites/core'
   import dashboard from 'vue-human/suites/dashboard'
   import sidebar from './sidebar'
-  import navbar from './navbar'
   import breadcrumb from './breadcrumb'
 
   export default {
@@ -29,7 +28,6 @@
       ...core.map(),
       ...dashboard.map(),
       sidebar,
-      navbar,
       breadcrumb
     },
     data () {
