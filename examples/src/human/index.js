6096dd09f5285aec8a30aa5b72f8b7a04dc5a88f
ChaselWu <minowu@foxmail.com>
2017-05-19 14:01:52 +0800

    Using core in human file

--- examples/src/human/index.js
+++ examples/src/human/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Human from 'vue-human'
+import core from 'vue-human/suites/core'
 import locales from '../locale'
 import './app.scss'
 
 // Use Human
 Vue.use(Human, { locales })
+
+Vue.use(core)
