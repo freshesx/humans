82f787d08dc474a0286a2a44d29538bd26094c1d
ChaselWu <minowu@foxmail.com>
2017-05-21 17:23:02 +0800

    Add Env test file

---
+++ test/unit/specs/utils/Env.spec.js
@@ -0,0 +1,60 @@
+import Env from 'vue-human/utils/Env'
+
+describe('utils/Env', () => {
+  it('#all', () => {
+    const state = Env.all()
+
+    expect(state).to.be.deep.equal({
+      _$debug: true,
+      _$info: false
+    })
+  })
+
+  it('#merge', () => {
+    const params1 = {
+      _$debug: false,
+      userToken: {
+        name: 'MinoWu'
+      }
+    }
+
+    const params2 = {
+      userToken: {
+        name: 'ChaselWu',
+        email: 'chasel.wu@vitualreaty.com'
+      }
+    }
+
+    const env = Env.merge(params1, params2)
+
+    const state = env.all()
+
+    expect(state.userToken.name).to.be.equal('ChaselWu')
+    expect(state._$debug).to.be.false
+  })
+
+  it('#get', () => {
+    const userToken = Env.get('userToken')
+
+    expect(userToken).to.be.deep.equal({
+      name: 'ChaselWu',
+      email: 'chasel.wu@vitualreaty.com'
+    })
+  })
+
+  it('#set', () => {
+    const user = {
+      name: 'Sun',
+      email: 'sun@example.com'
+    }
+
+    const env = Env.set('userToken', user)
+
+    const userToken = env.get('userToken')
+
+    expect(userToken).to.be.deep.equal({
+      name: 'Sun',
+      email: 'sun@example.com'
+    })
+  })
+})
