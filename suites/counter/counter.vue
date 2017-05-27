ff6d9ddf65ce79d4d7a92e11613744598bbc9fd8
ChaselWu <minowu@foxmail.com>
2017-05-27 21:45:08 +0800

    Change

--- suites/counter/counter.vue
+++ suites/counter/counter.vue
@@ -107,60 +107,60 @@
   .mn-counter {
     display: flex;
     max-width: 8rem;
+  }
 
-    &-box {
-      flex-shrink: 1;
-      border-top: solid 1px #ddd;
-      border-bottom: solid 1px #ddd;
+  .mn-counter-box {
+    flex-shrink: 1;
+    border-top: solid 1px #ddd;
+    border-bottom: solid 1px #ddd;
+  }
+
+  .mn-counter-control {
+    width: 100%;
+    display: block;
+    padding: 0.25rem 0.5rem;
+    text-align: center;
+    outline: none;
+    appearance: none;
+    -webkit-tap-highlight-color: transparent;
+    border: none;
+    background: transparent;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none !important;
+      margin: 0;
     }
 
-    &-control {
-      width: 100%;
-      display: block;
-      padding: 0.25rem 0.5rem;
-      text-align: center;
+    &[type="number"] {
+      -moz-appearance: textfield;
+    }
+  }
+
+  .mn-counter-btn {
+    flex-shrink: 0;
+    border: solid 1px #ddd;
+    background: transparent;
+    width: 2.375rem;
+    text-align: center;
+    cursor: pointer;
+    padding: 0;
+    appearance: none;
+    -webkit-tap-highlight-color: transparent;
+
+    &.is-disabled {
+      background: #eee;
       outline: none;
-      -webkit-appearance: none;
-      -webkit-tap-highlight-color: transparent;
-      border: none;
-      background: transparent;
-
-      &::-webkit-outer-spin-button,
-      &::-webkit-inner-spin-button {
-        -webkit-appearance: none !important;
-        margin: 0;
-      }
-
-      &[type="number"] {
-        -moz-appearance: textfield;
-      }
     }
 
-    &-btn {
-      flex-shrink: 0;
-      border: solid 1px #ddd;
-      background: transparent;
-      width: 2.375rem;
-      text-align: center;
-      cursor: pointer;
-      padding: 0;
-      appearance: none;
-      -webkit-tap-highlight-color: transparent;
+    &:first-child {
+      border-top-left-radius: $base-radius;
+      border-bottom-left-radius: $base-radius;
+    }
 
-      &.is-disabled {
-        background: #eee;
-        outline: none;
-      }
-
-      &:first-child {
-        border-top-left-radius: $base-radius;
-        border-bottom-left-radius: $base-radius;
-      }
-
-      &:last-child {
-        border-top-right-radius: $base-radius;
-        border-bottom-right-radius: $base-radius;
-      }
+    &:last-child {
+      border-top-right-radius: $base-radius;
+      border-bottom-right-radius: $base-radius;
     }
   }
 </style>
