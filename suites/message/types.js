d7394d99afdf14076409f473bfb0ead451d458a6
ChaselWu <minowu@foxmail.com>
2017-06-04 00:03:05 +0800

    Add success for primary alias in message types, #131

--- suites/message/types.js
+++ suites/message/types.js
@@ -6,6 +6,7 @@ import information from 'vue-human-icons/js/ios/information'
 export default {
   default: { text: 'Message', icon: chatbubble, color: 'black' },
   primary: { text: 'Success', icon: checkmark, color: 'green' },
+  success: { text: 'Success', icon: checkmark, color: 'green' },
   warning: { text: 'Warning', icon: information, color: 'orange' },
   error: { text: 'Error', icon: close, color: 'pink' }
 }
