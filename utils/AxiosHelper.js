cbe9ed505a156d065fdc24bab78b508070d9414a
ChaselWu <minowu@foxmail.com>
2017-06-10 20:44:22 +0800

    Rewrite AxiosHelper

--- utils/AxiosHelper.js
+++ utils/AxiosHelper.js
@@ -1,107 +1,73 @@
 import isFunction from 'lodash/isFunction'
 
-/** Class axios helper for user */
+/** axios 辅助类，用于判断 response error 和打开相应的 Message 浮层 */
 export default class AxiosHelper {
   /**
-   * Handle axios error message
-   * @param {Object} error - axios return error
-   * @return {void}
+   * handleErrorByStatus 的别名
+   * @alias handleErrorByStatus
+   * @public
+   * @param  {Object}    error - 错误响应的对象
+   * @return {*}
    */
-  error (errorDate) {
-    // If error doesn't has response
-    // So, use default exception error
-    if (!errorDate.response) {
-      return this.errorException(errorDate)
-    }
-
-    // Build errorxxx function
-    const funcName = `error${errorDate.response.status}`
-
-    if (isFunction(this[funcName])) {
-      return this[funcName](errorDate)
-    }
-
-    return '未定义 error 处理方式'
+  error (errorResponse) {
+    return this.handleErrorByStatus(errorResponse)
   }
 
   /**
-   * Open error type message
-   * @param {Object} messageParams
-   * @return {void}
+   * 通过 response status code 执行定义后的相应方法
+   * @method handleErrorByStatus
+   * @public
+   * @param  {Object}     errorResponse - 错误响应的对象
+   * @return {*}
    */
-  openErrorMessage (messageParams) {
-    // 异步加载，优先保持其他前置组件优先完成加载
-    require(['./Message'], (resolve) => {
-      this.createMessage(resolve.default, messageParams)
-    })
+  handleErrorByStatus (errorResponse) {
+    // 如果不存在 reponse，则属于 axios 的异常错误
+    if (!errorResponse.response) {
+      return this.openExceptionMessage(errorResponse)
+    }
 
-    return this
+    // 构造函数名
+    const functionName = `error${errorResponse.response.status}`
+
+    // 查找是否定义了该函数
+    if (isFunction(this[functionName])) {
+      return this[functionName](errorResponse)
+    }
   }
 
   /**
-   * Create Message
+   * 异步载入 utils/Message，完成后创建 message
+   * @method openMessage
+   * @public
+   * @param  {Object}    propsData - MnMessage props 属性的值
+   * @return {Promise}
+   */
+  async openMessage (propsData = {}) {
+    const { default: Message } = await import('./Message')
+    return this.createMessage(Message, propsData)
+  }
+
+  /**
+   * 初始化 utils/Message
+   * @method createMessage
    * @protected
-   * @param  {Message}     Message
-   * @param  {Object}     messageParams
+   * @param  {Message}    Message   - utils/Message 对象
+   * @param  {Object}     propsData - MnMessage props 属性的值
    * @return {Message}
    */
-  createMessage (Message, messageParams) {
-    const message = Message.create({
-      ...{ type: 'error' }, ...messageParams
-    })
-    return message.show()
+  createMessage (Message, propsData = {}) {
+    const defaultData = { type: 'error', message: '网络异常错误，请刷新。' }
+    return Message.create({ ...defaultData, ...propsData }).show()
   }
 
   /**
-   * Default exception error
+   * 异常错误，打开对应的 message
+   * @method openExceptionMessage
+   * @public
+   * @param  {Object}             errorResponse - 错误信息
+   * @return {Promise}
    */
-  errorException (errorDate) {
-    return this.openErrorMessage({
-      description: '网络异常错误，请刷新。'
-    })
-  }
-
-  /**
-   * 401 Unauthorized
-   * @protected
-   * @param {Object} error
-   */
-  $error401 (errorDate) {
-    return this.openErrorMessage({
-      description: '帐户已注销，请重新登录。'
-    })
-  }
-
-  /**
-   * 403 Forbidden
-   * @protected
-   * @param {Object} error
-   */
-  $error403 (errorDate) {
-    return this.openErrorMessage({
-      description: '您没有权限操作该功能。'
-    })
-  }
-
-  /**
-   * 404 Not Found
-   * @protected
-   * @param {Object} error
-   */
-  $error404 (errorDate) {
-    return this.openErrorMessage({
-      description: '资源不存在或已删除。'
-    })
-  }
-
-  /**
-   * 500 Internal Server Error
-   * @protected
-   * @param {Object} error
-   */
-  $error500 (errorDate) {
-    return this.openErrorMessage({
-      description: '服务器出了一点故障，请稍后重试。'
-    })
+  async openExceptionMessage (errorResponse) {
+    return await this.openMessage()
   }
 }
