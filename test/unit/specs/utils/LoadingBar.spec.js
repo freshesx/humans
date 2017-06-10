c4bcc4d736f1ba2a04142031c210f05e2a0dfd2b
ChaselWu <minowu@foxmail.com>
2017-06-10 15:55:59 +0800

    Simpilify loadingBar.spec

--- test/unit/specs/utils/LoadingBar.spec.js
+++ test/unit/specs/utils/LoadingBar.spec.js
@@ -4,11 +4,11 @@ describe('utils/LoadingBar', () => {
   it('#finish 70', done => {
     const loadingBar = new LoadingBar()
 
-    loadingBar.finish(70).then(() => {
-      const vm = loadingBar.vueComponent
-      expect(vm.$props.isShow).to.equal(true)
-      expect(vm.$props.percent).to.equal(70)
-      expect(vm.$props.millisecond).to.equal(1000)
+    loadingBar.finish(70).then(self => {
+      const vm = self.vueComponent
+      expect(vm.isShow).to.equal(true)
+      expect(vm.percent).to.equal(70)
+      expect(vm.millisecond).to.equal(1000)
       done()
     })
   })
@@ -18,9 +18,9 @@ describe('utils/LoadingBar', () => {
 
     loadingBar.finish(100).then(self => {
       const vm = self.vueComponent
-      expect(vm.$props.isShow).to.equal(false)
-      expect(vm.$props.percent).to.equal(0)
-      expect(vm.$props.millisecond).to.equal(0)
+      expect(vm.isShow).to.equal(false)
+      expect(vm.percent).to.equal(0)
+      expect(vm.millisecond).to.equal(0)
       done()
     })
   })
