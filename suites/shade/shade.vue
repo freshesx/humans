de49a25c581eba9f2831d85cbf63c3a29c3ec55d
ChaselWu <minowu@foxmail.com>
2017-06-11 14:26:03 +0800

    New shade component

---
+++ suites/shade/shade.vue
@@ -0,0 +1,60 @@
+<template>
+  <transition :name="transition">
+    <div class="mn-shade" v-if="visible" :style="{ 'z-index': zIndex }"
+      @click="onHide"></div>
+  </transition>
+</template>
+
+<script>
+  import Element from '../../utils/Element'
+
+  export default new Element({
+    name: 'mn-shade',
+    props: {
+      theme: String,
+      visible: Boolean,
+      zIndex: {
+        type: Number,
+        default: 2000
+      },
+      transition: {
+        type: String,
+        default: 'mn-shade-fade'
+      }
+    },
+    methods: {
+      onHide () {
+        this.hide()
+      },
+      show () {
+        this.$emit('update:visible', true)
+      },
+      hide () {
+        this.$emit('update:visible', false)
+      }
+    }
+  })
+</script>
+
+<style lang="scss">
+  .mn-shade {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #000;
+    opacity: 0.4;
+    z-index: 1000;
+  }
+
+  .mn-shade-fade-enter-active,
+  .mn-shade-fade-leave-active {
+    transition: opacity 0.3s linear;
+  }
+
+  .mn-shade-fade-enter,
+  .mn-shade-fade-leave-active {
+    opacity: 0;
+  }
+</style>
