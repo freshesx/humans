4558e163a2d1848a164206bda6641ade503fc969
ChaselWu <minowu@foxmail.com>
2017-06-11 22:32:32 +0800

    Rewrite new utils/Confirm.

--- utils/Confirm.js
+++ utils/Confirm.js
@@ -1,6 +1,6 @@
-import Popup from './Popup'
+import Layer from './Layer'
 import confirmElement from '../suites/confirm/confirm'
 
-export default class Confirm extends Popup {
+export default class Confirm extends Layer {
   element = confirmElement
 }
