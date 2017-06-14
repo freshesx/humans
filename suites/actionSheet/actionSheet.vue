cafdd6d8f4727222fba69022ad564ddec60c70c2
ChaselWu <minowu@foxmail.com>
2017-06-14 12:16:32 +0800

    Rewrite actionSheet

--- suites/actionSheet/actionSheet.vue
+++ suites/actionSheet/actionSheet.vue
@@ -1,38 +1,40 @@
 <template>
-  <mn-popup class="mn-action-sheet"
-    animation="slideInBottom"
-    :show="show"
-    @close="onClosePopup">
-    <!-- Main contents -->
-    <div :style="hasCancelButtonStyle">
-      <slot></slot>
+  <transition :name="transition || 'has-slide-in-bottom'">
+    <div class="mn-action-sheet" v-if="visible" :style="{ zIndex }">
+      <!-- Main contents -->
+      <div :style="hasCancelButtonStyle">
+        <slot></slot>
+      </div>
+      <!-- Cancel button -->
+      <mn-card class="has-none-margin-bottom" v-if="cancelButton" rounded>
+        <mn-card-btns>
+          <button class="has-blue-text" @click="hide">
+            <strong>{{ $t('mn.popup.cancelText') }}</strong>
+          </button>
+        </mn-card-btns>
+      </mn-card>
     </div>
-    <!-- Cancel button -->
-    <mn-card class="has-none-margin-bottom" v-if="cancelButton" rounded>
-      <mn-card-btns>
-        <button class="has-blue-text" @click="onClosePopup">
-          <strong>{{ $t('mn.popup.cancelText') }}</strong>
-        </button>
-      </mn-card-btns>
-    </mn-card>
-  </mn-popup>
+  </transition>
 </template>
 
 <script>
   import Element from '../../utils/Element'
-  import popup from '../popup/popup'
   import card from '../card/card'
   import cardBtns from '../card/cardBtns'
+  import layerMixin from '../layer/layerMixin'
+  import shadeMixin from '../layer/shadeMixin'
 
   export default new Element({
     name: 'mn-action-sheet',
     components: {
-      ...popup.inject(),
       ...card.inject(),
       ...cardBtns.inject()
     },
+    mixins: [
+      layerMixin,
+      shadeMixin
+    ],
     props: {
-      show: Boolean,
       cancelButton: {
         type: Boolean,
         default: true
@@ -46,8 +48,8 @@
       }
     },
     methods: {
-      onClosePopup () {
-        this.$emit('update:show', false)
+      whenShadeCallHiding () {
+        this.hide()
       }
     }
   })
@@ -58,6 +60,7 @@
   @import "./vars";
 
   .mn-action-sheet {
+    position: fixed;
     right: $mn-action-sheet-grid-gutter / 2;
     bottom: $mn-action-sheet-grid-gutter / 2;
     left: $mn-action-sheet-grid-gutter / 2;
