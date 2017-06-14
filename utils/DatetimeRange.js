93cc20d3019f8de1700e3ba1d444024b297c5f29
ChaselWu <minowu@foxmail.com>
2017-06-14 18:24:37 +0800

    Add new create static mehtod, and some comments

--- utils/DatetimeRange.js
+++ utils/DatetimeRange.js
@@ -1,31 +1,96 @@
 import Vue from 'vue'
+import isUndefined from 'lodash/isUndefined'
 import Datetime from './Datetime'
 import { addDay, addHours } from '../suites/datetime/dateChecker'
 
 export default class DatetimeRange {
+  /**
+   * 存储开始时间
+   * @private
+   * @type {Date}
+   */
   fromAt
 
+  /**
+   * 存储结束时间
+   * @private
+   * @type {Date}
+   */
   toAt
 
+  /**
+   * 存储开始时间的浮层
+   * @private
+   * @type {Layer}
+   */
   fromAtLayer
 
+  /**
+   * 存储结束时间的浮层
+   * @private
+   * @type {Layer}
+   */
   toAtLayer
 
+  /**
+   * 存储开始时间的配置
+   * @private
+   * @type {Object}
+   */
   fromAtConfig
 
+  /**
+   * 存储结束时间的配置
+   * @private
+   * @type {Object}
+   */
   toAtConfig
 
+  /**
+   * 静态方法创建实例化类，然后配置类的参数
+   * @method create
+   * @param  {Object} [fromPropsData={}] - 开始时间的配置数据
+   * @param  {Object} [toPropsData={}]   - 结束时间的配置数据
+   * @return {DatetimeRange}
+   */
+  static create (fromPropsData = {}, toPropsData = {}) {
+    const instance = new this()
+    instance.setFromAtConfig(fromPropsData)
+    instance.setToAtConfig(toPropsData)
+    return instance
+  }
+
+  /**
+   * 请使用 create 方法实现，fromAt 方法即将弃用
+   * 配置开始时间
+   * @method fromAt
+   * @param  {Object} [options={}]
+   * @return {DatetimeRange}
+   */
   static fromAt (options = {}) {
+    console.warn('该 static fromAt 方法将被弃用，建议使用 create 实现。')
     const instance = new this()
     instance.setFromAtConfig(options)
     return instance
   }
 
+  /**
+   * 请使用 create 方法的第二个参数实现，toAt 方法即将弃用
+   * @method toAt
+   * @param  {Object} [options={}]
+   * @return {DatetimeRange}
+   */
   toAt (options = {}) {
+    console.warn('该 toAt 方法将被弃用，建议使用 create 实现。')
     this.setToAtConfig(options)
     return this
   }
 
+  /**
+   * 显示浮层
+   * @method show
+   * @return {DatetimeRange}
+   */
   show () {
     return this.showFromAt()
   }
