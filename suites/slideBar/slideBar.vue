17e5a0504b3e9b9853348122fc54e662ed9524db
ChaselWu <minowu@foxmail.com>
2017-07-05 16:50:59 +0800

    Update slideBar

--- suites/slideBar/slideBar.vue
+++ suites/slideBar/slideBar.vue
@@ -6,8 +6,8 @@
     :style="{ height: `${itemsMaxHeight}px` }">
     <div class="mn-slide-bar-contents"
       :style="{
-        transform: `translate(${this.left * -1}px, 0)`,
-        'transition-duration': `${500}ms`
+        transform: `translate(${computedLeft * -1}px, 0)`,
+        'transition-duration': `${computedDuration}ms`
       }">
       <slot></slot>
     </div>
@@ -25,7 +25,12 @@
         touchMoveX: 0,
         itemIndex: 0,
         items: [],
-        left: 0
+        moving: false,
+        left: 0,
+        // movedLeft: 0,
+        finishedLeft: 0,
+        touchStartLeft: 0,
+        duration: 500
       }
     },
     computed: {
@@ -35,19 +40,49 @@
           height = item.height > height ? item.height : height
         })
         return height
+      },
+      itemsMaxWidth () {
+        let width = 0
+        this.items.forEach(item => {
+          width += item.width
+        })
+        return width
+      },
+      computedDuration () {
+        return this.moving ? 0 : this.duration
+      },
+      computedLeft () {
+        return this.moving
+          ? this.left
+          : this.finishedLeft
       }
     },
     methods: {
+      // 记录开始 X 坐标，left 偏移值，进入触摸状态
       touchStart (event) {
         this.touchStartX = event.touches[0].pageX
+        this.touchStartLeft = this.left
+        this.moving = true
       },
+      // 计算 move 的偏移值，计算在 left 上
       touchMove (event) {
+        const minLeft = document.body.offsetWidth * -0.3
+        const maxLeft = this.itemsMaxWidth - document.body.offsetWidth * 0.7
+
         this.touchMoveX = event.touches[0].pageX
+        let left = this.touchStartLeft + (this.touchMoveX - this.touchStartX) * -1
+
+        if (left < minLeft) left = minLeft
+        if (left > maxLeft) left = maxLeft
+
+        this.left = left
       },
+      // 根据偏移值计算下一项或上一项，关闭触摸状态，更新最后的 left 值
       touchEnd (event) {
         const offset = this.touchMoveX - this.touchStartX
         if (offset > 80) this.prev()
         if (offset < -80) this.next()
+        this.moving = false
         this.updateLeft()
       },
       next () {
@@ -64,13 +99,25 @@
         }
         this.itemIndex = itemIndex
       },
+      // 计算 finishedLft 可以进入的范围
       updateLeft () {
         let left = 0
+        const minLeft = 0
+        const maxLeft = this.itemsMaxWidth - document.body.offsetWidth
         const pickeds = this.items.slice(0, this.itemIndex)
+
+        // 根据项目计算偏移位置
         pickeds.forEach(item => {
           left += item.width
         })
+
+        // 控制最小值和最大值
+        if (left < minLeft) left = minLeft
+        if (left > maxLeft) left = maxLeft
+
+        // 赋值
         this.left = left
+        this.finishedLeft = left
       }
     },
     mounted () {
--- suites/slideBar/slideBarItem.vue
+++ suites/slideBar/slideBarItem.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="mn-slide-bar-item">
+  <div class="mn-slide-bar-item"
+    :style="{ width: `${width}px`, height: `${height}px` }">
     <slot></slot>
   </div>
 </template>
@@ -8,6 +9,25 @@
   import Element from 'vue-human/utils/Element'
 
   export default new Element({
-    name: 'mn-slide-bar-item'
+    name: 'mn-slide-bar-item',
+    props: {
+      mobile: {
+        type: String
+      }
+    },
+    computed: {
+      width () {
+        return parseInt(this.mobile.split(/,\s*/)[0])
+      },
+      height () {
+        return parseInt(this.mobile.split(/,\s*/)[1])
+      }
+    }
   })
 </script>
+
+<style lang="scss">
+  .mn-slide-bar-item {
+    overflow: hidden;
+  }
+</style>
