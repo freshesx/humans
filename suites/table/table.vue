047f2090425ac2be7dc63a532b86f0c9a88cab64
ChaselWu <minowu@foxmail.com>
2017-07-14 23:27:32 +0800

    Sperate tableHeader

--- suites/table/table.vue
+++ suites/table/table.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="mn-table">
-    <div class="mn-table-hd">
-      <div class="mn-table-col" v-for="(column, key) in columns" :key="key" :style="[ calcWidth(column.width) ]">
-        {{ column.title }}
-      </div>
-    </div>
+    <mn-table-header :columns="columns"></mn-table-header>
   </div>
 </template>
 
 <script>
   import Element from '../../utils/Element'
-  import isUndefined from 'lodash/isUndefined'
+  import tableHeader from './tableHeader'
 
   export default new Element({
     name: 'mn-table',
+    components: {
+      ...tableHeader.inject()
+    },
     props: {
       // undefined 为加载状态、空数组为无数据、非空数组展示内容
       items: Array,
@@ -36,39 +35,11 @@
       },
       emitChoices () {
 
-      },
-      calcWidth (width) {
-        if (isUndefined(width)) {
-          return { width: '120px' }
-        }
-
-        if (/^\d+&/.test(width)) {
-          return { flex: width }
-        }
-
-        return {
-          width: width
-        }
       }
     }
   })
 </script>
 
 <style lang="scss">
-  .mn-table-hd {
-    display: flex;
-    flex-wrap: nowrap;
-    border-radius: 0.25rem;
-    background: #007aff;
-    color: rgba(255, 255, 255, 1);
-  }
 
-  .mn-table-col {
-    padding: 0.75rem 1rem;
-    font-weight: 500;
-
-    & + & {
-      border-left: solid 2px rgba(255, 255, 255, 0.3);
-    }
-  }
 </style>
---
+++ suites/table/tableHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mn-table-hd">
+    <div class="mn-table-hd-col" v-for="(column, key) in columns" :key="key" :style="[ calcWidth(column.width) ]">
+      {{ column.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  import Element from '../../utils/Element'
+  import isUndefined from 'lodash/isUndefined'
+
+  export default new Element({
+    name: 'mn-table-header',
+    props: {
+      columns: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    methods: {
+      calcWidth (width) {
+        if (isUndefined(width)) {
+          return { width: '120px' }
+        }
+
+        if (/^\d+&/.test(width)) {
+          return { flex: width }
+        }
+
+        return {
+          width: width
+        }
+      }
+    }
+  })
+</script>
+
+<style lang="scss">
+  .mn-table-hd {
+    display: flex;
+    flex-wrap: nowrap;
+    border-radius: 0.25rem;
+    background: #007aff;
+    color: rgba(255, 255, 255, 1);
+  }
+
+  .mn-table-hd-col {
+    padding: 0.75rem 1rem;
+    font-weight: 500;
+
+    & + & {
+      border-left: solid 2px rgba(255, 255, 255, 0.3);
+    }
+  }
+</style>
