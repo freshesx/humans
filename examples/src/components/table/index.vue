034886ad8fe33b5fe67badf3800d31f574e8ecfb
ChaselWu <minowu@foxmail.com>
2017-07-15 03:51:15 +0800

    Update example

--- examples/src/components/table/index.vue
+++ examples/src/components/table/index.vue
@@ -6,13 +6,15 @@
       </mn-letter-body>
     </mn-letter>
 
-    <mn-table :columns="tableColumns"></mn-table>
+    <mn-table :items="tableItems | updateItems" :columns="tableColumns"></mn-table>
   </page>
 </template>
 
 <script>
   import table from 'vue-human/suites/table'
   import tableColumns from './tableColumns'
+  import axios from 'axios'
+  import isUndefined from 'lodash/isUndefined'
 
   export default {
     components: {
@@ -20,7 +22,32 @@
     },
     data () {
       return {
-        tableColumns
+        tableColumns,
+        tableItems: undefined
+      }
+    },
+    methods: {
+      async fetchMovie (start, count) {
+        const response = await axios.get('/api/movie/in_theaters')
+        this.tableItems = response.data.subjects
+      }
+    },
+    created () {
+      this.fetchMovie()
+    },
+    filters: {
+      updateItems (items) {
+        if (isUndefined(items)) return undefined
+        return items.map(item => {
+          return {
+            cover: item.title,
+            id: item.id,
+            title: item.title,
+            rating: item.rating.average,
+            tags: item.genres.join(', '),
+            year: item.year
+          }
+        })
       }
     }
   }
