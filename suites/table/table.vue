712cc07a61009f25f5b4af18eea59e68f3fd7f93
ChaselWu <minowu@foxmail.com>
2017-07-14 22:47:37 +0800

    Defined table component

---
+++ suites/table/table.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>nihao</div>
+</template>
+
+<script>
+  import Element from '../utils/Element'
+
+  export default new Element({
+    name: 'mn-table',
+    props: {
+      // undefined 为加载状态、空数组为无数据、非空数组展示内容
+      items: Array,
+      choices: {
+        type: Array,
+        default () {
+          return []
+        }
+      },
+      columns: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    methods: {
+      emitActions () {
+
+      },
+      emitChoices () {
+
+      }
+    }
+  })
+</script>
