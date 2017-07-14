65a0cfc6e727ec0f0c821c487538907753cb0293
ChaselWu <minowu@foxmail.com>
2017-07-14 23:28:13 +0800

    Add table example

---
+++ examples/src/components/table/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <page>
+    <mn-letter>
+      <mn-letter-body>
+        <h1>Table</h1>
+      </mn-letter-body>
+    </mn-letter>
+
+    <mn-table :columns="tableColumns"></mn-table>
+  </page>
+</template>
+
+<script>
+  import table from 'vue-human/suites/table'
+  import tableColumns from './tableColumns'
+
+  export default {
+    components: {
+      ...table.map()
+    },
+    data () {
+      return {
+        tableColumns
+      }
+    }
+  }
+</script>
---
+++ examples/src/components/table/tableColumns.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    name: 'id',
+    title: 'ID',
+    width: '60px'
+  },
+  {
+    name: 'name',
+    title: '城市名称'
+  },
+  {
+    name: 'population',
+    title: '人口数'
+  },
+  {
+    name: 'gdp',
+    title: 'GDP'
+  },
+  {
+    name: 'perGdp',
+    title: '人均 GDP'
+  },
+  {
+    name: 'rate',
+    title: '增长率'
+  },
+  {
+    name: '$action',
+    title: '操作',
+    actions: [
+      {
+        name: 'edit',
+        title: '查看'
+      },
+      {
+        name: 'remove',
+        title: '删除'
+      }
+    ]
+  }
+]
