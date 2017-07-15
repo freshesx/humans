2559d6f00c527c88dd8e8ef4a74460b253569ad7
ChaselWu <minowu@foxmail.com>
2017-07-15 17:55:27 +0800

    Update example

--- examples/src/components/table/index.vue
+++ examples/src/components/table/index.vue
@@ -6,9 +6,10 @@
       </mn-letter-body>
     </mn-letter>
 
-    <mn-table :items="tableItems | updateItems" :columns="tableColumns">
+    <mn-table :items="tableItems | updateItems"
+      :columns="tableColumns">
       <template scope="scope" slot="cover">
-        <img :src="scope.item.cover" alt="scope.cover.title" height="80">
+        <img :src="scope.item.cover" alt="scope.cover.title" height="80" style="display: block;">
       </template>
     </mn-table>
   </page>
