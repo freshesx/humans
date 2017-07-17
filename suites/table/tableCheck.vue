5a175bc703c078be200c594c75e774d785f9d984
ChaselWu <minowu@foxmail.com>
2017-07-17 15:57:43 +0800

    Rewrite table-check and it’s style

--- suites/table/tableCheck.vue
+++ suites/table/tableCheck.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="checkbox" :checked="checked">
+  <div class="mn-table-check" :class="{ 'is-active': checked }" @click="onClick"></div>
 </template>
 
 <script>
@@ -9,6 +9,25 @@
     name: 'mn-table-check',
     props: {
       checked: Boolean
+    },
+    methods: {
+      onClick (event) {
+        this.$emit('click', event)
+      }
     }
   })
 </script>
+
+<style lang="scss">
+  .mn-table-check {
+    border: solid 2px #666;
+    border-radius: 0.25rem;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &.is-active {
+      background: #666;
+    }
+  }
+</style>
