ab84fbcf4de37da954b78f058dea82575b87e53d
ChaselWu <minowu@foxmail.com>
2017-07-17 15:08:32 +0800

    New tableCheck component

---
+++ suites/table/tableCheck.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="checkbox" :checked="checked">
+</template>
+
+<script>
+  import Element from '../../utils/Element'
+
+  export default new Element({
+    name: 'mn-table-check',
+    props: {
+      checked: Boolean
+    }
+  })
+</script>
