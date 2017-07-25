007bdeecdc520a7c2e44c1ca55f8d3ddc1099b3a
ChaselWu <minowu@foxmail.com>
2017-07-25 18:34:53 +0800

    Update example home page

--- examples/src/components/home/index.vue
+++ examples/src/components/home/index.vue
@@ -3,32 +3,44 @@
     <mn-section>
       <mn-letter>
         <mn-letter-body>
-          <h1>Vue Human</h1>
+          <h1>Freshes</h1>
         </mn-letter-body>
       </mn-letter>
     </mn-section>
 
-    <mn-columns>
-      <mn-column tablet="4" desktop="3"
-        v-for="(item, itemIndex) in menu" :key="itemIndex">
-        <mn-section>
-          <mn-section-note>
-            {{ item.name }}
-          </mn-section-note>
-          <mn-card>
-            <mn-card-item type="link" @click="$router.push(child.push)"
-              v-for="(child, childIndex) in item.children" :key="childIndex">
-              <mn-card-body>
-                {{ child.name }}
-              </mn-card-body>
-              <mn-card-suffix>
-                <small>{{ child.small }}</small>
-              </mn-card-suffix>
-            </mn-card-item>
-          </mn-card>
-        </mn-section>
-      </mn-column>
-    </mn-columns>
+    <mn-section>
+      <mn-card>
+        <mn-card-item>
+          <mn-card-body>
+            <div class="has-six-padding-y">
+              <img src="https://raw.githubusercontent.com/HumanUI/vue-human/master/docs/vue-human.jpg" width="320" class="has-block" style="margin: 0 auto;">
+            </div>
+            <div style="text-align: center;">
+              <a href="https://www.npmjs.com/package/vue-human" title="npm">
+                <img src="https://badge.fury.io/js/vue-human.svg">
+              </a>
+              <span>
+                <img :src="`https://img.shields.io/badge/案例版本-${version}-brightgreen.svg`">
+              </span>
+              <a href="https://minowu.gitbooks.io/vue-human" title="gitbook">
+                <img src="https://img.shields.io/badge/gitbook-中文文档-brightgreen.svg">
+              </a>
+              <a href="https://gitter.im/HumanUI/Lobby" title="GITTER">
+                <img src="https://img.shields.io/badge/gitter-chat-brightgreen.svg">
+              </a>
+              <span title="License MIT">
+                <img src="https://img.shields.io/badge/license-MIT-brightgreen.svg">
+              </span>
+            </div>
+            <div style="text-align: center;">
+              <a href="https://circleci.com/gh/HumanUI/vue-human/tree/master" title="CircleCI">
+                <img src="https://circleci.com/gh/HumanUI/vue-human/tree/master.svg?style=svg">
+              </a>
+            </div>
+          </mn-card-body>
+        </mn-card-item>
+      </mn-card>
+    </mn-section>
   </page>
 </template>
 
@@ -38,7 +50,8 @@
   export default {
     data () {
       return {
-        menu
+        menu,
+        version: process.env.VERSION
       }
     }
   }
