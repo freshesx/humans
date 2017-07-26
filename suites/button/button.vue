4eb894fe5e9c81440e482f588dcad4fbf0ecf970
ChaselWu <minowu@foxmail.com>
2017-07-26 13:55:25 +0800

    Add since

--- suites/button/button.vue
+++ suites/button/button.vue
@@ -113,6 +113,7 @@
 
       /**
        * 按钮处于载入状态时触发 loading 事件。
+       * @since 2.6.4
        * @event loading
        * @prop {Event}    - 事件对象
        * @prop {this}     - 按钮实例
@@ -123,6 +124,7 @@
 
       /**
        * 启动加载状态
+       * @since 2.6.4
        * @method start
        * @return {this}     - 按钮实例
        */
@@ -133,6 +135,7 @@
 
       /**
        * 完成载入状态
+       * @since 2.6.4
        * @method finish
        * @return {this}     - 按钮实例
        */
