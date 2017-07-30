ed6aa9ee42f1e7e00bdc132a6f4b6538904450b1
ChaselWu <minowu@foxmail.com>
2017-07-30 16:45:33 +0800

    Update table tool

--- suites/table/tableTool.vue
+++ suites/table/tableTool.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="mn-table-tool">
-    <div class="mn-table-tool-filter" v-if="hasFilters">
-      <slot></slot>
-      <div class="mn-table-tool-filter-box">
-        <div class="mn-table-tool-filter-action">
-          <slot name="action"></slot>
-        </div>
-        <div class="mn-table-tool-filter-operate">
-          <slot name="operate"></slot>
-        </div>
+    <slot></slot>
+    <div class="mn-table-tool-box">
+      <div class="mn-table-tool-action">
+        <slot name="action"></slot>
+      </div>
+      <div class="mn-table-tool-operate">
+        <!-- 预留插槽，未启用 -->
+        <!-- <slot name="operate"></slot> -->
       </div>
     </div>
   </div>
@@ -18,52 +17,12 @@
   import Element from '../../utils/Element'
 
   export default new Element({
-    name: 'mn-table-tool',
-    data () {
-      return {
-        cog: require('vue-human-icons/js/android/apps')
-      }
-    },
-    computed: {
-      hasFilters () {
-        return this.$slots.default
-      }
-    }
+    name: 'mn-table-tool'
   })
 </script>
 
 <style lang="scss">
   .mn-table-tool {
-  }
-
-  .mn-table-tool-action {
-    display: flex;
-    background: #fff;
-    padding: 0.5rem 1rem;
-    align-items: center;
-    border-bottom: solid 1px rgba(0, 0, 0, 0.05);
-  }
-
-  .mn-table-tool-prefix {
-    display: flex;
-    flex: 1;
-  }
-
-  .mn-table-tool-suffix {
-    display: flex;
-  }
-
-  .mn-table-tool-btn {
-    background: #fff;
-    height: 2.25rem + 0.5rem;
-    width: 2.25rem;
-    padding: 0.5rem;
-    border-top-left-radius: 0.25rem;
-    border-top-right-radius: 0.25rem;
-    margin-right: 0.5rem;
-  }
-
-  .mn-table-tool-filter {
     background: #fff;
     padding: 1rem;
     border-bottom-left-radius: 0.25rem;
@@ -72,15 +31,15 @@
     margin-bottom: 1rem;
   }
 
-  .mn-table-tool-filter-box {
+  .mn-table-tool-box {
     display: flex;
   }
 
-  .mn-table-tool-filter-action {
+  .mn-table-tool-action {
     flex: 1;
   }
 
-  .mn-table-tool-filter-operate {
+  .mn-table-tool-operate {
 
   }
 </style>
