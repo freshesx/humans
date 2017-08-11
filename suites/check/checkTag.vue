3adfeb97d34764127a1174294b892629cc68a06b
ChaselWu <minowu@foxmail.com>
2017-08-11 15:22:58 +0800

    Add checkTag

---
+++ suites/check/checkTag.vue
@@ -0,0 +1,26 @@
+<script>
+  import Element from '../../utils/Element'
+  import radioTag from '../radio/radioTag'
+
+  export default new Element({
+    name: 'mn-check-tag',
+    mixins: [ radioTag ],
+    computed: {
+      checked () {
+        return this.value.includes(this.data)
+      }
+    },
+    methods: {
+      click () {
+        if (!this.disabled) {
+          this.$emit(
+            'input',
+            this.value.includes(this.data)
+              ? this.value.filter(item => item !== this.data)
+              : [ ...this.value, this.data ]
+          )
+        }
+      }
+    }
+  })
+</script>
--- suites/check/index.js
+++ suites/check/index.js
@@ -2,9 +2,11 @@ import Suites from '../../utils/Suites'
 import check from './check'
 import checkItem from './checkItem'
 import checkIcon from './checkIcon'
+import checkTag from './checkTag'
 
 export default new Suites([
   check,
   checkItem,
-  checkIcon
+  checkIcon,
+  checkTag
 ])
