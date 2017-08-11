e2ccfa615f1038f253301b56b7a4cf7ae718225a
ChaselWu <minowu@foxmail.com>
2017-08-11 15:18:39 +0800

    Add radioTag component

--- suites/radio/index.js
+++ suites/radio/index.js
@@ -2,9 +2,11 @@ import Suites from '../../utils/Suites'
 import radio from './radio'
 import radioItem from './radioItem'
 import radioIcon from './radioIcon'
+import radioTag from './radioTag'
 
 export default new Suites([
   radio,
   radioItem,
-  radioIcon
+  radioIcon,
+  radioTag
 ])
---
+++ suites/radio/radioTag.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mn-radio-tag" :class="{ 'is-active': checked }" @click="click">
+    <slot>placeholder</slot>
+  </div>
+</template>
+
+<script>
+  import Element from '../../utils/Element'
+
+  export default new Element({
+    name: 'mn-radio-tag',
+    props: {
+      value: null,
+      data: null,
+      disabled: Boolean
+    },
+    computed: {
+      checked () {
+        return this.data === this.value
+      }
+    },
+    methods: {
+      click () {
+        if (!this.disabled) {
+          this.$emit('input', this.data)
+        }
+      }
+    }
+  })
+</script>
+
+
+<style lang="scss">
+  @import "./vars";
+
+  .mn-radio-tag {
+    display: inline-block;
+    border: solid 2px $gray;
+    border-radius: 0.25rem;
+    padding: 0.25rem 0.5rem;
+    cursor: pointer;
+
+    & + & {
+      margin-left: 0.5rem;
+    }
+
+    &.is-active {
+      background: $green;
+      color: #fff;
+      border-color: $green;
+    }
+  }
+</style>
