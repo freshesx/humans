10404992b09c3e4b6d9fab5a9cc4cca679981dde
ChaselWu <minowu@foxmail.com>
2017-11-16 12:02:43 +0800

    Remove vue-i18n and locale files

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import './scss/bases.scss'
 import Vuelidate from 'vuelidate'
-import VueI18n from 'vue-i18n'
-import defaultsDeep from 'lodash/defaultsDeep'
-import componentLocales from './locale'
 
 /**
  * Human 的入口方法，用于 Vue.use() 加载 vue-human
@@ -28,18 +25,6 @@ export default {
     // Vuelidate
     Vue.use(Vuelidate)
 
-    // VueI18n
-    Vue.use(VueI18n)
-
-    // Set Vue config language
-    Vue.config.lang = options.lang || 'zh-CN'
-
-    // Use Promise all
-    this.mergeLocals([ componentLocales, options.locales ])
-      .then(translations => {
-        Vue.locale(this.$vue.config.lang, defaultsDeep(...translations))
-      })
-
     // Merge default screens and options screens
     this.$screens = [
       ...[
@@ -50,42 +35,5 @@ export default {
       ],
       ...Array.isArray(options.screens) ? options.screens : []
     ]
-  },
-
-  /**
-   * 返回一个 Promise 用于加载所有的多语言文件
-   * @protected
-   * @param {Array} locales - 多语言表集合
-   * @return {Promise}
-   */
-  mergeLocals (locales) {
-    return Promise.all(locales.map(locale => this.addLocales(locale)))
-  },
-
-  /**
-   * 将一个具体的语言内容的对象表异步加载入程序
-   * @protected
-   * @param {Array} [locales = []] - 多语言表集合
-   * @param {Object} locales[].locale - 某一个多语言对象
-   * @param {String} locales[].locale.name - 当前多语言的名称
-   * @param {Function|Object} locales[].locale.translation - 当前多语言的内容
-   * @return {Promise}
-   */
-  addLocales (locales = []) {
-    return new Promise(resolve => {
-      let locale = locales.find(item => item.name === this.$vue.config.lang)
-
-      if (!locale) {
-        resolve({})
-      } else if (typeof locale.translation === 'function') {
-        locale.translation(file => {
-          resolve(file.default)
-        })
-      } else if (locale.translation.default) {
-        resolve(locale.translation.default)
-      } else {
-        resolve(locale.translation)
-      }
-    })
   }
 }
