c1565f5bc434b8593c25ad3acd1f89a0cad13bb3
ChaselWu <minowu@foxmail.com>
2017-11-18 23:03:53 +0800

    first loadingIcon example

---
+++ examples/src/components/loadingIcon/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <mn-loading-icon></mn-loading-icon> loading
+  </div>
+</template>
+
+<script>
+  import loadingIcon from '@packages/loadingIcon/dist'
+  import '@packages/loadingIcon/dist/index.css'
+
+  export default {
+    components: {
+      ...loadingIcon.map()
+    }
+  }
+</script>
