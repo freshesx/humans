fbef6a9f57437629c0251fa0783a6ada461872b5
ChaselWu <minowu@foxmail.com>
2017-11-18 22:19:12 +0800

    Write new Suites

---
+++ utils/helpers/Suites.js
@@ -0,0 +1,31 @@
+export default class Suites {
+  /**
+   * @param {Array} vueElements
+   * @param {Object} vueElements[].vueElement
+   */
+  constructor (vueElements) {
+    this.elements = [].concat(vueElements)
+  }
+
+  /**
+   * @return {Object}
+   */
+  map () {
+    const elements = {}
+
+    this.elements.forEach(element => {
+      elements[element.name] = element
+    })
+
+    return elements
+  }
+
+  /**
+   * @param {Vue} Vue
+   */
+  install (Vue) {
+    this.elements.forEach(element => {
+      Vue.use(element)
+    })
+  }
+}
