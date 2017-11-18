dc7812ae6e4fdfd00964fc0a7705b81ead0c8e8a
ChaselWu <minowu@foxmail.com>
2017-11-18 22:22:06 +0800

    Init loadingIcon package

---
+++ packages/loadingIcon/src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import loadingIcon from './loadingIcon.vue'
+import Suites from '../../../utils/helpers/Suites.js'
+
+export default new Suites([
+  loadingIcon
+])
---
+++ packages/loadingIcon/src/loadingIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="mn-loading-icon"></span>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import icon from './icon.vue'
+
+  export default {
+    name: 'mn-loading-icon',
+    props: {
+      icon: {
+        type: Object,  // is a vueComponent
+        default () {
+          const Component = Vue.extend(icon)
+          return new Component().$mount()
+        }
+      }
+    },
+    mounted () {
+      this.$el.appendChild(this.icon.$el)
+    },
+    beforeDestroy () {
+      this.$el.removeChild(this.icon.$el)
+    }
+  }
+</script>
