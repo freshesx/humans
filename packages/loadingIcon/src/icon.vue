8d3d32d95fe071d0c061b33bc9d5a9e5fff220ad
ChaselWu <minowu@foxmail.com>
2017-11-19 13:34:04 +0800

    Sperate scss file

--- packages/loadingIcon/src/icon.vue
+++ packages/loadingIcon/src/icon.vue
@@ -1,36 +1,9 @@
 <template>
-  <span class="mn-default-icon"></span>
+  <span class="mn-loading-icon-default"></span>
 </template>
 
 <script>
   export default {
-    name: 'mn-default-icon'
+    name: 'mn-loading-icon-default'
   }
 </script>
-
-<style lang="scss">
-  .mn-default-icon {
-    @keyframes mn-default-icon {
-      0% {
-        transform: scale(1);
-      }
-
-      50% {
-        transform: scale(0.6);
-      }
-
-      100% {
-        transform: scale(1);
-      }
-    }
-
-    width: 16px;
-    height: 16px;
-    margin: 0 2px;
-    display: inline-block;
-    background: currentColor;
-    border-radius: 8px;
-    vertical-align: -2px;
-    animation: mn-default-icon 1.5s infinite linear;
-  }
-</style>
--- packages/loadingIcon/src/index.js
+++ packages/loadingIcon/src/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import loadingIcon from './loadingIcon.vue'
 import Suites from '../../../utils/helpers/Suites.js'
+import loadingIcon from './loadingIcon.vue'
+import './index.scss'
 
 export default new Suites([
   loadingIcon
