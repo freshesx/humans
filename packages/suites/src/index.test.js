5a4d42ad5618469e3ceb1e7124e85575aea17737
ChaselWu <minowu@foxmail.com>
2017-11-21 19:23:09 +0800

    Update suites test file

--- packages/suites/src/index.test.js
+++ packages/suites/src/index.test.js
@@ -1,8 +1,32 @@
 import Suites from './index.js'
+import Vue from 'vue'
+
+const vueElement = {
+  name: 'mn-test'
+}
 
 describe('constructor', () => {
   test('without vueElement', () => {
     const suites = new Suites()
-    expect(suites.elements).toBe([])
+    expect(suites.elements).toEqual([])
+
+    const map = suites.map()
+    expect(map).toEqual({})
+  })
+
+  test('with vueElement', () => {
+    const suites = new Suites([ vueElement ])
+    expect(suites.elements).toEqual([ vueElement ])
+
+    const map = suites.map()
+    expect(map).toEqual({ 'mn-test': vueElement })
+  })
+})
+
+// @todo use Mock Function to rewrite Vue to test me deeply.
+describe('install', () => {
+  test('install', () => {
+    const suites = new Suites([ vueElement ])
+    Vue.use(suites)
   })
 })
