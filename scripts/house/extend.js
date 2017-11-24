edad6cd2bac1e0ec57dcf24128a3456da5ba92c5
ChaselWu <minowu@foxmail.com>
2017-11-24 18:05:07 +0800

    Add style-import-loader

--- scripts/house/extend.js
+++ scripts/house/extend.js
@@ -7,4 +7,16 @@ module.exports = runner => {
     }
     return webpack
   })
+
+  runner.webpackBuilder.addRule({
+    test: /\.(js|vue)$/,
+    use: [
+      {
+        loader: '@freshes/style-import-loader',
+        options: {
+          scoped: '@freshes/[\\w-]+'
+        }
+      }
+    ]
+  })
 }
