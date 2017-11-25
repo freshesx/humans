f7ef614645e8bee7d49d5dc69c5c3567101cec1b
ChaselWu <minowu@foxmail.com>
2017-11-25 18:39:59 +0800

    Add annotation for scrollers test

--- packages/scroller/src/Scrollers.test.js
+++ packages/scroller/src/Scrollers.test.js
@@ -1,9 +1,11 @@
 import Scrollers from './Scrollers'
 
-describe('srcollers', () => {
+/** @test {Scrollers} */
+describe('Scrollers', () => {
   const scrollers = new Scrollers()
 
-  test('srcollers.addScroller with top 200', () => {
+  /** @test {Scrollers#addScroller} */
+  test('Scrollers.prototype.addScroller with top 200', () => {
     scrollers.addScroller('/', 'default', 200)
     expect(scrollers.scrollers).toEqual([
       {
@@ -15,7 +17,8 @@ describe('srcollers', () => {
     ])
   })
 
-  test('srcollers.addScroller with left 300', () => {
+  /** @test {Scrollers#addScroller} */
+  test('Scrollers.prototype.addScroller with left 300', () => {
     scrollers.addScroller('/', 'default', undefined, 300)
     expect(scrollers.scrollers).toEqual([
       {
@@ -27,7 +30,8 @@ describe('srcollers', () => {
     ])
   })
 
-  test('srcollers.findScroller', () => {
+  /** @test {Scrollers#findScroller} */
+  test('Scrollers.prototype.findScroller', () => {
     scrollers.addScroller('/', 'default', 100, 300)
     const output = scrollers.findScroller('/', 'default')
     expect(output).toEqual({
@@ -38,23 +42,27 @@ describe('srcollers', () => {
     })
   })
 
-  test('srcollers.removeScroller', () => {
+  /** @test {Scrollers#removeScroller} */
+  test('Scrollers.prototype.removeScroller', () => {
     scrollers.removeScroller('/', 'default')
     expect(scrollers.scrollers).toEqual([])
   })
 
-  test('srcollers.getScroller without finded', () => {
+  /** @test {Scrollers#getScroller} */
+  test('Scrollers.prototype.getScroller without finded', () => {
     const output = scrollers.getScroller('/', 'sidebar')
     expect(output).toEqual({ top: 0, left: 0 })
   })
 
-  test('srcollers.getScroller with only top 300', () => {
+  /** @test {Scrollers#getScroller} */
+  test('Scrollers.prototype.getScroller with only top 300', () => {
     scrollers.addScroller('/', 'default', 300)
     const output = scrollers.getScroller('/', 'default')
     expect(output).toEqual({ top: 300, left: 0 })
   })
 
-  test('srcollers.getScroller with only left 500', () => {
+  /** @test {Scrollers#getScroller} */
+  test('Scrollers.prototype.getScroller with only left 500', () => {
     scrollers.addScroller('/', 'default', undefined, 500)
     const output = scrollers.getScroller('/', 'default')
     expect(output).toEqual({ top: 0, left: 500 })
