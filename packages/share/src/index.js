089ea54289d212cbc627672b0191b477077daf50
ChaselWu <minowu@foxmail.com>
2017-11-25 18:20:41 +0800

    Rename

--- packages/share/src/index.js
+++ packages/share/src/index.js
@@ -1,44 +1,4 @@
-/**
- * Component level state shared for @human
- * @example
- * import share from '@humans/share'
- * // Do not use import { Share } from '@humans/share'.
- * // Use 'static function: share' instead of 'constructor function'.
- *
- * share.getLoading()
- */
-export class Share {
-  /**
-   * constructor
-   */
-  constructor () {
-    /**
-     * @protected
-     * @type {Object}
-     * @property {Object} state.loading - e.g. { name: 'mn-card' }
-     */
-    this.state = {
-      loading: undefined
-    }
-  }
-
-  /**
-   * Get loading state
-   * @return {VueObject} - e.g. { name: 'mn-card' }
-   */
-  getLoading () {
-    return this.state.loading
-  }
-
-  /**
-   * Set loading state
-   * @param {VueObject} vueElement - e.g. { name: 'mn-card' }
-   */
-  setLoading (vueElement) {
-    this.state.loading = vueElement
-    return this.state.loading
-  }
-}
+import Share from './Share'
 
 /**
  * Default function
