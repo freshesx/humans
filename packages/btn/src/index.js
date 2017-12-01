5bfef14547b75195da904dc226150bfd7800c0e5
ChaselWu <minowu@foxmail.com>
2017-12-01 15:30:35 +0800

    Move button

---
+++ packages/btn/src/index.js
@@ -0,0 +1,6 @@
+import btn from './btn.vue'
+import './index.scss'
+
+export default {
+  [btn.name]: btn
+}
--- src/suites/button/index.js
+++
@@ -1,7 +0,0 @@
-import Suites from '../../utils/Suites'
-
-import Btn from './button'
-
-export default new Suites([
-  Btn
-])
