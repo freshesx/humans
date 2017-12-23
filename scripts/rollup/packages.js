dddb3aca3b7ecdebca56ee4d30d970a5ebb4feaa
ChaselWu <minowu@foxmail.com>
2017-12-23 23:21:22 +0800

    Add annotations

--- scripts/rollup/packages.js
+++ scripts/rollup/packages.js
@@ -4,10 +4,13 @@ import path from 'path'
 const folders = fs.readdirSync(path.join(process.cwd(), './packages'))
 const excludes = [ '.DS_Store', '.babelrc' ]
 
+// splice excludes name
 excludes.forEach(item => {
   const index = folders.indexOf(item)
   folders.splice(index, 1)
 })
+
+// @todo if it has camelCase, throw error
 
 export default function packages (prefix) {
   if (!prefix) return folders
