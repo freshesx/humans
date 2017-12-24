369e0130dc56ed91909769c323848762e5893bb5
ChaselWu <minowu@foxmail.com>
2017-12-24 17:17:13 +0800

    Add vuelidate into example

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import human from './human'
 import router from './router'
+import vuelidate from './vuelidate'
 import app from './app'
 
 new Vue({
   el: '#app',
   human,
   router,
+  vuelidate,
   render: h => h(app)
 })
