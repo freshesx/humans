9dee46ea661d282c303b90aabe67093c402e1f7c
ChaselWu <minowu@foxmail.com>
2017-12-24 18:45:21 +0800

    Remove old files

--- src/suites/helper/index.js
+++
@@ -1,8 +0,0 @@
-import Suites from '../../utils/Suites'
-import helper from './helper'
-import helperItem from './helperItem'
-
-export default new Suites([
-  helper,
-  helperItem
-])
--- src/suites/label/index.js
+++
@@ -1,6 +0,0 @@
-import Suites from '../../utils/Suites'
-import label from './label'
-
-export default new Suites([
-  label
-])
