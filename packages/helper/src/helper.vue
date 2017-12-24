802b1f50aad1bd10f03e572262dec2d45331ed70
ChaselWu <minowu@foxmail.com>
2017-12-24 18:44:22 +0800

    Add annotations for helper

--- packages/helper/src/helper.vue
+++ packages/helper/src/helper.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="mn-helper" v-if="validate.$error">
+    <!-- show helper contents -->
     <slot></slot>
   </div>
 </template>
 
 <script>
+  /**
+   * Helper component for form
+   */
   export default {
     name: 'mn-helper',
     props: {
+      /**
+       * vuelidate child object, e.g. this.$v.models.age, not this.$v
+       * @see https://github.com/monterail/vuelidate
+       */
       validate: Object
     }
   }
