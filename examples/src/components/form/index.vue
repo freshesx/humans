b14554632a755f74ce4e0b4afe23ec384288d2a8
ChaselWu <minowu@foxmail.com>
2017-12-24 17:17:22 +0800

    Update form example

--- examples/src/components/form/index.vue
+++ examples/src/components/form/index.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <mn-form>
+    <mn-form :validate="$v">
       form area
     </mn-form>
+    <div>
+      {{ $v }}
+    </div>
   </div>
 </template>
 
 <script>
   import form from '@humans/form'
+  import { between } from 'vuelidate/lib/validators'
 
   export default {
-    components: Object.assign({}, form)
+    components: Object.assign({}, form),
+    data () {
+      return {
+        age: 0
+      }
+    },
+    validations: {
+      age: {
+        between: between(20, 30)
+      }
+    }
   }
 </script>
