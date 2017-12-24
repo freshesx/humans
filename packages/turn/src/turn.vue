3d8b2606c0c000bfb089b79ac270ac9db3ca39f8
ChaselWu <minowu@foxmail.com>
2017-12-24 19:10:59 +0800

    Update turn component.

--- packages/turn/src/turn.vue
+++ packages/turn/src/turn.vue
@@ -2,22 +2,35 @@
   <div
     class="mn-turn"
     :class="{ [`is-${theme}`]: !!theme, 'is-checked': data === value }"
-    @click="onInput"></div>
+    @click.prevent.stop="click"></div>
 </template>
 
 <script>
   export default {
     name: 'mn-turn',
     props: {
+      /**
+       * The value can be a string, number, boolean, array, object or anything.
+       * The value is equal undefined or given this.data.
+       */
       value: null,
-      data: null,
+      /**
+       * If the value is setted, the value is data.
+       */
+      data: {
+        default: true
+      },
+      /**
+       * Pass string to change theme, you can define any theme in your css code.
+       * `is-primary` is default. for example, you can set `is-success`.
+       */
       theme: {
         type: String,
         default: 'primary'
       }
     },
     methods: {
-      onInput (event) {
+      click (event) {
         this.$emit('input', this.value === undefined ? this.data : undefined)
       }
     }
