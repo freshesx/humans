96cbd42c7b55336f803dd1ab83920a21be2bc4f1
ChaselWu <minowu@foxmail.com>
2017-12-24 18:23:58 +0800

    Add label component package

---
+++ packages/label/src/index.js
@@ -0,0 +1,6 @@
+import label from './label.vue'
+import './index.scss'
+
+export default {
+  [label.name]: label
+}
---
+++ packages/label/src/label.vue
@@ -0,0 +1,21 @@
+<template>
+  <label
+    class="mn-label"
+    :class="{ 'is-error': isInvalid }">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: 'mn-label',
+    props: {
+      validate: Object
+    },
+    computed: {
+      isInvalid () {
+        return this.validate ? this.validate.$error : false
+      }
+    }
+  }
+</script>
