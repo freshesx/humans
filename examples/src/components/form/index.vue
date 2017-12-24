c62b5f3c58f2e651eb704a5ad390769b4fb7fdc6
ChaselWu <minowu@foxmail.com>
2017-12-24 18:44:30 +0800

    Update form example

--- examples/src/components/form/index.vue
+++ examples/src/components/form/index.vue
@@ -9,6 +9,11 @@
         <input type="number" v-model="models.age">
       </div>
       <div>
+        <mn-helper :validate="$v.models.age">
+          <mn-helper-item name="between">The age need between in 20 - 30.</mn-helper-item>
+        </mn-helper>
+      </div>
+      <div>
         <button>Submit form</button>
       </div>
     </mn-form>
@@ -25,10 +30,11 @@
 <script>
   import form from '@humans/form'
   import label from '@humans/label'
+  import helper from '@humans/helper'
   import { between } from 'vuelidate/lib/validators'
 
   export default {
-    components: Object.assign({}, form, label),
+    components: Object.assign({}, form, label, helper),
     data () {
       return {
         models: {
