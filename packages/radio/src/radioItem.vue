1e149191d5300671745ef88e658711d217068e8c
ChaselWu <minowu@foxmail.com>
2017-12-25 22:03:38 +0800

    Update radio-item annotations

--- packages/radio/src/radioItem.vue
+++ packages/radio/src/radioItem.vue
@@ -1,5 +1,6 @@
 <template>
   <mn-card-item type="cursor" :disabled="disabled" @click="click">
+    <!-- show radio item contents -->
     <slot></slot>
   </mn-card-item>
 </template>
@@ -7,17 +8,33 @@
 <script>
   import card from '@humans/card'
 
+  /**
+   * The radio-item component, like card-item
+   */
   export default {
     name: 'mn-radio-item',
     components: Object.assign({}, card),
     props: {
+      /**
+       * The input value
+       */
       value: null,
+      /**
+       * The parent current value
+       */
       data: null,
+      /**
+       * Disable
+       */
       disabled: Boolean
     },
     methods: {
       click () {
         if (!this.disabled) {
+          /**
+           * @event input
+           * @property {*} data - the output data
+           */
           this.$emit('input', this.data)
         }
       }
