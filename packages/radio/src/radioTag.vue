d78594214a5ff641aa43f9681545960e0fcfc513
ChaselWu <minowu@foxmail.com>
2017-12-25 22:05:19 +0800

    Add radio-tag annotations.

--- packages/radio/src/radioTag.vue
+++ packages/radio/src/radioTag.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="mn-radio-tag" :class="{ 'is-active': checked }" @click="click">
+  <div class="mn-radio-tag"
+    :class="{ 'is-active': checked }"
+    @click="click"
+  >
+    <!-- show radio tag label -->
     <slot>placeholder</slot>
   </div>
 </template>
 
 <script>
+  /**
+   * The radio-tag component
+   */
   export default {
     name: 'mn-radio-tag',
     props: {
+      /**
+       * The input value
+       */
       value: null,
+      /**
+       * The parent current value
+       */
       data: null,
+      /**
+       * Disable
+       */
       disabled: Boolean
     },
     computed: {
@@ -20,6 +36,10 @@
     methods: {
       click () {
         if (!this.disabled) {
+          /**
+           * @event input
+           * @property {*} data - the output data
+           */
           this.$emit('input', this.data)
         }
       }
