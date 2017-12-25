b913cc78ffbd515345364171e95a839c7ae4f003
ChaselWu <minowu@foxmail.com>
2017-12-25 21:56:59 +0800

    Update radio annotations

--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -1,8 +1,11 @@
 <template>
   <label class="mn-radio-label" @click="click">
-    <mn-icon class="mn-radio-checkmark" :class="{ 'is-active': checked }"
-      :name="icon"></mn-icon>
+    <mn-icon class="mn-radio-checkmark"
+      :class="{ 'is-active': checked }"
+      :name="icon"
+    ></mn-icon>
     <span v-if="$slots.default">
+      <!-- show radio label -->
       <slot>placeholder</slot>
     </span>
   </label>
@@ -10,19 +13,39 @@
 
 <script>
   import icon from '@humans/icon'
+  import activeIcon from 'vue-human-icons/js/ios/circle-filled'
+  import unactiveIcon from 'vue-human-icons/js/ios/circle-outline'
 
+  /**
+   * Radio component
+   */
   export default {
     name: 'mn-radio',
     components: Object.assign({}, icon),
     props: {
+      /**
+       * Input value
+       */
       value: null,
+      /**
+       * Expected equal data
+       */
       data: null,
+      /**
+       * Disable
+       */
       disabled: Boolean,
+      /**
+       * Active icon
+       */
       active: {
-        default () { return require('vue-human-icons/js/ios/circle-filled') }
+        default: () => activeIcon
       },
+      /**
+       * Unactive icon
+       */
       unactive: {
-        default () { return require('vue-human-icons/js/ios/circle-outline') }
+        default: () => unactiveIcon
       }
     },
     computed: {
@@ -38,6 +61,10 @@
     methods: {
       click () {
         if (!this.disabled) {
+          /**
+           * @event input
+           * @property {*} newValue - the output value
+           */
           this.$emit('input', this.data)
         }
       }
