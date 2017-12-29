ffcef2542bcf0156b57a3adc44898b9aa954bb84
ChaselWu <minowu@foxmail.com>
2017-12-29 23:22:27 +0800

    Init Layer.test.js file

---
+++ packages/layer/src/Layer.test.js
@@ -0,0 +1,12 @@
+import Layer from './Layer.js'
+
+/** @test {Layer} */
+describe('Layer', () => {
+  /** @test {Layer#createComponent} */
+  test('#createComponent: Should throw error when this.element is not defined.', () => {
+    const layer = new Layer()
+    expect(() => {
+      layer.createComponent()
+    }).toThrowError('The this.element should be a vue element.')
+  })
+})
