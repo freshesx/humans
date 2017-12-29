724e534b609b3706314dab83da05f323f7cbc953
ChaselWu <minowu@foxmail.com>
2017-12-29 23:37:51 +0800

    Move jest scripts

---
+++ scripts/unit/jest.conf.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../../'),
+  mapCoverage: true,
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/scripts/unit/coverage',
+  moduleFileExtensions: [
+    'js',
+    'vue'
+  ],
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  }
+}
