5503cfc7e4234c7fea020e3f3012cba7ed7c5a5a
ChaselWu <minowu@foxmail.com>
2017-12-30 17:47:50 +0800

    Delete old files.

--- src/suites/icon/index.js
+++
@@ -1,6 +0,0 @@
-import Suites from '../../utils/Suites'
-import icon from './icon'
-
-export default new Suites([
-  icon
-])
--- src/suites/loadingIcon/index.js
+++
@@ -1,6 +0,0 @@
-import Suites from '../../utils/Suites'
-import loadingIcon from './loadingIcon'
-
-export default new Suites([
-  loadingIcon
-])
