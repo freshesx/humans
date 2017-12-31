2c4059362011d6998e40c07796d869626f0e55d8
ChaselWu <minowu@foxmail.com>
2017-12-31 16:02:16 +0800

    Add loading-bar component.

---
+++ packages/loading-bar/src/index.js
@@ -0,0 +1,9 @@
+import loadingBar from './loadingBar.vue'
+import LoadingBar from './LoadingBar.js'
+import './index.scss'
+
+export default {
+  [loadingBar.name]: loadingBar
+}
+
+export { LoadingBar }
---
+++ packages/loading-bar/src/loadingBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mn-loading-bar" v-if="isShow">
+    <div class="mn-loading-bar-finish" :style="{
+      width: `${percent}%`,
+      'transition-duration': `${millisecond}ms`
+    }"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'mn-loading-bar',
+    props: {
+      /**
+       * 是否显示加载条
+       */
+      isShow: Boolean,
+
+      /**
+       * 主题类
+       * @todo 预留
+       */
+      theme: String,
+
+      /**
+       * 加载百分比
+       */
+      percent: {
+        type: Number,
+        default: 0,
+        validator: val => val >= 0 && val <= 100
+      },
+
+      /**
+       * 动画的毫秒
+       */
+      millisecond: {
+        type: Number,
+        default: 1000
+      }
+    }
+  }
+</script>
