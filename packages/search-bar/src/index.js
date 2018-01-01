cd378c3b620167c3c6614416a9138455fb38604c
ChaselWu <minowu@foxmail.com>
2018-01-01 17:07:57 +0800

    Add search-bar component.

---
+++ packages/search-bar/src/index.js
@@ -0,0 +1,6 @@
+import searchBar from './searchBar.vue'
+import './index.scss'
+
+export default {
+  [searchBar.name]: searchBar
+}
---
+++ packages/search-bar/src/searchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mn-search-bar" :class="{ [`is-${size}`]: !!size }">
+    <mn-icon :name="searchIcon"></mn-icon>
+    <div class="mn-search-bar-control">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import icon from '@humans/icon'
+  import searchIcon from 'vue-human-icons/js/ios/search-strong'
+
+  export default {
+    components: Object.assign({}, icon),
+    name: 'mn-search-bar',
+    props: {
+      size: String
+    },
+    data () {
+      return {
+        searchIcon
+      }
+    }
+  }
+</script>
