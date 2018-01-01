d91f7ba8a338ef09a3c9ddf411238ddd580b0d6b
ChaselWu <minowu@foxmail.com>
2018-01-01 20:04:27 +0800

    Delete old column files.

--- src/suites/column/cols.vue
+++
@@ -1,9 +0,0 @@
-<script>
-  import Element from '../../utils/Element'
-  import columns from './columns'
-
-  export default new Element({
-    name: 'mn-cols',
-    mixins: [ columns ]
-  })
-</script>
