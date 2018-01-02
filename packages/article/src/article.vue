70106856f86e0ee60c075cda5470e554c3d46bab
ChaselWu <minowu@foxmail.com>
2018-01-02 15:34:33 +0800

    Add annotations for article

--- packages/article/src/article.vue
+++ packages/article/src/article.vue
@@ -1,10 +1,14 @@
 <template>
   <article class="mn-article">
+    <!-- The article contents -->
     <slot></slot>
   </article>
 </template>
 
 <script>
+  /**
+   * Article component
+   */
   export default {
     name: 'mn-article'
   }
