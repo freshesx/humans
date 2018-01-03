ec9186bbb932cd8572288d2c4cbfe683384b1282
ChaselWu <minowu@foxmail.com>
2018-01-03 17:36:37 +0800

    Remove console.log

--- packages/confirm/src/Confirm.js
+++ packages/confirm/src/Confirm.js
@@ -1,8 +1,6 @@
 import { Layer } from '@humans/layer'
 import confirmElement from './confirm.vue'
 
-console.log('confirmElement', confirmElement)
-
 export default class Confirm extends Layer {
   constructor () {
     super()
