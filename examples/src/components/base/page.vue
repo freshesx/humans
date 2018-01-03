1424ca2eba9fcf2fdbd2ded787a6bee2323a2df8
ChaselWu <minowu@foxmail.com>
2018-01-03 19:46:36 +0800

    Delete old example files.

--- examples/src/components/base/page.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <mn-page>
-    <mn-scroller>
-      <mn-container fluid>
-        <slot></slot>
-      </mn-container>
-    </mn-scroller>
-  </mn-page>
-</template>
--- examples/src/components/popup/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>popup</div>
-</template>
