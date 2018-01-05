e3dae854b6a18e716509a4a89e5420808416b840
ChaselWu <minowu@foxmail.com>
2018-01-05 16:17:52 +0800

    Add packages.js valid checker.

--- scripts/rollup/packages.js
+++ scripts/rollup/packages.js
@@ -10,7 +10,25 @@ excludes.forEach(item => {
   if (index > -1) folders.splice(index, 1)
 })
 
-// @todo if it has camelCase, throw error
+// Check the folder name, and package.json valid.
+folders.forEach(folder => {
+  // Only allow a-z and -, if it has camelCase, throw error
+  if (!/^[a-z-]+$/.test(folder)) {
+    throw new Error(`The folder name is wrong. Only allow [a-z] and -: ${folder}`)
+  }
+
+  // Check the folder name is equal package name
+  const pkg = JSON.parse(
+    fs.readFileSync(
+      path.join(process.cwd(), 'packages', folder, './package.json')
+    )
+  )
+  const pkgName = pkg.name.replace('@humans/', '')
+
+  if (folder !== pkgName) {
+    throw new Error(`The package.json name happend wrong: ${folder} and ${pkgName}.`)
+  }
+})
 
 export default function packages (prefix) {
   if (!prefix) return folders
