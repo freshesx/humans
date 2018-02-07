8b59512dfc79cc2d34024029cdef5bdd2e88dea7
ChaselWu <minowu@foxmail.com>
2018-02-07 22:41:30 +0800

    Add scroller and box.

--- packages/index.js
+++ packages/index.js
@@ -2,9 +2,13 @@ import './index.scss'
 import MnLoadingIcon from './loading-icon/MnLoadingIcon.vue'
 import MnIcon from './icon/MnIcon.vue'
 import MnCardSuites from './card/MnCardSuites.js'
+import MnScroller from './scroller/MnScroller.vue'
+import MnBox from './box/MnBox.vue'
 
 export {
   MnLoadingIcon,
   MnIcon,
-  MnCardSuites
+  MnCardSuites,
+  MnScroller,
+  MnBox
 }
