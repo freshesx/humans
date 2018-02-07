61a3dc415d77c3a98b7eb6048141ec2d67c4c793
ChaselWu <minowu@foxmail.com>
2018-02-07 21:17:58 +0800

    Change house extend.

--- scripts/house/extend.js
+++ scripts/house/extend.js
@@ -3,17 +3,8 @@ const path = require('path')
 module.exports = runner => {
   runner.webpackBuilder.extend(webpack => {
     webpack.resolve.alias = {
-      '@humans': path.resolve('./packages')
+      '@freshes/human': path.resolve('./dist/main.esm.js')
     }
     return webpack
-  })
-
-  runner.webpackBuilder.addRule({
-    test: /\.(js|vue)$/,
-    use: [
-      {
-        loader: '@freshes/style-import-loader',
-      }
-    ]
   })
 }
